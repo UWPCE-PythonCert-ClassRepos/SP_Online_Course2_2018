963ca83748000e05634e7aae2a53b5b179866720
Andy Miles <akmiles@icloud.com>
2018-03-23 16:04:06 -0700

    adding utilities.py

---
+++ src/utilities.py
@@ -0,0 +1,34 @@
+import logging
+import logging.config
+
+def configure_logger(name, log_path):
+    logging.config.dictConfig({
+        'version': 1,
+        'formatters': {
+            'default': {'format': '%(asctime)s - %(levelname)s - %(message)s', 'datefmt': '%Y-%m-%d %H:%M:%S'}
+        },
+        'handlers': {
+            'console': {
+                'level': 'DEBUG',
+                'class': 'logging.StreamHandler',
+                'formatter': 'default',
+                'stream': 'ext://sys.stdout'
+            },
+            'file': {
+                'level': 'DEBUG',
+                'class': 'logging.handlers.RotatingFileHandler',
+                'formatter': 'default',
+                'filename': log_path,
+                'maxBytes': 1024,
+                'backupCount': 3
+            }
+        },
+        'loggers': {
+            'default': {
+                'level': 'DEBUG',
+                'handlers': ['console', 'file']
+            }
+        },
+        'disable_existing_loggers': False
+    })
+    return logging.getLogger(name)
