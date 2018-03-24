d2e16dcfb1f8ddef3ab2ce3ae8c20cf78f8d559e
Andy Miles <akmiles@icloud.com>
2018-03-24 12:08:02 -0700

    tidying up
    Signed-off-by: Andy Miles <akmiles@icloud.com>

--- src/mongodb_script.py
+++ src/mongodb_script.py
@@ -3,66 +3,54 @@
 """
 
 import pprint
-
-""""
-from login_database import login_mongodb_cloud
-from login_database import get_credentials
-"""
 import src.login_database
+import src.utilities
 
+log = src.utilities.configure_logger('default', 'logs/mongodb_script.log')
 
 def run_example(furniture_items):
     """
-
+    mongodb data manipulation
     """
-    credentials = src.login_database.get_credentials('mongodb_cloud')
 
-    with src.login_database.login_mongodb_cloud(credentials) as client:
-        #        logger.info('We are going to use a database called dev')
-        #        logger.info('If it doesnt exist mongodb creates it')
+    with src.login_database.login_mongodb_cloud() as client:
+        log.info('We are going to use a database called dev')
+        log.info('If it doesnt exist mongodb creates it')
         db = client['dev']
 
-#        logger.info('And in that database use a collection called furniture')
-#        logger.info('If it doesnt exist mongodb creates it')
+        log.info('And in that database use a collection called furniture')
+        log.info('If it doesnt exist mongodb creates it')
 
         furniture = db['furniture']
 
-#        logger.info('Now we add data from the dictionary above')
-        results = furniture.insert_many(furniture_items)
+        log.info('Now we add data from the dictionary above')
+        furniture.insert_many(furniture_items)
 
-#        logger.info('Find the products that are described as plastic')
+        log.info('Find the products that are described as plastic')
         query = {'description': 'Plastic'}
         results = furniture.find_one(query)
 
-#        logger.info('Print the plastic products')
+        log.info('Print the plastic products')
         pprint.pprint(results)
 
-#        logger.info('Delete the blue couch (atually deletes all blue couches)')
+        log.info('Delete the blue couch (actually deletes all blue couches)')
         furniture.remove({"product": {"$eq": "Blue couch"}})
 
-#        logger.info('Check it is deleted with a query and print')
+        log.info('Check it is deleted with a query and print')
         query = {'product': 'Blue couch'}
         results = furniture.find_one(query)
         pprint.pprint(results)
 
-#        logger.info(
-#            'Find multiple documents, iterate though the results and print')
-#        logger.info('TBD')
+        log.info(
+            'Find multiple documents, iterate though the results and print')
+        log.info('TBD')
 
-#        logger.info('More sophisticated query examples')
-#        logger.info('TBD')
+        log.info('More sophisticated query examples')
+        log.info('TBD')
 
-#        logger.info('Using richer doucment structures')
-#        logger.info('TBD')
+        log.info('Using richer document structures')
+        log.info('TBD')
 
-#        logger.info('Delete the collection so we can start over')
-#        db.drop_collection('furniture')
+        log.info('Delete the collection so we can start over')
+        db.drop_collection('furniture')
 
-        # needed?
-        # client.close()
-#    logger.info('This should fail - if not try with explicit close')
-    query = {'description': 'Plastic'}
-    results = furniture.find_one(query)
-#    logger.info('Print the plastic products')
-    pprint.pprint(results)
-    client.close()
