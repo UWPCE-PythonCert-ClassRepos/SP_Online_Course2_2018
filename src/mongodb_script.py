1a23e8de26e7b9e3e80be1ad53142371573e001c
Andy Miles <akmiles@icloud.com>
2018-03-25 14:04:11 -0700

    updated mongodb example
    Signed-off-by: Andy Miles <akmiles@icloud.com>

--- src/mongodb_script.py
+++ src/mongodb_script.py
@@ -15,7 +15,7 @@ def run_example(furniture_items):
 
     with src.login_database.login_mongodb_cloud() as client:
         log.info('We are going to use a database called dev')
-        log.info('If it doesnt exist mongodb creates it')
+        log.info('But if it doesnt exist mongodb creates it')
         db = client['dev']
 
         log.info('And in that database use a collection called furniture')
@@ -31,6 +31,7 @@ def run_example(furniture_items):
         results = furniture.find_one(query)
 
         log.info('Print the plastic products')
+        print('Plastic products')
         pprint.pprint(results)
 
         log.info('Delete the blue couch (actually deletes all blue couches)')
@@ -39,18 +40,18 @@ def run_example(furniture_items):
         log.info('Check it is deleted with a query and print')
         query = {'product': 'Blue couch'}
         results = furniture.find_one(query)
+        print('The blue couch is deleted')
         pprint.pprint(results)
 
         log.info(
             'Find multiple documents, iterate though the results and print')
-        log.info('TBD')
 
-        log.info('More sophisticated query examples')
-        log.info('TBD')
+        cursor = furniture.find({'monthly_rental_cost': {'$gte': 15.00}}).sort('monthly_rental_cost', 1)
+        print('Results of search')
+        log.info('Notice how we parse out the data from the document')
 
-        log.info('Using richer document structures')
-        log.info('TBD')
+        for doc in cursor:
+            print(f"Cost: {doc['monthly_rental_cost']} product name: {doc['product']} Stock: {doc['in_stock_quantity']}")
 
         log.info('Delete the collection so we can start over')
         db.drop_collection('furniture')
-
