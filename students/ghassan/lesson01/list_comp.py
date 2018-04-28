c20d2e540b61623386617b852af6eba190e61494
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2018-04-27 21:33:34 -0700

    list comprehensions

---
+++ students/ghassan/lesson01/list_comp.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+
+def top_5ive():
+    music = pd.read_csv("featuresdf.csv")
+    top_five = sorted(
+        [(x, y, i, j) for x, y, i, j in zip(
+            music.artists, music.name, music.danceability, music.loudness
+            ) if i > 0.8 if j < 0.5]
+        )[0:5]
+    return top_five
