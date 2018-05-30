b63759c1dd68d56af5414674a6829ecf72daca94
Andrew Park <apark46.work@gmail.com>
2018-05-29 17:34:57 -0700

    Lesson03|recursive factorial Made the recursive factorial.

---
+++ students/nDruP/lesson03/locke_manager.py
@@ -0,0 +1,46 @@
+from time import sleep
+
+
+class Locke:
+    def __init__(self, capacity=0):
+        self.capacity = capacity
+
+    def __enter__(self):
+        print('Now in enter')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('Now in Exit\n\n')
+        pass
+    
+    def move_boats_through(self, num_boats):
+        if num_boats > self.capacity:
+            raise ValueError('Too many boats for the locke')
+        else:
+            print("Stopping the pumps")
+            sleep(1)
+            print("Opening the doors")
+            sleep(1)
+            print("Letting the boats in")
+            sleep(num_boats/2)
+            print("Closing the doors")
+            sleep(1)
+            print("Restarting the pumps")
+            sleep(1)
+
+        pass
+
+
+if __name__ == '__main__':
+    small_locke = Locke(5)
+    large_locke = Locke(10)
+    boats = 8
+
+    with small_locke as locke:
+        try:
+            locke.move_boats_through(boats)
+        except ValueError:
+            print("Too many boats for the locke")
+
+    with large_locke as locke:
+        locke.move_boats_through(boats)
