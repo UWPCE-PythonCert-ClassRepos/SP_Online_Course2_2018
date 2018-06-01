4c9ff1a324b611b689013abf9ba9c6752c98cdda
johnpharmd <jhumphre@uw.edu>
2018-06-01 16:10:36 -0700

    refactored style

--- students/johnpharmd/lesson01/comprehensions.py
+++ students/johnpharmd/lesson01/comprehensions.py
@@ -3,9 +3,9 @@ import pandas as pd
 # 1st ex.: Using Spotify 2017 top100 track data w/ Pandas + List Comprehensions
 m = pd.read_csv('featuresdf.csv')
 
-print('m.head() returns:')
+print('m.head() returns:\n')
 print(m.head())
-print('\nm.describe() returns:')
+print('\nm.describe() returns:\n')
 print(m.describe(), '\n'*2)
 
 # dance = [x for x in music.danceability if x > 0.8]
@@ -16,11 +16,12 @@ print(m.describe(), '\n'*2)
 # quiet_dance = list(zip(quiet, sorted(dance, reverse=True)))
 # artists_songs = list(zip(artists, songs))
 
-# print('Top 5 tracks by loudness < -5.0 and sorted descending' +
-#       ' by danceability > 0.8:\n')
 # print(list(zip(artists_songs, quiet_dance))[:5])
 
-a = sorted(list(zip(m.danceability, m.artists, m.name, m.loudness)),
+print('Top 5 tracks in descending order by danceability > 0.8' +
+      ' and loudness < -5.0:\n')
+
+a = sorted(list(zip(m.danceability, m.name, m.artists, m.loudness)),
            reverse=True)
 b = [track for track in a if track[3] < -5.0]
 
