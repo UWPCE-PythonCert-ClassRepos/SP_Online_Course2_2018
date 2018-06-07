dcdd3a27be5f9938d8891fd1ccc9b1779cec5a63
Ross Martin <stephen.ross.martin@gmail.com>
2018-06-06 21:16:07 -0700

    updated test for factorial method

--- students/rmart300/lesson03_context_manager/recursion.py
+++ students/rmart300/lesson03_context_manager/recursion.py
@@ -1,14 +1,12 @@
-
-
 def factorial(n):
 
     product = n
     if n > 1:
-        product *= factorial(n-1)
-
+        product *=  factorial(n-1)
+        
     return product
 
 if __name__ == '__main__':
 
     for i in range(1,11):
-        print(f"{i}! = {factorial(3)}")
+        print(f"{i}! = {factorial(i)}")
