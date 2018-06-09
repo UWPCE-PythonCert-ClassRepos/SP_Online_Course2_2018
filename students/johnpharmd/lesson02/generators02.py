ad5e1a0090da3ae023ef1a973dc9ced523a72e8d
johnpharmd <jhumphre@uw.edu>
2018-06-09 09:57:30 -0700

    removed generator function, kept generator expression

--- students/johnpharmd/lesson02/generators02.py
+++ students/johnpharmd/lesson02/generators02.py
@@ -1,19 +1,18 @@
 import pandas as pd
 
 # Again uses Spotify 2017 top100 track data and Pandas, but with generator
-# expression and generator function instead of list comprehension this time
+# expression instead of list comprehension this time
 
 m = pd.read_csv('featuresdf.csv')
 
-
-def find_tracks():
-    """
-    returns all Ed Sheeran tracks, one by one
-    """
-    # generator expressions are lazy (memory-saving), compared to list
-    # comprehensions, which are eager
-    trackgen = (track for track in zip(m.name, m.artists)
+trackgen = (track for track in zip(m.name, m.artists)
                 if track[1] == 'Ed Sheeran')
-    while True:
-        for track in trackgen:
-            yield track
+
+for track in trackgen:
+    print(track)
+
+# returns
+# ('Shape of You', 'Ed Sheeran')
+# ('Castle on the Hill', 'Ed Sheeran')
+# ('Galway Girl', 'Ed Sheeran')
+# ('Perfect', 'Ed Sheeran')
