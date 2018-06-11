8f345c19be004ab1c40b1943b8d9b80fb9757e1e
csdotson <csdotson1@gmail.com>
2018-06-11 08:12:28 -0700

    Add logic for sum_of_integers

--- students/csdotson/lesson01/generator_solution.py
+++ students/csdotson/lesson01/generator_solution.py
@@ -1,3 +1,23 @@
 #!/usr/bin/env python
 """ A series of simple generators """
 
+def sum_of_integers(a=0):
+    count = a
+    while True:
+        yield(a)
+        count += 1
+        a += count
+
+
+def doubler():
+    pass
+
+
+def fib_sequence():
+    pass 
+
+
+def prime_numbers():
+    pass 
+
+
