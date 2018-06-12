3f27c5b04aacd376f8937a62462bcd89af34f601
Wesley Moy <wesmoy@uw.edu>
2018-06-11 19:05:07 -0500

    Resubmitting generator.py

--- students/WesleyM/Lesson02/generator.py
+++ students/WesleyM/Lesson02/generator.py
@@ -3,11 +3,6 @@ music = pd.read_csv("featuresdf.csv")
 
 result = sorted([x for x in
                  zip(music.artists, music.name) if x[0] == 'Ed Sheeran'])
-
-def track_generator(music_data):
-    for i in music_data:
-        yield i
-
-ed_sheeran = track_generator(result)
-print(next(ed_sheeran))
+for i in result:
+    print(i[0] +' - '+ i[1])
     
