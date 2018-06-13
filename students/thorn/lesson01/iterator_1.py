4d2a92e938e7f58328a8373f802d66234f881d7e
Thomas Horn <hortw1991@gmail.com>
2018-06-13 15:43:42 -0500

    Started iterator2.

--- students/thorn/lesson01/iterator_1.py
+++ students/thorn/lesson01/iterator_1.py
@@ -1,20 +1,29 @@
 """
-Iterators & Iterables
+Simple iterator examples
 """
 
+
 class IterateMe_1:
+    """
+    About as simple an iterator as you can get:
+
+    returns the sequence of numbers from zero to 4
+    ( like range(4) )
+    """
+
     def __init__(self, stop=5):
-        self.current = 0
+        self.current = -1
         self.stop = stop
+
     def __iter__(self):
         return self
+
     def __next__(self):
+        self.current += 1
         if self.current < self.stop:
-            self.current += 1
             return self.current
         else:
             raise StopIteration
-
 
 """
     Extend (iterator_1.py ) to be more like range() – add three input parameters: iterator_2(start, stop, step=1)
@@ -24,7 +33,6 @@ it = IterateMe_2(2, 20, 2)
 for i in it:
     if i > 10:  break
     print(i)
-
 for i in it:
     print(i)
 
@@ -32,3 +40,15 @@ for i in it:
         make yours match range()
         is range an iterator or an iteratable? 
 """
+
+class IteratorMe_2(IterateMe_1):
+    """ Default step to 1."""
+    def __init__(self, start, stop, step=1):
+        
+
+
+
+if __name__ == "__main__":
+    # print("Testing the iterator")
+    # for i in IterateMe_1():
+    #     print(i)
