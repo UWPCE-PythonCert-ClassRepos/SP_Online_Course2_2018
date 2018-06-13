fcad3242d695d67afe81de77230ff6f7faede891
Thomas Horn <hortw1991@gmail.com>
2018-06-13 15:56:54 -0500

    Completed iterator_1.py

--- students/thorn/lesson01/iterator_1.py
+++ students/thorn/lesson01/iterator_1.py
@@ -25,30 +25,45 @@ class IterateMe_1:
         else:
             raise StopIteration
 
-"""
-    Extend (iterator_1.py ) to be more like range() – add three input parameters: iterator_2(start, stop, step=1)
-    What happens if you break from a loop and try to pick it up again:
 
-it = IterateMe_2(2, 20, 2)
-for i in it:
-    if i > 10:  break
-    print(i)
-for i in it:
-    print(i)
+class IterateMe_2(IterateMe_1):
+    def __init__(self, start, stop=5, step=1):
+        self.start = start
+        self.stop = stop
+        self.step = step
+        self.current = start - step
 
-    Does range() behave the same?
-        make yours match range()
-        is range an iterator or an iteratable? 
-"""
-
-class IteratorMe_2(IterateMe_1):
-    """ Default step to 1."""
-    def __init__(self, start, stop, step=1):
-        
-
+    def __next__(self):
+        """
+        Add the step to current number.  Return current if less than stop number (want 1 before stop).  Raise StopIteration if over stop.
+        """
+        self.current += self.step
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
 
 
 if __name__ == "__main__":
     # print("Testing the iterator")
     # for i in IterateMe_1():
     #     print(i)
+
+    it = IterateMe_2(2, 20, 2)
+    for i in it:
+        if i > 10:
+            break
+        print(i)
+
+    for i in it:
+        print(i)
+
+    zero_thru_8_by_two = IterateMe_2(0, 10, 2)
+    test_list = list(zero_thru_8_by_two)
+    print(test_list)
+    assert test_list == [i for i in range(0, 10, 2)]
+
+    three_thru_eighteen_by_three = IterateMe_2(3, 21, 3)
+    test_list = list(three_thru_eighteen_by_three)
+    print(test_list)
+    assert test_list == [i for i in range(3, 21, 3)]
