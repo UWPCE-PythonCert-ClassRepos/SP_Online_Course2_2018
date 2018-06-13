127531717518f9ac2ca78381de62067d00f2c38a
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-06-13 16:40:46 -0700

    Generator tests passing.

--- students/stuartkershaw/lesson01/test_generator.py
+++ students/stuartkershaw/lesson01/test_generator.py
@@ -21,7 +21,6 @@ def test_intsum():
     assert next(g) == 15
 
 
-'''
 def test_intsum2():
 
     g = gen.intsum2()
@@ -56,8 +55,8 @@ def test_fib():
     assert [next(g) for i in range(9)] == [1, 1, 2, 3, 5, 8, 13, 21, 34]
 
 
+
 def test_prime():
     g = gen.prime()
     for val in [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]:
         assert next(g) == val
-'''
