1c9db5129f38d2cba3fa51c6a02b12eb861efbd7
Thomas Horn <hortw1991@gmail.com>
2018-06-13 16:00:59 -0500

    set up generator_solution.py

---
+++ students/thorn/lesson01/generator_solution.py
@@ -0,0 +1,32 @@
+
+def intsum():
+    """ 
+    Adds the next integer to current.
+
+    0 + 1 + 2 + 3 + 4 + 5 + …
+    --> 0, 1, 3, 6, 10, 15 …..
+    """
+    pass
+
+
+def doubler():
+    """ 
+    Current is double the previous value.
+    --> 1, 2, 4, 8, 16, 32,
+    """
+    pass
+
+
+def fib():
+    """
+    f(n) = f(n-1) + f(n-2)
+    --> 1, 1, 2, 3, 5, 8, 13, 21, 34…
+    """
+    pass
+
+def prime():
+    """
+    Generates prime numbers.
+    2, 3, 5, 7, 11, 13, 17, 19, 23…
+    """
+    pass
