0bd17c457ef4342799489186784ca674a3bf712f
Dennis Lee <dennis271lee@yahoo.com>
2018-06-16 23:59:14 -0700

    [DennisLee][L1][Comprehensions] Implemented danceability song list comprehension.

---
+++ students/DennisLee/lesson01/MusicSelector.py
@@ -0,0 +1,12 @@
+import pandas as pd
+music = pd.read_csv("featuresdf.csv")
+
+ideal_songlist = [(artist, song) for danceable, volume, artist, song in sorted(
+        zip(music.danceability, music.loudness, music.artists, music.name), 
+        reverse=True) if danceable > 0.8 and volume < -5.0]
+
+print("\n\t***Soft danceable song list!***\n")
+print(f"RANK | {'ARTIST':<20} | {'SONG':<40}")
+print(f"---- | {'-'*20} | {'-'*40}")
+for i, j in zip(range(5), ideal_songlist[:5]):
+    print(f'{i+1:>4} | {j[0]:<20} | {j[1]:<40}')
