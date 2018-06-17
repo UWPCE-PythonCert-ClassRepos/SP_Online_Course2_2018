031d98f57c60f9a88859546c9d54f94652c5cc2e
Dennis Lee <dennis271lee@yahoo.com>
2018-06-17 14:25:34 -0700

    [DennisLee][L1][Comprehensions] Implemented intsum, doubler, fibonacci, and prime number generators.

--- students/DennisLee/lesson01/generator_solution.py
+++ students/DennisLee/lesson01/generator_solution.py
@@ -1,14 +1,36 @@
+from math import sqrt
+
 def intsum():
-    pass
+    result, increment = 0, 1
+    while True:
+        yield result
+        result += increment
+        increment += 1
 
 def intsum2():
-    pass
+    result = 0
+    while True:
+        yield result * (result + 1) / 2
+        result += 1
 
 def doubler():
-    pass
+    result = 1
+    while True:
+        yield result
+        result *= 2
 
 def fib():
-    pass
+    x, y = 0, 1
+    while True:
+        x, y = y, x + y
+        yield x
 
 def prime():
-    pass
+    result = 1
+    while True:
+        result += 1
+        for i in range(int(sqrt(result)), 1, -1):
+            if not result % i:
+                break
+        else:
+            yield result
