d333e8486aafd5110372ab3222c0a7e18ff70bcf
johnpharmd <jhumphre@uw.edu>
2018-06-18 05:15:25 -0700

    added new methods, further refactored __enter__ and __exit__

--- students/johnpharmd/lesson03/locks_context_manager.py
+++ students/johnpharmd/lesson03/locks_context_manager.py
@@ -6,11 +6,33 @@ class Locke(object):
     Creates Locke objects
     """
 
-    def __init__(self, size):
-        self.size = size
+    def __init__(self, boat_capacity):
+        self.boat_capacity = boat_capacity
 
     def __enter__(self):
-        return self
+        print('Preparing to allow entry into locke.\n')
+        typical_operation()
+        print('\nBoats entering locke.')
 
     def __exit__(self, *args):
-        pass
+        print('Preparing locke for exiting.\n')
+        typical_operation()
+        print('\nBoats exiting locke.')
+
+    @staticmethod
+    def typical_operation():
+        op_text = """
+        Stopping the pumps.
+        Opening the doors.
+        Closing the doors.
+        Restarting the pumps.
+        """
+        print(op_text)
+
+    def move_boats_through(boats):
+        try:
+            assert boats <= self.boat_capacity
+            self.__enter__()
+            self.__exit__()
+        except AssertionError:
+            print('Number of boats exceeds locke\'s capacity.')
