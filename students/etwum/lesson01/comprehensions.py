7b284e62bb1bc1a041e04c67780552dc91d764db
etwum <33198751+etwum@users.noreply.github.com>
2018-06-18 19:01:26 -0700

    update
    sort the list just for the top 5

--- students/etwum/lesson01/comprehensions.py
+++ students/etwum/lesson01/comprehensions.py
@@ -5,7 +5,7 @@ music = pd.read_csv("featuresdf.csv")
 
 songs = [(a,b,c,d) for (a,b,c,d) in zip(music.artists, music.name, music.danceability, music.loudness) if c > 0.8 and d > -5.0]
 
-sorted_list = sorted(songs, key=lambda x: x[2], reverse=True)
-print(sorted_list)
+sorted_list_top5 = sorted(songs, key=lambda x: x[2], reverse=True)[0:5]
+print(sorted_list_top5)
 
 
