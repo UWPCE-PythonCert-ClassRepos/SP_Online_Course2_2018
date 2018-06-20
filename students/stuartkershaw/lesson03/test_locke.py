fd32714d6c2edff676c9c1e60b8f7e2a9a0c18a1
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-06-20 10:04:25 -0700

    Basic tests passing.

--- students/stuartkershaw/lesson03/test_locke.py
+++ students/stuartkershaw/lesson03/test_locke.py
@@ -11,3 +11,13 @@ def test_locke_init():
 
     locke = Locke(5)
     assert locke.limit == 5
+
+
+def test_small_locke():
+    small_locke = Locke(5)
+    assert small_locke.size == "SMALL LOCKE"
+
+
+def test_large_locke():
+    small_locke = Locke(10)
+    assert small_locke.size == "LARGE LOCKE"
