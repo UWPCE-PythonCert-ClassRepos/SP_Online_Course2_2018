93ed037142908f8ec9f51fa0ae01aedb6c4a12c3
etwum <33198751+etwum@users.noreply.github.com>
2018-06-20 18:03:09 -0700

    working on iterator

--- students/etwum/lesson01/iterator_1.py
+++ students/etwum/lesson01/iterator_1.py
@@ -23,8 +23,36 @@ class IterateMe_1:
             raise StopIteration
 
 
+class IterateMe_2(IterateMe_1):
+
+    def __init__(self, start, stop, step=1):
+        self.current = -1
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current += 1
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
+
+
 if __name__ == "__main__":
 
     print("Testing the iterator")
-    for i in IterateMe_1():
+    it = IterateMe_2(2, 20, 2)
+
+    # prints only 0-10
+    for i in it:
+        if i > 10:
+            break
+        print(i)
+
+    # skips breakpoint of 11 and starts print at 12
+    for i in it:
         print(i)
