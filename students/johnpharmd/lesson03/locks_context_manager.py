eac7a507e45e1ffc1eb8b5a9fd73eb1747cb31c7
johnpharmd <jhumphre@uw.edu>
2018-06-24 05:38:02 -0700

    completed initial iteration (functional)

--- students/johnpharmd/lesson03/locks_context_manager.py
+++ students/johnpharmd/lesson03/locks_context_manager.py
@@ -21,20 +21,19 @@ class Locke(object):
         print(op_text)
 
     def __enter__(self):
-        print('Preparing to allow entry into locke.\n')
-        # self.typical_operation()
-        print('\nBoats entering locke.')
+        return self
 
     def __exit__(self, *args):
-        print('Preparing locke for exiting.\n')
-        # self.typical_operation()
-        print('\nBoats exiting locke.')
+        print('\nLocke operation halted.')
 
     def move_boats_through(self, boats):
         try:
             assert boats <= self.boat_capacity
-            self.__enter__()
+            print('Preparing to allow entry into locke.\n')
             self.typical_operation()
-            self.__exit__()
+            print('\nBoats entering locke.')
+            print('Preparing locke for exiting.\n')
+            self.typical_operation()
+            print('\nBoats have exited locke.')
         except AssertionError:
             print('Number of boats exceeds locke\'s capacity.')
