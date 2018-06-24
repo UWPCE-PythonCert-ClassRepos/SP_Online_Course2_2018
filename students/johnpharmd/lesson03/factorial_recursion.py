ab95751a4c34e7b010671446fac5b2cea53036f0
johnpharmd <jhumphre@uw.edu>
2018-06-24 13:02:14 -0700

    completed initial iteration (functional)

--- students/johnpharmd/lesson03/factorial_recursion.py
+++ students/johnpharmd/lesson03/factorial_recursion.py
@@ -2,7 +2,7 @@
 
 def fac_recur(num=1):
     num = list(range(1, num + 1))
-    if num:
-        return num.pop(-1) * fac_recur(len(num))
-    elif None:
-        return []
+    nlist = [1]
+    if len(num) > 0:
+        return nlist[0] * num.pop(-1) * fac_recur(len(num))
+    return nlist[0]
