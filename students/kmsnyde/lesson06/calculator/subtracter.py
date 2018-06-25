9525d95c0ba90b6d5ba63f2272c0ba37ff882533
kmsnyde <kmsnyder2@verizon.net>
2018-06-25 18:53:13 -0400

    lesson06 part1 - calculator

---
+++ students/kmsnyde/lesson06/calculator/subtracter.py
@@ -0,0 +1,12 @@
+"""
+This module provides a subtraction operator
+"""
+
+
+class Subtracter(object):
+    """perform subtraction"""
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """subtraction"""
+        return operand_1 - operand_2
---
+++ students/kmsnyde/lesson06/squarer.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 22 13:54:44 2018
+
+@author: Karl M. Snyder
+"""
+
+class Squarer():
+    
+    @staticmethod
+    def calc(operand):
+        # return operand * operand #old
+        #return operand ** operand #wrong
+        return operand * operand #old
+        #return operand ** 2
+    
