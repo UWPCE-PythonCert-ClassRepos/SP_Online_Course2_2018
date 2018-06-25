6d4a24b36561b86fa31f25b0130f0fca160557d5
Thomas Horn <hortw1991@gmail.com>
2018-06-25 16:40:38 -0500

    Closures exercise setup.

---
+++ students/thorn/lesson02/closure.py
@@ -0,0 +1,9 @@
+"""
+Write a closure to capture high energy tracks (over 8.0).
+
+Return tracks, artist name, track name and energy value.
+"""
+
+import pandas as pd
+music = pd.read_csv("featuresdf.csv")
+
