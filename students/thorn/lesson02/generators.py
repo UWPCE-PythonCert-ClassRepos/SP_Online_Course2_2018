cf565924af0dc610da2cd9076938c9bc7789facf
Thomas Horn <hortw1991@gmail.com>
2018-06-25 16:38:59 -0500

    Finished generators returning post malone songs

--- students/thorn/lesson02/generators.py
+++ students/thorn/lesson02/generators.py
@@ -1,9 +1,16 @@
 """
 Generators
+
+Write a generator to find and print all of your favorite artist’s tracks from the data set - Post Malone in this case.
 """
 
-"""
-Write a generator to find and print all of your favorite artist’s tracks from the data set. 
-    - Ed Sheeran if I don't like anyone
-"""
+import pandas as pd
+music = pd.read_csv("featuresdf.csv")
 
+# Get post songs
+post_songs = [info for info in zip(music.artists, music.name) if info[0] == "Post Malone"]
+
+# Print post songs
+print("Post Malone Songs:")
+for item in post_songs:
+    print(item[1])
