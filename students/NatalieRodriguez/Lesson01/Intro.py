bb8b97d20e915e383a20819e836e09cfb85f5d4b
Natalie Rodriguez <natrod8@outlook.com>
2018-06-25 21:39:16 -0500

    commits

--- students/NatalieRodriguez/Lesson01/Intro.py
+++
@@ -1,6 +0,0 @@
-test push
-
-I'm Natalie Rodriguez and I am a GIS Analyst for the City of Lawrence. 
-I am excited to continue learning Python to help automate mapping and other
-database tasks in ArcGIS. I also enjoy making other small programs for my own personal use. 
-
--- students/NatalieRodriguez/Lesson01/ReadMe.rst
+++
@@ -1,5 +0,0 @@
-Test Push
-
-I am Natalie Rodriguez and I am a GIS Analyst for the City of Lawrence, KS.
-I am very excited to learn Python in order to expedite my work flow in ArcGIS. 
-Also, I really enjoy coding!
