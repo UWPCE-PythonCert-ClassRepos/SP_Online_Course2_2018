e062a2b1a4f0abfb9b6d3dbdf741253211bf31d8
Dennis Lee <dennis271lee@yahoo.com>
2018-06-29 10:58:36 -0700

    [DennisLee][L3][Context Managers] Implemented recursion to handle scenarios when boat count exceeds locke capacity. Ready for submission.

--- students/DennisLee/lesson03/locke.py
+++ students/DennisLee/lesson03/locke.py
@@ -5,7 +5,7 @@ from contextlib import contextmanager
 
 def pump_handler(func):
     def wrapper(*args, **kwargs):
-        print("Stopping the pumps.")
+        print("\nStopping the pumps.")
         result = func(*args, **kwargs)
         print("Restarting the pumps.")
         return result
@@ -22,38 +22,48 @@ def door_handler(func):
 class Locke:
     def __init__(self, capacity):
         self.capacity = capacity
+        self.queue = 0
     
     def __enter__(self):
-        self.locke_function_process()
+        self.locke_function_process(True)
         return self
 
     def __exit__(self, e_type, e_value, e_traceback):
         if e_type == ValueError:
             print(e_value)
-        return self.locke_function_process()
+        self.locke_function_process(False)
+        if self.queue > 0:
+            with Locke(self.capacity) as locke:
+                locke.move_boats_through(self.queue)
+        return True
 
     @pump_handler
     @door_handler
-    def locke_function_process(self):
-        return True
+    def locke_function_process(self, entering=True):
+        if entering:
+            print("Boats entering locke.")
+        else:
+            print("Boats exiting locke.")
 
     def move_boats_through(self, num_boats):
-        if num_boats > self.capacity:
-            raise ValueError(f"Aborting - number of boats passing through "
-                    f"({num_boats}) exceeds locke capacity ({self.capacity}).")
+        self.queue = max(0, num_boats - self.capacity)
+        if self.queue > 0:
+            raise ValueError(f"Number of boats trying to pass ({num_boats}) "
+                    f"exceeds locke capacity ({self.capacity}). {self.queue} "
+                    f"boat(s) are waiting for next passage cycle.")
         else:
-            print(f"{num_boats} boats passing through the locke.")
+            print(f"{num_boats} boat(s) passing through the locke.")
 
 
 if __name__ == "__main__":
     small_locke = Locke(5)
     large_locke = Locke(10)
-    boats = 8
 
-    print("\n\n\t *** Small locke - 5-boat capacity: *** \n")
-    with small_locke as locke:
-        locke.move_boats_through(boats)
+    for boats in range(2, 27, 6):
+        print(f"\n\n\t ***Small locke, {boats} boat(s) attempting passage:***")
+        with small_locke as locke:
+            locke.move_boats_through(boats)
 
-    print("\n\n\t *** Large locke - 10-boat capacity: *** \n")
-    with large_locke as locke:
-        locke.move_boats_through(boats)
+        print(f"\n\n\t ***Large locke, {boats} boat(s) attempting passage:***")
+        with large_locke as locke:
+            locke.move_boats_through(boats)
