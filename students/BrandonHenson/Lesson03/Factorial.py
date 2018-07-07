fca1ea82d08089ffddd4b4a09cdd38024913068b
BrandonHenson <bigbrandonh@gmail.com>
2018-07-06 20:24:47 -0700

    I forgot to check online pep8 for factorial. Fixed it

--- students/BrandonHenson/Lesson03/Factorial.py
+++ students/BrandonHenson/Lesson03/Factorial.py
@@ -4,13 +4,14 @@
 # 7-6-18
 # !/usr/bin/env python3
 
+
 def factorial(number):
     if number > 1:
         return number * factorial(number - 1)
     else:
         return 1
-    
+
 if __name__ == '__main__':
     factorial(5)
-    for i in range(0,20):
-        print(i, "=" , factorial(i))
+    for i in range(0, 20):
+        print(i, "=", factorial(i))
