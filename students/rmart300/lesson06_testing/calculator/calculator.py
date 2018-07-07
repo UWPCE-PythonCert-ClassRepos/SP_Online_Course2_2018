c87082526d8a9f11f5a9249c85ed642313eb76d2
rmart300 <stephen.ross.martin@gmail.com>
2018-07-07 10:46:38 -0700

    modifications to satisfy pylint and flake8

--- students/rmart300/lesson06_testing/calculator/calculator.py
+++ students/rmart300/lesson06_testing/calculator/calculator.py
@@ -1,8 +1,16 @@
+"""
+module for calculator
+"""
+
 from .exceptions import InsufficientOperands
 
+
 class Calculator(object):
+    """ class for calculator """
 
     def __init__(self, adder, subtracter, multiplier, divider):
+        """ initializes operators for calculator and stack for operands """
+
         self.adder = adder
         self.subtracter = subtracter
         self.multiplier = multiplier
@@ -11,9 +19,13 @@ class Calculator(object):
         self.stack = []
 
     def enter_number(self, number):
+        """ appends number to operand stack """
+
         self.stack.append(number)
 
     def _do_calc(self, operator):
+        """ performs generalized calc method for different operators """
+
         try:
             result = operator.calc(self.stack[0], self.stack[1])
         except IndexError:
@@ -23,13 +35,21 @@ class Calculator(object):
         return result
 
     def add(self):
+        """ performs add operation """
+
         return self._do_calc(self.adder)
 
     def subtract(self):
+        """ performs subtract operation """
+
         return self._do_calc(self.subtracter)
 
     def multiply(self):
+        """ performs multiply operation """
+
         return self._do_calc(self.multiplier)
 
     def divide(self):
+        """ performs division operation """
+
         return self._do_calc(self.divider)
---
+++ students/rmart300/lesson06_testing/calculator/multiplier.py
@@ -0,0 +1,13 @@
+"""
+this module provides a multiplier operator
+"""
+
+
+class Multiplier(object):
+    """ this class multiplies two operands """
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """ multiplies one operand to another """
+
+        return operand_1*operand_2
