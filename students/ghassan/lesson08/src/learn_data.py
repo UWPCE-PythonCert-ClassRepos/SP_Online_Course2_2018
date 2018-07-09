63babad3707dfbe38a5e0408edcce7638361f743
Ghassan Al Mamar <Ghassan.Al.Mamar@oracle.com>
2018-07-08 20:24:38 -0700

    Lesson8 - Assignment1

---
+++ students/ghassan/lesson08/src/learn_data.py
@@ -0,0 +1,87 @@
+"""
+    Data for database demonstrations
+"""
+
+
+def get_furniture_data():
+    """
+    demonstration data
+    """
+
+    furniture_data = [
+        {
+            'product': 'Red couch',
+            'description': 'Leather low back',
+            'monthly_rental_cost': 12.99,
+            'in_stock_quantity': 10
+        },
+        {
+            'product': 'Blue couch',
+            'description': 'Cloth high back',
+            'monthly_rental_cost': 9.99,
+            'in_stock_quantity': 3
+        },
+        {
+            'product': 'Coffee table',
+            'description': 'Plastic',
+            'monthly_rental_cost': 2.50,
+            'in_stock_quantity': 25
+        },
+        {
+            'product': 'Red couch',
+            'description': 'Leather high back',
+            'monthly_rental_cost': 15.99,
+            'in_stock_quantity': 17
+        },
+        {
+            'product': 'Blue recliner',
+            'description': 'Leather high back',
+            'monthly_rental_cost': 19.99,
+            'in_stock_quantity': 6
+        },
+        {
+            'product': 'Chair',
+            'description': 'Plastic',
+            'monthly_rental_cost': 1.00,
+            'in_stock_quantity': 45
+        }
+    ]
+    return furniture_data
+
+
+# Part1 of assignment1
+def mongo_furniture_data():
+    """
+    Adding new furniture data
+    :return: list
+    """
+    furniture_data = [
+        {
+            'product': {
+                'type': 'couch',
+                'color': 'red'
+            },
+            'description': 'Leather low back',
+            'monthly_rental_cost': 12.99,
+            'in_stock_quantity': 10
+        },
+        {
+            'product': {
+                'type': 'couch',
+                'color': 'blue'
+            },
+            'description': 'Cloth high back',
+            'monthly_rental_cost': 9.99,
+            'in_stock_quantity': 3
+        },
+        {
+            'product': {
+                'type': 'coffee table',
+                'color': 'wood'
+            },
+            'description': 'Plastic',
+            'monthly_rental_cost': 2.50,
+            'in_stock_quantity': 25
+        }
+    ]
+    return furniture_data
