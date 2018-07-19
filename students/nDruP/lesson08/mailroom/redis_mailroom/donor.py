9447ecd72a590233c1473b31ff83040604146aa5
Andrew Park <apark46.work@gmail.com>
2018-07-18 20:13:00 -0700

    Lesson08|RedisMailroom noticed a problem with donor projections and fixed it. clientside mailroom is now fully functional.

--- students/nDruP/lesson08/mailroom/redis_mailroom/donor.py
+++ students/nDruP/lesson08/mailroom/redis_mailroom/donor.py
@@ -23,6 +23,14 @@ class Donor():
     def info(self):
         return (self.name, len(self.history), self.avg_gift, self.sum_gift)
 
+    @property
+    def col_len(self):
+        name_col = max(len("Donor Name"), len(self.name))
+        hist_col = max(len("Gifts"), len(str(self.info[1])))
+        avg_col = max(len("Avg Gift"), len(str(self.avg_gift)))
+        sum_col = max(len("Total Given"), len(str(self.sum_gift)))
+        return [name_col, hist_col, avg_col, sum_col]
+
     def add_gift(self, value):
         self.history.append(round(value, 2))
 
@@ -31,13 +39,16 @@ class Donor():
             min_gift = min(self.history)
         if max_gift is None:
             max_gift = max(self.history)
-        filt_hist = list(filter(lambda x: x >= min_gift and x <= max_gift,
-                                self.history))
-        remain_hist = list(filter(lambda x: x < min_gift or x > max_gift,
-                                  self.history))
-        filt_hist = list(map(lambda x: x*factor, filt_hist))
-        return Donor("*"+self.name, filt_hist+remain_hist)
-
+        if len(self.history) > 1:
+            filt_hist = list(filter(lambda x: x >= min_gift and x <= max_gift,
+                                    self.history))
+            remain_hist = list(filter(lambda x: x < min_gift or x > max_gift,
+                                      self.history))
+            filt_hist = list(map(lambda x: x*factor, filt_hist))
+            return Donor("*"+self.name, filt_hist+remain_hist)
+        else:
+            return Donor('*'+self.name, [factor*x for x in self.history])
+    
     def thank_u_letter_str(self, new_gift=False):
         divider = "\n" + "*" * 50 + "\n"
         thank_u_letter = divider + f"Dearest {self.name},\n"
@@ -55,3 +66,18 @@ class Donor():
                            f"We're a Pyramid Scheme & so is {self.name}" +
                            divider)
         return thank_u_letter
+
+    def donor_report_row(self, header=0):
+        donor_col = "{:<" + f"{self.col_len[0]}" + "}"
+        hist_col = "{:>" + f"{self.col_len[1]}" + "}"
+        avg_col = "{:>" + f"{self.col_len[2]}" + ((".2f")*(not header)) + "}"
+        sum_col = "{:>" + f"{self.col_len[3]}" + ((".2f")*(not header)) + "}"
+        return (donor_col+"\t"+hist_col+"\t"+avg_col+"\t"+sum_col)
+
+    def donor_report(self):
+        col_name_list = ["Donor Name", "Gifts", "Avg Gift", "Total Given"]
+        report = self.donor_report_row(1).format(*col_name_list)
+        row = self.donor_report_row()
+        row_data = self.info
+        report += "\n" + row.format(*row_data)
+        return report
