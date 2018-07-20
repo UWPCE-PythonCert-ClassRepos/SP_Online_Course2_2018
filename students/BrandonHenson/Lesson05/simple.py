5a835ef8afaaf1746557d8a8423e41d86352139b
Brandon Henson <Bigbrandonh@gmail.com>
2018-07-20 07:36:57 -0700

    Added files to start the logging portion.

---
+++ students/BrandonHenson/Lesson05/simple.py
@@ -0,0 +1,32 @@
+#simple.py
+import logging
+
+format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
+
+formatter = logging.Formatter(format)
+
+file_handler = logging.FileHandler('mylog.log')
+file_handler.setLevel(logging.WARNING)           
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()        
+console_handler.setLevel(logging.DEBUG)          
+console_handler.setFormatter(formatter)          
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)                   
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)               
+
+def my_fun(n):
+    for i in range(0, n):
+        logging.debug(i)
+        if i == 50:
+            logging.warning("The value of i is 50.")
+        try:
+            i / (50 - i)
+        except ZeroDivisionError:
+            logging.error("Tried to divide by zero. Var i was {}. Recovered gracefully.".format(i))
+
+if __name__ == "__main__":
+    my_fun(100)
