617890264659af24cde525df87f475028ce40ea8
kmsnyde <kmsnyder2@verizon.net>
2018-07-27 16:49:27 -0400

    lesson08 PART 2 - nosql mailroom

--- students/kmsnyde/lesson08/nosql_repo/src/login_database.py
+++ students/kmsnyde/lesson08/nosql_repo/src/login_database.py
@@ -3,18 +3,15 @@
 """
 
 import configparser
-from pathlib import Path
+import utilities
 import pymongo
 import redis
 from neo4j.v1 import GraphDatabase, basic_auth
-import urllib
-import dns
+from pathlib import Path
 
-import utilities
 
 log = utilities.configure_logger('default', '../logs/login_databases_dev.log')
-#config_file = Path(__file__).parent.parent / '.config/config.ini'
-config_file = Path('z:/uofw/Python220/nosql_repo/.config/config1.ini')
+config_file = Path('z:/uofw/repo/SP_Online_Course2_2018/students/kmsnyde/lesson08/nosql_repo/.config/config.ini')
 config = configparser.ConfigParser()
 
 
@@ -43,7 +40,7 @@ def login_redis_cloud():
         connect to redis and login
     """
     try:
-        
+        #log.info('Trying to log in to redis_cloud...')
         config.read(config_file)
         host = config["redis_cloud"]["host"]
         port = config["redis_cloud"]["port"]
@@ -57,11 +54,12 @@ def login_redis_cloud():
     log.info('Here is where we use the connect to redis.')
 
     try:
-        
+        #log.info('passing host, port, pw')
         r = redis.Redis(host=host, port=port, password=pw)
         
 
     except Exception as e:
+        log.info('There is an error...')
         print(f'error: {e}')
 
     return r
@@ -73,20 +71,20 @@ def login_neo4j_cloud():
 
     """
 
-    log.info('Here is where we use the connect to neo4j.')
-    log.info('')
+    #log.info('Here is where we use the connect to neo4j.')
+    #log.info('')
 
     config.read(config_file)
-    log.info('Read the config_file')
+    #log.info('Read the config_file')
 
     graphenedb_user = config["neo4j_cloud"]["user"]
-    log.info('Read the user name')
+    #log.info('Read the user name')
     graphenedb_pass = config["neo4j_cloud"]["pw"]
-    log.info('Read the pw')
+    #log.info('Read the pw')
     graphenedb_url = 'bolt://hobby-bnehfidmdoangbkeoomlekbl.dbs.graphenedb.com:24786'
     driver = GraphDatabase.driver(graphenedb_url, 
             auth=basic_auth(graphenedb_user, graphenedb_pass))
-
+    #log.info('Return the driver to the caller...')
     return driver
 
 #if __name__ == '__main__':
