429d761cda93adc18a7b1f69f52b2c5e538aae3b
etwum <33198751+etwum@users.noreply.github.com>
2018-07-28 13:12:23 -0700

    starting water regulation program

---
+++ students/etwum/lesson06/water-regulation/waterregulation/integrationtest.py
@@ -0,0 +1,23 @@
+"""
+Module tests for the water-regulation module
+"""
+
+import unittest
+from unittest.mock import MagicMock
+
+from pump import Pump
+from sensor import Sensor
+
+from .controller import Controller
+from .decider import Decider
+
+
+class ModuleTests(unittest.TestCase):
+    """
+    Module tests for the water-regulation module
+    """
+
+    # TODO: write an integration test that combines controller and decider,
+    #       using a MOCKED sensor and pump.
+
+    pass
