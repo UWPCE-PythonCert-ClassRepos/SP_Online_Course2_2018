6c437f00e941f5de347a74095e8e4f3e25d87df5
Dustin L <r.dustin.lane@gmail.com>
2018-07-29 10:06:30 -0700

    Lesson08 - Mongodb updates

--- students/Dustin_L/Lesson08/src/learn_data.py
+++ students/Dustin_L/Lesson08/src/learn_data.py
@@ -10,40 +10,52 @@ def get_furniture_data():
 
     furniture_data = [
         {
-            'product': 'Red couch',
+            'product type': 'Couch',
+            'product color': 'Red',
             'description': 'Leather low back',
             'monthly_rental_cost': 12.99,
             'in_stock_quantity': 10
         },
         {
-            'product': 'Blue couch',
+            'product type': 'Couch',
+            'product color': 'Blue',
             'description': 'Cloth high back',
             'monthly_rental_cost': 9.99,
             'in_stock_quantity': 3
         },
         {
-            'product': 'Coffee table',
+            'product type': 'Coffee table',
+            'product color': 'Green',
             'description': 'Plastic',
             'monthly_rental_cost': 2.50,
             'in_stock_quantity': 25
         },
         {
-            'product': 'Red couch',
+            'product type': 'Couch',
+            'product color': 'Red',
             'description': 'Leather high back',
             'monthly_rental_cost': 15.99,
             'in_stock_quantity': 17
         },
         {
-            'product': 'Blue recliner',
+            'product type': 'Recliner',
+            'product color': 'Blue',
             'description': 'Leather high back',
             'monthly_rental_cost': 19.99,
             'in_stock_quantity': 6
         },
         {
-            'product': 'Chair',
+            'product type': 'Chair',
+            'product color': 'Orange',
             'description': 'Plastic',
             'monthly_rental_cost': 1.00,
             'in_stock_quantity': 45
+        },
+        {
+            'product type': 'Rolltop desk',
+            'product color': 'Oak',
+            'monthly_rental_cost': 50.00,
+            'in_stock_quantity': 7
         }
     ]
     return furniture_data
--- students/Dustin_L/Lesson08/src/mongodb_script.py
+++ students/Dustin_L/Lesson08/src/mongodb_script.py
@@ -27,32 +27,44 @@ def run_example(furniture_items):
         log.info('Step 2: Now we add data from the dictionary above')
         furniture.insert_many(furniture_items)
 
-        log.info('Step 3: Find the products that are described as plastic')
-        query = {'description': 'Plastic'}
-        results = furniture.find_one(query)
+        log.info('Step 3: Retrieve all red items')
+        query = {'product color': 'Red'}
+        results = furniture.find(query)
+        for item in results:
+            pprint.pprint(item)
 
-        log.info('Step 4: Print the plastic products')
-        print('Plastic products')
-        pprint.pprint(results)
+        log.info('Step 4: Retrieve all red couches')
+        query = {'product type': 'Couch', 'product color': 'Red'}
+        results = furniture.find(query)
+        for item in results:
+            pprint.pprint(item)
 
-        log.info('Step 5: Delete the blue couch (actually deletes all blue couches)')
-        furniture.remove({"product": {"$eq": "Blue couch"}})
+        # log.info('Step 3: Find the products that are described as plastic')
+        # query = {'description': 'Plastic'}
+        # results = furniture.find_one(query)
 
-        log.info('Step 6: Check it is deleted with a query and print')
-        query = {'product': 'Blue couch'}
-        results = furniture.find_one(query)
-        print('The blue couch is deleted, print should show none:')
-        pprint.pprint(results)
+        # log.info('Step 4: Print the plastic products')
+        # print('Plastic products')
+        # pprint.pprint(results)
 
-        log.info(
-            'Step 7: Find multiple documents, iterate though the results and print')
+        # log.info('Step 5: Delete the blue couch (actually deletes all blue couches)')
+        # furniture.remove({"product": {"$eq": "Blue couch"}})
 
-        cursor = furniture.find({'monthly_rental_cost': {'$gte': 15.00}}).sort('monthly_rental_cost', 1)
-        print('Results of search')
-        log.info('Notice how we parse out the data from the document')
+        # log.info('Step 6: Check it is deleted with a query and print')
+        # query = {'product': 'Blue couch'}
+        # results = furniture.find_one(query)
+        # print('The blue couch is deleted, print should show none:')
+        # pprint.pprint(results)
 
-        for doc in cursor:
-            print(f"Cost: {doc['monthly_rental_cost']} product name: {doc['product']} Stock: {doc['in_stock_quantity']}")
+        # log.info(
+        #     'Step 7: Find multiple documents, iterate though the results and print')
+
+        # cursor = furniture.find({'monthly_rental_cost': {'$gte': 15.00}}).sort('monthly_rental_cost', 1)
+        # print('Results of search')
+        # log.info('Notice how we parse out the data from the document')
+
+        # for doc in cursor:
+        #     print(f"Cost: {doc['monthly_rental_cost']} product name: {doc['product']} Stock: {doc['in_stock_quantity']}")
 
         log.info('Step 8: Delete the collection so we can start over')
         db.drop_collection('furniture')
