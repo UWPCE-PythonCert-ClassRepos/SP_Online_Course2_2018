9652d24c224dfacf2b47117f38707a744d8a9b4d
Jerry.H <doridoriae86@gmail.com>
2018-08-01 16:20:58 -0700

    Lesson6 - fix pylint

--- students/JerryH/Lesson06/calculator/calculator.py
+++ students/JerryH/Lesson06/calculator/calculator.py
@@ -1,9 +1,19 @@
+"""
+Calculator module
+"""
+
 from .exceptions import InsufficientOperands
 
 
 class Calculator():
+    """
+    Create the calculator object.
+    """
 
     def __init__(self, adder, subtracter, multiplier, divider):
+        """
+        Initialize the calculator
+        """
         self.adder = adder
         self.subtracter = subtracter
         self.multiplier = multiplier
@@ -12,10 +22,16 @@ class Calculator():
         self.stack = []
 
     def enter_number(self, number):
+        """
+        Insert the input to the front of self.stack
+        """
         # self.stack.insert(0, number)
         self.stack.append(number)
 
     def _do_calc(self, operator):
+        """
+        Return result of the operation of the first 2 elements of self.stack
+        """
         try:
             result = operator.calc(self.stack[0], self.stack[1])
         except IndexError:
@@ -27,13 +43,25 @@ class Calculator():
         return result
 
     def add(self):
+        """
+        Return the sum of the first 2 elements of self.stack
+        """
         return self._do_calc(self.adder)
 
     def subtract(self):
+        """
+        Return the difference of the first 2 elements of self.stack
+        """
         return self._do_calc(self.subtracter)
 
     def multiply(self):
+        """
+        Return the product of the first 2 elements of self.stack
+        """
         return self._do_calc(self.multiplier)
 
     def divide(self):
+        """
+        Return the quotient of the first 2 elements of self.stack
+        """
         return self._do_calc(self.divider)
