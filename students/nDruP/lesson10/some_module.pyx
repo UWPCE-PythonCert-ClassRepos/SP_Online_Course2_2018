4a61ae9fadc21c56ed2d6c877cf99a602ff9fbf1
Andrew Park <apark46.work@gmail.com>
2018-08-05 23:21:49 -0700

    Lesson10|Cython Timing Still haven't added any cython specific coding. Gonna try using cdef and whatnot on some of the variables.

--- students/nDruP/lesson10/some_module.pyx
+++ students/nDruP/lesson10/some_module.pyx
@@ -1,12 +1,34 @@
+# cython: profile=True
+
 def fibonacci(n):
     if n < 2:
        return 1
     return fibonacci(n-1)+fibonacci(n-2)
 
+def alt_fibonacci(n):
+    two_fibo = [1,1]
+    for i in range(n-2):
+        two_fibo[1] += two_fibo[0]
+        two_fibo[0] = two_fibo[1] - two_fibo[0]
+    return two_fibo[1]	
+    
+
 def hewwo():
-    print('H-hewwo world?!')
+    return 'H-hewwo world?!'
 
 def factorial(n):
     if n <= 1:
        return 1
     return n * factorial(n-1)
+
+def run_fact():
+    for i in range(25):
+        factorial(i)
+
+def run_fibo():
+    for i in range(15):
+        fibonacci(i)
+
+def alt_run_fibo():
+    for i in range(15):
+        alt_fibonacci(i)
--- students/nDruP/lesson10/use_some_module.py
+++ students/nDruP/lesson10/use_some_module.py
@@ -1,1 +1,10 @@
 from some_module import *
+from timeit import timeit as timer
+
+print(hewwo())
+
+print(timer(lambda: run_fact()))
+
+print(timer(lambda: run_fibo()))
+
+print(timer(lambda: alt_run_fibo()))
