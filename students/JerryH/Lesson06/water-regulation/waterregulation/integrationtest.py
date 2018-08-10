3a1e785e530843f3d378360082dd5c2d4b869c50
Jerry.H <doridoriae86@gmail.com>
2018-08-09 19:34:10 -0700

    Fix integration.py

--- students/JerryH/Lesson06/water-regulation/waterregulation/integrationtest.py
+++ students/JerryH/Lesson06/water-regulation/waterregulation/integrationtest.py
@@ -17,20 +17,21 @@ class ModuleTests(unittest.TestCase):
     Module tests for the water-regulation module
     """
 
-    # write an integration test that combines controller and decider,
-    #       using a MOCKED sensor and pump.
-    pump_address = Pump('125.0.0.1', '8000')
-    sensor_address = Sensor('125.0.0.2', '8000')
-    decider = Decider(100, .1)
-    controller = Controller(sensor_address, pump_address, decider)
-    controller.pump.set_state = MagicMock(return_value=True)
+    def test_integration(self):
+        # write an integration test that combines controller and decider,
+        #       using a MOCKED sensor and pump.
+        pump_address = Pump('125.0.0.1', '8000')
+        sensor_address = Sensor('125.0.0.2', '8000')
+        decider = Decider(100, .1)
+        controller = Controller(sensor_address, pump_address, decider)
+        controller.pump.set_state = MagicMock(return_value=True)
 
-    for action in controller.actions.values():
-        for water_level in range(80, 120, 5):
-            controller.sensor.measure = MagicMock(return_value=water_level)
+        for action in controller.actions.values():
+            for water_level in range(80, 120, 5):
+                controller.sensor.measure = MagicMock(return_value=water_level)
 
-            controller.pump.get_state = MagicMock(
-                return_value=decider.decide(
-                    water_level, action, controller.actions))
+                controller.pump.get_state = MagicMock(
+                    return_value=decider.decide(
+                        water_level, action, controller.actions))
 
-            controller.tick()
+                controller.tick()
