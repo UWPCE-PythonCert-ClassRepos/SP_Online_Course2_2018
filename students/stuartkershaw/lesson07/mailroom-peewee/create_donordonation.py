139e2a6718e9e903578e133f0c9f2c4129cb3e38
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-08-11 09:44:19 -0700

    Create Donor and Donation tables.

---
+++ students/stuartkershaw/lesson07/mailroom-peewee/create_donordonation.py
@@ -0,0 +1,45 @@
+import logging
+import datetime
+from peewee import (SqliteDatabase, Model, CharField, ForeignKeyField,
+                    DateTimeField, DecimalField)
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+database = SqliteDatabase('mailroom.db')
+database.connect()
+database.execute_sql('PRAGMA foreign_keys = ON;')
+
+
+class BaseModel(Model):
+    class Meta:
+        database = database
+
+
+class Donor(BaseModel):
+    """
+        This class defines Donor, which maintains donor name
+    """
+    donor_name = CharField(primary_key=True, max_length=30)
+    donor_created = DateTimeField(default=datetime.datetime.now)
+    logger.info('Created Donor table with donor name and date created fields.')
+
+
+class Donation(BaseModel):
+    """
+        This class defines Donation, which maintains donation value and
+        donor name.
+    """
+    donation_value = DecimalField(max_digits=8, decimal_places=2)
+    donation_donor = ForeignKeyField(Donor, null=False)
+    donation_date = DateTimeField(default=datetime.datetime.now)
+    logger.info('Created Donation table with donation value, '
+                'date given, and donor name foreign key fields.')
+
+
+database.create_tables([
+    Donor,
+    Donation
+])
+
+database.close()
