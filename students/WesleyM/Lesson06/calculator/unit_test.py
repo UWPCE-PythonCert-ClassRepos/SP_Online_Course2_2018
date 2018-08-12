8ba4c045bcb85b375e91a63028e9383e5bdd94c7
Wesley Moy <wesmoy@uw.edu>
2018-08-12 13:50:02 -0500

    Submitting Water Regulation Assignment

--- students/WesleyM/Lesson06/calculator/unit_test.py
+++ students/WesleyM/Lesson06/calculator/unit_test.py
@@ -1,3 +1,4 @@
+"""Test Cases to verify calculator functions"""
 from unittest import TestCase
 from unittest.mock import MagicMock
 
@@ -8,9 +9,11 @@ from calculator.divider import Divider
 from calculator.calculator import Calculator
 from calculator.exceptions import InsufficientOperands
 
-class AdderTests(TestCase):
 
+class AdderTests(TestCase):
+    """Adder Test Case"""
     def test_adding(self):
+        """Test case that adds from range -10 to 10"""
         adder = Adder()
 
         for i in range(-10, 10):
@@ -19,25 +22,30 @@ class AdderTests(TestCase):
 
 
 class SubtracterTests(TestCase):
-
+    """Subtracter Test Case"""
     def test_subtracting(self):
+        """Test case that subtracts from range -10 to 10"""
         subtracter = Subtracter()
 
         for i in range(-10, 10):
             for j in range(-10, 10):
                 self.assertEqual(i - j, subtracter.calc(i, j))
 
-class MultiplierTests(TestCase):
 
+class MultiplierTests(TestCase):
+    """Multiplier Test Case"""
     def test_multiplying(self):
+        """Test case that multiplies from range -10 to 10"""
         multiplier = Multiplier()
         for i in range(-10, 10):
             for j in range(-10, 10):
                 self.assertEqual(i * j, multiplier.calc(i, j))
 
-class DividerTests(TestCase):
 
+class DividerTests(TestCase):
+    """Divider Test Case"""
     def test_divider(self):
+        """Test case that divides from range -10 to 10"""
         divider = Divider()
         for i in range(-10, 10):
             for j in range(-10, 10):
@@ -46,22 +54,27 @@ class DividerTests(TestCase):
 
 
 class CalculatorTests(TestCase):
-
+    """Series of Calculator Tests"""
     def setUp(self):
+        """Test case that setups the other test cases from range -10 to 10"""
         self.adder = Adder()
         self.subtracter = Subtracter()
         self.multiplier = Multiplier()
         self.divider = Divider()
 
-        self.calculator = Calculator(self.adder, self.subtracter, self.multiplier, self.divider)
+        self.calculator = Calculator(self.adder,
+                                     self.subtracter,
+                                     self.multiplier, self.divider)
 
     def test_insufficient_operands(self):
+        """Test insufficient operands"""
         self.calculator.enter_number(0)
 
         with self.assertRaises(InsufficientOperands):
             self.calculator.add()
 
     def test_adder_call(self):
+        """Test Case Adds Two Numbers"""
         self.adder.calc = MagicMock(return_value=0)
 
         self.calculator.enter_number(1)
@@ -71,6 +84,7 @@ class CalculatorTests(TestCase):
         self.adder.calc.assert_called_with(1, 2)
 
     def test_subtracter_call(self):
+        """Test Case Subtracts Two Numbers"""
         self.subtracter.calc = MagicMock(return_value=0)
 
         self.calculator.enter_number(1)
