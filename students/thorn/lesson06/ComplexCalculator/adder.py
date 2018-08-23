994198341fa55f7820a88d55dc4e756cd5872a15
Thomas Horn <hortw1991@gmail.com>
2018-08-23 16:14:14 -0500

    Initial commit.

---
+++ students/thorn/lesson06/ComplexCalculator/adder.py
@@ -0,0 +1,7 @@
+class Adder:
+    """ Adds 2 numbers. """
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        return operand_1 + operand_2
+    
---
+++ students/thorn/lesson06/ComplexCalculator/subtracter.py
@@ -0,0 +1,7 @@
+class Subtracter:
+    """ Subtracts 2 numbers. """
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        return operand_1 - operand_2
+    
