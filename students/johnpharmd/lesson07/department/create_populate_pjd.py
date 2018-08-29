6f1b37be55d9a7e3a38dd3ad140f9f834e1ffce2
johnpharmd <jhumphre@uw.edu>
2018-08-29 09:33:14 -0700

    added in more populate logic to create_populate_pjd and slightly refactored pjd_model

--- students/johnpharmd/lesson07/department/create_populate_pjd.py
+++ students/johnpharmd/lesson07/department/create_populate_pjd.py
@@ -12,7 +12,8 @@ from peewee import *
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-logger.info('One off program to build the classes from the model in the database')
+logger.info('One off program to build the classes from the model in ' +
+            'the database')
 
 logger.info('Here we define our data (the schema)')
 logger.info('First name and connect to a database (sqlite here)')
@@ -25,7 +26,8 @@ database.execute_sql('PRAGMA foreign_keys = ON;')  # needed for sqlite only
 
 logger.info('This means we can easily switch to a different database')
 logger.info('Enable the Peewee magic! This base class does it all')
-logger.info('By inheritance only we keep our model (almost) technology neutral')
+logger.info('By inheritance only we keep our' +
+            'model (almost) technology neutral')
 
 
 class BaseModel(Model):
@@ -41,7 +43,7 @@ class Person(BaseModel):
 
     logger.info('Note how we defined the class')
 
-    logger.info('Specify the fields in our model, their lengths and if mandatory')
+    logger.info('Specify the fields in our model, their lengths,+if mandatory')
     logger.info('Must be a unique identifier for each person')
 
     person_name = CharField(primary_key=True, max_length=30)
@@ -64,7 +66,8 @@ class Job(BaseModel):
 
     salary = DecimalField(max_digits=7, decimal_places=2)
     logger.info('Which person had the Job')
-    person_employed = ForeignKeyField(Person, related_name='was_filled_by', null=False)
+    person_employed = ForeignKeyField(Person, related_name='was_filled_by',
+                                      null=False)
 
 
 class Department(BaseModel):
@@ -98,17 +101,18 @@ class PersonNumKey(BaseModel):
 
 def populate_db():
     """
-    add person data to database
+    add person, job, and department data to database
     """
 
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
-    database = SqliteDatabase('pjd.db')
+    # database = SqliteDatabase('pjd.db')
 
     logger.info('Working with Person class')
     logger.info('Note how I use constants and a list of tuples as a simple schema')
-    logger.info('Normally you probably will have prompted for this from a user')
+    logger.info('Normally you probably will have prompted for ' +
+                'this from a user')
 
     PERSON_NAME = 0
     LIVES_IN_TOWN = 1
@@ -127,21 +131,21 @@ def populate_db():
     logger.info('and the transaction tells the database to fail on error')
 
     try:
-        database.connect()
-        database.execute_sql('PRAGMA foreign_keys = ON;')
+        # database.connect()
+        # database.execute_sql('PRAGMA foreign_keys = ON;')
         for person in people:
             with database.transaction():
                 new_person = Person.create(
-                        person_name = person[PERSON_NAME],
-                        lives_in_town = person[LIVES_IN_TOWN],
-                        nickname = person[NICKNAME])
+                        person_name=person[PERSON_NAME],
+                        lives_in_town=person[LIVES_IN_TOWN],
+                        nickname=person[NICKNAME])
                 new_person.save()
                 logger.info('Database add successful')
 
         logger.info('Print the Person records we saved...')
         for saved_person in Person:
-            logger.info(f'{saved_person.person_name} lives in {saved_person.lives_in_town} ' +\
-                f'and likes to be known as {saved_person.nickname}')
+            logger.info(f'{saved_person.person_name} lives in {saved_person.lives_in_town} ' +
+                        f'and likes to be known as {saved_person.nickname}')
 
     except Exception as e:
         logger.info(f'Error creating = {person[PERSON_NAME]}')
@@ -149,7 +153,48 @@ def populate_db():
         logger.info('See how the database protects our data')
 
     finally:
-        logger.info('database creates tables, populates, and closes')
+        logger.info('Database will now populate jobs')
+
+    logger.info('Working with Job class')
+    logger.info('Creating Job records: just like Person. ' +
+                'We use the foreign key')
+
+    JOB_NAME = 0
+    START_DATE = 1
+    END_DATE = 2
+    SALARY = 3
+    PERSON_EMPLOYED = 4
+
+    jobs = [
+        ('Analyst', '2001-09-22', '2003-01-30', 65500, 'Andrew'),
+        ('Senior analyst', '2003-02-01', '2006-10-22', 70000, 'Andrew'),
+        ('Senior business analyst', '2006-10-23', '2016-12-24', 80000, 'Andrew'),
+        ('Admin supervisor', '2012-10-01', '2014-11,10', 45900, 'Peter'),
+        ('Admin manager', '2014-11-14', '2018-01,05', 45900, 'Peter')
+        ]
+
+    try:
+        # database.connect()
+        # database.execute_sql('PRAGMA foreign_keys = ON;')
+        for job in jobs:
+            with database.transaction():
+                new_job = Job.create(
+                    job_name=job[JOB_NAME],
+                    start_date=job[START_DATE],
+                    end_date=job[END_DATE],
+                    salary=job[SALARY],
+                    person_employed=job[PERSON_EMPLOYED])
+                new_job.save()
+
+        logger.info('Reading and print all Job rows (note the value of person)...')
+        for job in Job:
+            logger.info(f'{job.job_name} : {job.start_date} to ' +
+                        f'{job.end_date} for {job.person_employed}')
+
+    except Exception as e:
+        logger.info(f'Error creating = {job[JOB_NAME]}')
+        logger.info(e)
+
 
 database.create_tables([
         Job,
