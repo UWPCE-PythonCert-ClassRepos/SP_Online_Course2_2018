38205fdaa0200b7b0a89d0c5ade1657411a6d165
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-09-02 15:59:26 -0700

    Mongo refactor underway.

---
+++ students/stuartkershaw/lesson08/mailroom-nosql/src/mongo_mailroom.py
@@ -0,0 +1,322 @@
+#!/usr/bin/env python3
+
+from mongo_persist_donordonation import (create_donor, create_donation,
+                                   update_donor, update_donation,
+                                   delete_donor, delete_donation,
+                                   get_donor_names, get_donor_donations)
+
+import pprint
+import pathlib
+pth = pathlib.Path('./')
+
+
+class DonorList:
+
+    def __init__(self):
+        self._rollup = {}
+
+    @property
+    def rollup(self):
+        return self._rollup
+
+    def update_rollup(self, donor_rollup):
+        self._rollup.update(donor_rollup)
+
+    @property
+    def donor_names(self):
+        return get_donor_names()
+
+    @property
+    def donor_donations(self):
+        return get_donor_donations()
+
+    def add_donor(self, name):
+        try:
+            create_donor(name)
+        except:
+            pass
+
+    def add_donation(self, donor, val):
+        if val < 1:
+            raise ValueError("A positive donation value is required.")
+
+        try:
+            create_donation(donor, val)
+        except:
+            pass
+
+    def get_donor(self, name):
+        if not name:
+            raise ValueError("Please provide a donor name.")
+
+        if name in self.donors:
+            return name
+        else:
+            return "Donor not found."
+
+    def get_donations(self, name):
+        if not name:
+            raise ValueError("Please provide a donor name.")
+
+        if name in self.donor_donations:
+            return self.donor_donations[name]
+        else:
+            return "Donor not found."
+
+    def get_donor_names(self):
+        return [donor for donor in self.donor.names]
+
+    def compose_thank_you(self, donor):
+        if not donor:
+            raise ValueError("Please provide a donor.")
+
+        message_obj = {
+            'donor_name': donor,
+            'donations': sum(self.get_donations(donor))
+        }
+
+        message = 'Dear {donor_name}, thanks so much '\
+                  'for your generous donations in the amount of: '\
+                  '${donations}.'.format(**message_obj)
+
+        return message
+
+    def generate_rollup(self):
+        for donor in self.donor_names:
+            donations = self.get_donations(donor)
+
+            number = len(donations)
+            total = int(sum(donations))
+            average = float(format(sum(donations) / len(donations), '.2f'))
+
+            donor_rollup = {
+                donor: dict(zip(('number', 'total', 'average'),
+                                (number, total, average)))
+            }
+
+            self.update_rollup(donor_rollup)
+
+    def generate_table(self):
+        if not len(self.donor_names):
+            print('The list of donors is empty.')
+            return
+
+        self.generate_rollup()
+
+        headings = ('Donor Name', 'Num Gifts', 'Total Given', 'Average Gift')
+
+        print('{:20}{:<15}{:<15}{:<15}'.format(*headings))
+        print('{:_<65}'.format(''))
+
+        for donor in self.donor_names:
+            print('{:<20}'.format(donor), ('{:<15}' * len(self.rollup[donor]))
+                  .format(*self.rollup[donor].values()))
+
+    def generate_letters(self):
+        if not len(self.donor_names):
+            print('The list of donors is empty.')
+            return
+
+        self.generate_rollup()
+
+        for donor in self.donor_names:
+            with open(donor.replace(' ', '_') + '.txt', 'w') as outfile:
+                outfile.write(self.compose_thank_you(donor))
+
+        print('Letters generated: ')
+
+        for f in pth.iterdir():
+            if '.txt' in str(f):
+                print(f)
+
+
+class DonorCli:
+
+    def __init__(self, donorCollection):
+        self._donorCollection = donorCollection
+
+    @property
+    def donorCollection(self):
+        return self._donorCollection
+
+    def set_donor(self):
+        while True:
+            try:
+                name = input('Please enter a donor name: ')
+                if not name:
+                    raise ValueError
+            except ValueError:
+                print('Oops, name is required.')
+                return
+            else:
+                self.donorCollection.add_donor(name)
+                self.add_donation(name)
+                print('{} added. Current donors: '.format(name))
+                print("\n".join(self.donorCollection.donor_names))
+                return
+
+    def add_donation(self, donor):
+        while True:
+            try:
+                donation = int(input('Please enter a donation amount: '))
+                if not donation > 0:
+                    raise ValueError
+            except ValueError:
+                print('Please provide a whole number greater than zero.')
+            else:
+                self.donorCollection.add_donation(donor, donation)
+                print('${} donation received.'.format(donation))
+                self.get_selection()
+
+    def find_donor_donation_update(self, donor, donation=None):
+        if not donation:
+            while True:
+                try:
+                    action = input('Please enter \'1\' to update or \'2\' to '
+                                   'delete: '.format(donor))
+                    if action not in ['1', '2']:
+                        raise ValueError
+                except ValueError:
+                    print('Oops, invalid selection...')
+                    return
+                else:
+                    if action == '1':
+                        updated_name = input('Please enter a new name for {}: '
+                                             .format(donor))
+                        update_donor(donor, updated_name)
+                    elif action == '2':
+                        validate = input('Are are you sure you wish to delete '
+                                         '{}? (yes/no): '.format(donor))
+                        if validate.lower() == 'yes':
+                            delete_donor(donor)
+                        else:
+                            print('Donor {} unchanged.'.format(donor))
+                    return
+        else:
+            while True:
+                try:
+                    action = input('Please enter \'1\' to update or \'2\' to '
+                                   'delete: '.format(donation))
+                    if action not in ['1', '2']:
+                        raise ValueError
+                except ValueError:
+                    print('Oops, invalid selection...')
+                    return
+                else:
+                    if action == '1':
+                        updated_value = input('Please enter a new val for {}: '
+                                              .format(donation))
+                        try:
+                            updated_value = int(updated_value)
+                            if not updated_value > 0:
+                                raise ValueError
+                            else:
+                                update_donation(donor, donation, updated_value)
+                        except ValueError:
+                            print('Oops, invalid selection...')
+                            return
+                    elif action == '2':
+                        validate = input('Are are you sure you wish to delete '
+                                         '{}? (yes/no): '.format(donation))
+                        if validate.lower() == 'yes':
+                            delete_donation(donor, donation)
+                        else:
+                            print('Donation {} unchanged.'.format(donation))
+                    return
+
+    def select_donation(self, donor):
+        donations = self.donorCollection.get_donations(donor)
+        range_donations = range(1, len(donations) + 1)
+        donations_dict = dict(zip(range_donations, donations))
+
+        while True:
+            try:
+                print('Updating {}\'s donations.'.format(donor))
+                pp = pprint.PrettyPrinter(width=1)
+                pp.pprint(donations_dict)
+                select = int(input('Please enter \'1\' through \'{}\' '
+                                   'to select a donation to update: '
+                                   .format(len(donations))))
+                if select not in donations_dict.keys():
+                    raise ValueError
+            except ValueError:
+                print('Oops, invalid selection...')
+                return
+            else:
+                print('Updating {}\'s donation of {}.'
+                      .format(donor, donations[select - 1]))
+                self.find_donor_donation_update(donor, donations[select - 1])
+                return
+
+    def get_donor_operation(self, operation=None):
+        if not self.donorCollection.donor_names:
+            print('The list of donors is empty.')
+            return
+
+        instruction = 'Please enter a name '\
+                      'or type \'list\' to see donors:\n'
+
+        name_input = input(instruction)
+
+        if name_input == 'list':
+            print("\n".join(self.donorCollection.donor_names))
+            self.get_donor_operation(operation)
+        elif name_input in self.donorCollection.donor_names:
+            if operation == 'update_donor':
+                self.find_donor_donation_update(name_input)
+            elif operation == 'set_donation':
+                self.add_donation(name_input)
+            elif operation == 'update_donation':
+                self.select_donation(name_input)
+        else:
+            print('Donor not found.')
+            self.get_donor_operation(operation)
+
+    def apply_selection(self, selection):
+        arg_dict = {
+            '1': self.set_donor,
+            '2': self.get_donor_operation,
+            '3': self.get_donor_operation,
+            '4': self.get_donor_operation,
+            '5': self.donorCollection.generate_table,
+            '6': self.donorCollection.generate_letters,
+            '7': quit
+        }
+        try:
+            if not arg_dict.get(selection):
+                raise KeyError
+            elif selection == '2':
+                arg_dict.get(selection)('update_donor')
+            elif selection == '3':
+                arg_dict.get(selection)('set_donation')
+            elif selection == '4':
+                arg_dict.get(selection)('update_donation')
+            else:
+                arg_dict.get(selection)()
+        except KeyError:
+            print('Oops, invalid selection.')
+
+    def get_selection(self):
+        options = 'Please select from the menu:\n'\
+                  '1) add new donor\n'\
+                  '2) update donor\n'\
+                  '3) add new donation\n'\
+                  '4) update donation\n'\
+                  '5) create a report\n'\
+                  '6) send letters to everyone\n'\
+                  '7) quit\n'
+        while True:
+            selection = input(options)
+            self.apply_selection(selection)
+            if selection == '3':
+                self.get_donor_operation()
+
+
+def main():
+    dl = DonorList()
+    cli = DonorCli(dl)
+    cli.get_selection()
+
+
+if __name__ == "__main__":
+    main()
---
+++ students/stuartkershaw/lesson08/mailroom-nosql/src/mongo_persist_donordonation.py
@@ -0,0 +1,133 @@
+import pprint
+
+import login_database
+import utilities
+
+log = utilities.configure_logger('default', '../logs/mongodb.log')
+
+
+def create_donor(name):
+    """
+    Add new donor to database
+    """
+    log.info('Working with Mongo create_donor function')
+
+    log.info('Creating Donor record...')
+
+    try:
+        database = login_database.login_mongodb_cloud()
+
+        with database as client:
+            log.info('Step 1: We are going to use a database called mailroom')
+            db = client['mailroom']
+
+            log.info('And in that database use a collection called donors')
+            donors = db['donors']
+
+            log.info('Step 2: Now we add a donor with the given name')
+            donors.insert_one({
+                'donor_name': name
+            })
+
+            log.info('Step 3: Query to ensure the donor was created')
+            query = {'donor_name': name}
+            results = donors.find(query)
+
+            log.info('Step 4: Print the results')
+            print('Donor created.')
+            for r in results:
+                pprint.pprint(r)
+
+            db.drop_collection('donors')
+
+    except Exception as e:
+        log.info(f'Error creating = {name}')
+        log.info(e)
+
+    finally:
+        log.info('Mongo create_donor complete')
+
+
+def update_donor(old_name, new_name):
+    """
+    Update donor name
+    """
+    pass
+
+
+def delete_donor(name):
+    """
+    Delete donor
+    """
+    pass
+
+
+def create_donation(donor, amount):
+    """
+    Add new donation to database
+    """
+    log.info('Working with Mongo create_donation function')
+
+    log.info('Creating Donation record...')
+
+    try:
+        database = login_database.login_mongodb_cloud()
+
+        with database as client:
+            log.info('Step 1: We are going to use a database called mailroom')
+            db = client['mailroom']
+
+            log.info('And in that database use a collection called donations')
+            donations = db['donations']
+
+            log.info('Step 2: Now we add a donation with the given donor name and amount')
+            donations.insert_one({
+                'donation_amount': amount,
+                'donor_name': donor
+            })
+
+            log.info('Step 3: Query to ensure the donation was created')
+            query = {'donation_amount': amount}
+            results = donations.find(query)
+
+            log.info('Step 4: Print the results')
+            print('Donation created.')
+            for r in results:
+                pprint.pprint(r)
+
+            db.drop_collection('donations')
+
+    except Exception as e:
+        log.info(f'Error creating = {donor, amount}')
+        log.info(e)
+
+    finally:
+        log.info('Mongo create_donation complete')
+
+
+def update_donation(donor, old_donation, new_donation):
+    """
+    Update donation amount
+    """
+    pass
+
+
+def delete_donation(donor, donation):
+    """
+    Delete donation
+    """
+    pass
+
+
+def get_donor_names():
+    """
+    Get donor names from database
+    """
+    pass
+
+
+def get_donor_donations():
+    """
+    Get donor donations from database
+    """
+    pass
