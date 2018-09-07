4519234071d9805641e2f6fa691bf47430bf9987
johnpharmd <jhumphre@uw.edu>
2018-09-07 09:09:08 -0700

    completed initial functional iteration

--- students/johnpharmd/lesson07/mailroom_db/report_mailroom.py
+++ students/johnpharmd/lesson07/mailroom_db/report_mailroom.py
@@ -1,12 +1,10 @@
 from peewee import SqliteDatabase
-# from peewee import Model
-# from peewee import CharField
-# from peewee import IntegerField
 
 
 def report():
     database = SqliteDatabase('mailroom.db')
     database.connect()
+    print('Last Name____Title__Total Donations__Number of Donations')
     donor_table = database.execute_sql('select * from donor;')
     for donor in donor_table:
         print(donor)
