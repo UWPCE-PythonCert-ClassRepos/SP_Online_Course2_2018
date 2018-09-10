82db3620bf9520f9a426cb303d7c9f0edda362d1
Stuart Kershaw <stuartdkershaw@gmail.com>
2018-09-09 19:19:21 -0700

    Redis underway.

--- students/stuartkershaw/lesson08/mailroom-nosql/src/login_database.py
+++ students/stuartkershaw/lesson08/mailroom-nosql/src/login_database.py
@@ -36,29 +36,29 @@ def login_mongodb_cloud():
     return client
 
 
-# def login_redis_cloud():
-#     """
-#         connect to redis and login
-#     """
-#     try:
-#         config.read(config_file)
-#         host = config["redis_cloud"]["host"]
-#         port = config["redis_cloud"]["port"]
-#         pw = config["redis_cloud"]["pw"]
+def login_redis_cloud():
+    """
+        connect to redis and login
+    """
+    try:
+        config.read(config_file)
+        host = config["redis_cloud"]["host"]
+        port = config["redis_cloud"]["port"]
+        pw = config["redis_cloud"]["pw"]
 
 
-#     except Exception as e:
-#         print(f'error: {e}')
+    except Exception as e:
+        print(f'error: {e}')
 
-#     log.info('Here is where we use the connect to redis.')
+    log.info('Here is where we use the connect to redis.')
 
-#     try:
-#         r = redis.StrictRedis(host=host, port=port, password=pw, decode_responses=True)
+    try:
+        r = redis.StrictRedis(host=host, port=port, password=pw, decode_responses=True)
 
-#     except Exception as e:
-#         print(f'error: {e}')
+    except Exception as e:
+        print(f'error: {e}')
 
-#     return r
+    return r
 
 
 def login_neo4j_cloud():
