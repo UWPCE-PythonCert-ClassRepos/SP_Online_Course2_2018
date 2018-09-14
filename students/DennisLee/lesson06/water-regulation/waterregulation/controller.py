e2a938b657edd8f57b17a4733c75fc9801eb70f2
Dennis Lee <dennis271lee@yahoo.com>
2018-09-13 23:39:09 -0700

    [DennisLee][L6][Water Regulation] pylint and flake8 changes for controller.py and decider.py.

--- students/DennisLee/lesson06/water-regulation/waterregulation/controller.py
+++ students/DennisLee/lesson06/water-regulation/waterregulation/controller.py
@@ -32,14 +32,18 @@ class Controller(object):
         On each call to tick, the controller shall:
 
           1. query the sensor for the current height of liquid in the tank
-          2. query the pump for its current state (pumping in, pumping out, or at rest)
-          3. query the decider for the next appropriate state of the pump, given the above
+          2. query the pump for its current state (pumping in, pumping out, or
+             at rest)
+          3. query the decider for the next appropriate state of the pump,
+             given the above
           4. set the pump to that new state
 
         :return: True if the pump has acknowledged its new state, else False
         """
         height = self.sensor.measure()
         pump_state_current = self.pump.get_state()
-        pump_state_next = self.decider.decide(height, pump_state_current, self.actions)
+        pump_state_next = self.decider.decide(
+            height, pump_state_current, self.actions
+        )
         result = self.pump.set_state(pump_state_next)
         return result
