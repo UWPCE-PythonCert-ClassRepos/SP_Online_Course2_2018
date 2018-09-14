b469ef839e627b34cd355738ee466dd440452d08
johnpharmd <jhumphre@uw.edu>
2018-09-14 06:43:43 -0700

    cleaned up

--- students/johnpharmd/lesson08/src/furniture_mongodb.py
+++ students/johnpharmd/lesson08/src/furniture_mongodb.py
@@ -7,32 +7,19 @@ log = utilities.configure_logger('default', '../logs/furniture_mongodb.log')
 
 def update_furniture():
     """
-        Updates furniture db in mongodb, according to assignmt instructions
+        Updates furniture db in mongodb, according to assignment instructions
     """
 
     with login_database.login_mongodb_cloud() as client:
         db = client['dev']
         furniture = db['furniture']
 
-        # cursor = furniture.find({})
-        # log.info('Here are all the furniture products\n')
-        # for doc in cursor:
-        #     pprint.pprint(doc)
-        #     print()
-
         log.info('\nNow will update all product documents ' +
                  'to have a color field\n')
         furniture.update_many({}, {'$set': {'color': ''}})
-        # newcursor = furniture.find({})
-        # for doc in newcursor:
-        #     pprint.pprint(doc)
-        #     print()
 
         log.info('\nNow will rename "product" to "product_type"\n')
         furniture.update_many({}, {'$rename': {'product': 'product_type'}})
-        # for doc in furniture.find({}):
-        #     pprint.pprint(doc)
-        #     print()
 
         log.info('\nNow will check each product type for colors "blue" ' +
                  'and "red". If either is found, will remove from product ' +
@@ -49,17 +36,9 @@ def update_furniture():
                               {'$set': {'color': 'Red'}})
         furniture.update_many({'product_type': 'Red couch'},
                               {'$set': {'product_type': 'Couch'}})
-        log.info('\nNow deleting any documents that have "product type" ' +
-                 'field\n')
-        furniture.delete_many({'product type': '$'})
 
         log.info('Finally, printing all documents in furniture collection:\n')
         for doc in furniture.find({}):
-            # for color in ('blue', 'red'):
-            # if color in doc['product_type']:
-            # furniture.update({doc['product_type'].strip(color)},
-            # {doc['color']: {'$set': {'color':
-            # color}}})
             pprint.pprint(doc)
             print()
 
