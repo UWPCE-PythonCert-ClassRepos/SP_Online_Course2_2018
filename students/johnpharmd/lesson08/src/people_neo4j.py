2f684518a862dde7c0ce1e3980155d6412dc30dc
johnpharmd <jhumphre@uw.edu>
2018-09-17 13:08:21 -0700

    refactored, but still in process

--- students/johnpharmd/lesson08/src/people_neo4j.py
+++ students/johnpharmd/lesson08/src/people_neo4j.py
@@ -4,6 +4,7 @@
 
 import utilities
 import login_database
+import neo4j_script
 
 
 log = utilities.configure_logger('default', '../logs/people_neo4j.log')
@@ -37,7 +38,7 @@ def add_colors():
 
         log.info('Adding 3 colors')
         for color in ['blue', 'red', 'green']:
-            cyph = "CREATE (n:Color {name:'%s'})" % (color)
+            cyph = "CREATE (c:Color {name:'%s'})" % (color)
             session.run(cyph)
 
         log.info('Putting the colors into the db')
@@ -50,9 +51,51 @@ def add_colors():
             print(record['name'])
 
 
+def match_people_colors():
+    person_list = []
+    blue_list = [('Fred', 'Barnes'), ('Jonas', 'Salk'),
+                 ('Rosalind', 'Franklin')]
+    red_list = []
+    green_list = []
+    for person in blue_list:
+        person_list.append(person)
+
+    with driver.session() as session:
+
+        log.info('Associating people with their respective favorite color')
+        log.info('Start with blue')
+        for first, last in blue_list:
+            cypher = """
+              MATCH (p:Person {first_name:'%s', last_name:'%s'})
+              CREATE (p)-[favorite_color:COLOR]->(c:Color {name:'blue'})
+              RETURN p
+            """ % (first, last)
+        session.run(cypher)
+
+        print("Find everyone whose favorite color is blue?")
+
+        try:
+            for first, last in person_list:
+                cyph = """
+                  MATCH (p {first_name:'%s', last_name:'%s'})-[:COLOR]->(blue)
+                  RETURN p.first_name as first_name, p.last_name as last_name
+                """ % (first, last)
+            result = session.run(cyph)
+            print("\nBlue is the favorite color of these people:")
+            for record in result:
+                print('record:', record)
+                # for person in record.values():
+                #     print(person['first_name'], person['last_name'])
+
+        except Exception as e:
+            print(f'neo4j error: {e}')
+
+
 def run_db():
+    neo4j_script.run_example()
     add_people()
     add_colors()
+    match_people_colors()
 
 
 if __name__ == '__main__':
