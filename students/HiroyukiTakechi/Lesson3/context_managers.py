79f86ad23eb72ce87b86578b88e77ab31bd0902e
Hiroyuki Takechi <hiroyukitakechi@gmail.com>
2018-09-19 21:19:28 -0700

    The second submission of context_managers.py, after office hours

--- students/HiroyukiTakechi/Lesson3/context_managers.py
+++ students/HiroyukiTakechi/Lesson3/context_managers.py
@@ -3,29 +3,31 @@ Lesson 3 Assignment #1
 Conext Managers
 '''
 
+class Locke:
 
-class Locke(object):
-
-    def __init__(self, boats):
-        print(('Number of boats = {}').format(boats))
-        self.boats = boats
-
-
-    def move_boats_throughs(self):
-        pass
-
+    def __init__(self, capacity):
+        print(('capacity = {}').format(capacity))
+        self.capacity = capacity
 
     def __enter__(self):
         print('__enter__()')
-        print("Stopping the pumps")
-        print("Opening the doors")
-        print("Closing the doors")
-        print("Restarting the pump")
         return self
 
-    def __exit__(self):
-        print('__exit__()')
-        return self
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        print('__exit__({},{},{})'.format(exc_type,exc_val,exc_tb))
+        return self.capacity
+
+    def move_boats_through(self, boats):
+        print(('Number of boats = {}').format(boats))
+        if boats > self.capacity:
+            raise Exception("Operation is cancelled.")
+        else:
+            print("Proceed operation...")
+            print("Stopping the pumps")
+            print("Opening the doors")
+            print("Closing the doors")
+            print("Restarting the pump")       
+
 
 def main():
     with small_locke as locke:
@@ -34,46 +36,12 @@ def main():
     with large_locke as locke:
         locke.move_boats_through(boats)
 
-if __main__ == '__main__':
-    main()
-
-
-import contextlib
-
-@contextlib.contextmanager
-
-def Locke(object):
-    print('enter')
-    
-    try:
-        print("Stopping the pumps")
-        yield
-        print("Opening the doors")
-        yield
-        print("Closing the doors")
-        yield
-        print("Restarting the pump")
-    except:
-        raise Exception('Error')
-    finally:
-        print('exit')
-
-def main():
-    with Locke():
-        raise Exception('Error')
-
-if __name__ == '__main__':
-    main()
-
 
 small_locke = Locke(5)
 large_locke = Locke(10)
 boats = 8
 
-# Too many boats through a small locke will raise an exception
-with small_locke as locke:
-    locke.move_boats_through(boats)
+if __name__ == '__main__':
+    main()
 
-# A lock with sufficient capacity can move boats without incident.
-with large_locke as locke:
-    locke.move_boats_through(boats)
+
