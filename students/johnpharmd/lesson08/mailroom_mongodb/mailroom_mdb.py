54e0c0fb859887eb7bc532ee7ff53c7175a4415f
johnpharmd <jhumphre@uw.edu>
2018-09-21 14:19:25 -0700

    completed first functional iteration of mailroom_mdb, and linted

--- students/johnpharmd/lesson08/mailroom_mongodb/mailroom_mdb.py
+++ students/johnpharmd/lesson08/mailroom_mongodb/mailroom_mdb.py
@@ -2,27 +2,22 @@
     Creates mongodb database for mailroom
 """
 
-from learn_data import get_mailroom_data
-import login_database
-import pprint
 import sys
+# from learn_data import get_mailroom_data
+import login_database
 import utilities
 
 log = utilities.configure_logger('default', '../logs/mailroom_mdb.log')
 
 with login_database.login_mongodb_cloud() as client:
-        db = client['dev']
-        mailroom = db['mailroom']
-        # mailroom.insert_many(get_mailroom_data()) -- used for 1st init
-
-
-class Donor():
-    pass
+    db = client['dev']
+    mailroom = db['mailroom']
+    # mailroom.insert_many(get_mailroom_data()) -- used for 1st init
 
 
 def get_donor_list():
     donor_list = []
-    for d in mailroom.find({}):
+    for d in mailroom.find():
         donor_list.append(d['title'] + ' ' + d['last_name'])
     print(donor_list)
 
@@ -30,57 +25,85 @@ def get_donor_list():
 def query_donor_info():
     title = input('Enter donor title: ')
     last_name = input('Enter last name: ')
-    total_donation_amt = int(input('Enter total donation amount: '))
+    donations = int(input('Enter total donation amount: '))
     num_donations = int(input('Enter number of donations: '))
     query_dict = {'title': title, 'last_name': last_name,
-                  'total_donation_amt': total_donation_amt,
+                  'donations': donations,
                   'num_donations': num_donations}
     return query_dict
 
 
-def add_donor(*donor):
+def add_donor(**donor):
     if donor:
-        updated_donor = mailroom.insert(donor)
-        print(updated_donor['title'], updated_donor['last_name'], 'updated')
+        print(donor['title'], donor['last_name'], 'updated')
+        mailroom.insert_one(donor)
     else:
         new_donor = query_donor_info()
-        new_donor = mailroom.insert(new_donor)
-        print(new_donor.title, new_donor.lastname, 'added to database')
+        print(new_donor['title'], new_donor['last_name'], 'added to database')
+        mailroom.insert_one(new_donor)
 
 
 def update_or_remove_donor():
     q_title = input('Enter donor title: ')
     q_lastname = input('Enter last name: ')
+    query = {}
+    for ddoc in mailroom.find():
+        if ddoc['title'] == q_title and ddoc['last_name'] == q_lastname:
+            query = ddoc
+    mailroom.delete_one(query)
     response = input('[U]pdate or [r]emove this donor? ')
-    # TODO: add mongodb logic here to find donor in db
-        if response.lower() == 'u':
-            print('Re-enter values for each of the donor\'s fields')
-            updated_donor = query_donor_info()
-            add_donor(updated_donor)
-        elif response.lower() == 'r':
-            mailroom.remove(donor)
-            print('Donor removed from database')
+    if response.lower() == 'u':
+        print('Re-enter values for each of the donor\'s fields')
+        updated_donor = query_donor_info()
+        add_donor(**updated_donor)
+    elif response.lower() == 'r':
+        print(query['title'], query['last_name'], 'removed from database')
 
 
 def get_report():
-    pass
+    print()
+    psv = ['Donor Name', '| Total Given', '| Num Gifts',
+           '| Average Gift']
+    print('{:<15}{:>12}{:>12}{:>12}'.format(psv[0], psv[1],
+                                            psv[2], psv[3]))
+    for i in range(55):
+        print('-', end='')
+    print()
+    new_list = []
+    try:
+        for ddoc in mailroom.find():
+            new_list.append([ddoc['donations'], ddoc['last_name'],
+                             ddoc['num_donations']])
+    except Exception as e:
+        print(f'Error in for loop, and {e}')
+
+    new_list.sort(reverse=True)
+    for d_list in new_list:
+        formatted_donor = ('{:<15}'.format(d_list[1])
+                           + '{}{:>10}'.format(' $', d_list[0])
+                           + '{:>13}'.format(d_list[2])
+                           + '{}{:>11}'.format(' $',
+                                               d_list[0] // d_list[2]))
+        print(formatted_donor)
 
 
 class UI():
     def __init__(self):
-        menu_dct = {'1': get_donor_list,
-                    '2': get_report,
-                    '3': add_donor,
-                    '4': update_or_remove_donor,
-                    'q': sys.exit}
+        menu_dct = {
+            '1': get_donor_list,
+            '2': get_report,
+            '3': add_donor,
+            '4': update_or_remove_donor,
+            'q': sys.exit
+            }
         main_text = '\n'.join((
-                               'Choose from the following:',
-                               '"1" - Get a List of Donors,',
-                               '"2" - Create a Report,',
-                               '"3" - Add a Donor,',
-                               '"4" - Update or Remove a Donor, or',
-                               '"q" to Quit: '
-                               ))
+            'Choose from the following:',
+            '"1" - Get a List of Donors,',
+            '"2" - Create a Report,',
+            '"3" - Add a Donor,',
+            '"4" - Update or Remove a Donor, or',
+            '"q" to Quit: '
+            ))
         while True:
             print('\nMain Menu:')
             response = input(main_text)
