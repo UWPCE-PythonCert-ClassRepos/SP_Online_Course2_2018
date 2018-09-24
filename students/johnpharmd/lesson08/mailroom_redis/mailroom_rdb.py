9b00061672e2927aec0216e88762d8ef8369acd6
johnpharmd <jhumphre@uw.edu>
2018-09-24 10:02:44 -0700

    completed first functional iteration of mailroom_rdb

--- students/johnpharmd/lesson08/mailroom_redis/mailroom_rdb.py
+++ students/johnpharmd/lesson08/mailroom_redis/mailroom_rdb.py
@@ -4,72 +4,64 @@
 
 import sys
 from login_database import login_redis_cloud
-from learn_data import get_mailroom_data
-
 
 r = login_redis_cloud()
+r.flushdb()
 
 
-def make_cache_dict():
-    cache_dict = {}
-    for d_dict in get_mailroom_data():
-        d_key = 'donor:' + d_dict['last_name']
-        try:
-            cache_dict[d_key] = {k: v for k, v in d_dict.items() if
-                                 k != 'last_name'}
-        except Exception as e:
-            print(f'Error occurred at {e}')
-    return cache_dict
+def cache_data():
+    """
+        given redis's non-robust querying, donor data is hardcoded here
+    """
+    r.hmset('donor:Gates', {'title': 'Mr.', 'donations': 150000,
+                            'num_donations': 3})
+    r.hmset('donor:Brin', {'title': 'Mr.', 'donations': 150000,
+                           'num_donations': 3})
+    r.hmset('donor:Cerf', {'title': 'Mr.', 'donations': 50000,
+                           'num_donations': 2})
+    r.hmset('donor:Musk', {'title': 'Mr.', 'donations': 100000,
+                           'num_donations': 1})
+    r.hmset('donor:Berners-Lee', {'title': 'Mr.', 'donations': 50000,
+                                  'num_donations': 2})
+    r.hmset('donor:Wojcicki', {'title': 'Ms.', 'donations': 125000,
+                               'num_donations': 1})
+    r.hmset('donor:Avey', {'title': 'Ms.', 'donations': 200000,
+                           'num_donations': 2})
 
 
-def make_hmset_statements():
-    hmset_statements_list = []
-    for donor, d_dict in make_cache_dict().items():
-        r_string = "r.hmset('" + donor + "', " + str(d_dict) + ")"
-        hmset_statements_list.append(r_string)
-    # for s in hmset_statements_list:
-    #     print(s)
-    return hmset_statements_list
-
-
-def run_hmset_statements():
-    for s in make_hmset_statements():
-        s = s.strip('"')
-        print(s)
-
-
-def make_hmget_statements():
-    hmget_statements_list = []
-    for donor, d_dict in make_cache_dict().items():
-        r_string = "r.hmget('" + donor + "', " + str(d_dict) + ")"
-        hmget_statements_list.append(r_string)
-    return hmget_statements_list
+def get_one_donor_data():
+    """
+        simple query to cache for one donor's data
+    """
+    while True:
+        q_last_name = input('Enter donor\'s last name, or "e" to exit: ')
+        if q_last_name.lower() == 'e':
+            break
+        else:
+            donor = 'donor:' + q_last_name
+            print('title:', r.hget(donor, 'title'))
+            print('total donations:', r.hget(donor, 'donations'))
+            print('number of donations:', r.hget(donor, 'num_donations'))
 
 
 def get_donor_list():
-    # donor_list = []
-    for s in make_hmget_statements():
-        donor = s.strip('"')
-        print(donor)
-    # return [s.strip('"') for s in make_hmget_statements()]
-    # print(donor_list)
+    """
+        returns list of all donor keys
+    """
+    print(r.keys())
 
 
 class UI():
     def __init__(self):
         menu_dct = {
             '1': get_donor_list,
-            # '2': get_report,
-            # '3': add_donor,
-            # '4': update_or_remove_donor,
+            '2': get_one_donor_data,
             'q': sys.exit
             }
         main_text = '\n'.join((
             'Choose from the following:',
             '"1" - Get a List of Donors,',
-            '"2" - Create a Report,',
-            '"3" - Add a Donor,',
-            '"4" - Update or Remove a Donor, or',
+            '"2" - Get Data from All Fields for One Donor, or',
             '"q" to Quit: '
             ))
         while True:
@@ -86,5 +78,5 @@ class UI():
 
 
 if __name__ == '__main__':
-    run_hmset_statements()
+    cache_data()
     interaction_instance = UI()
