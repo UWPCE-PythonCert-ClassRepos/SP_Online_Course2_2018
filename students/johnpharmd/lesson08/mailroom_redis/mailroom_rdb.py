351bca0a60748bc89fe3f1d79f3caff9550847fc
johnpharmd <jhumphre@uw.edu>
2018-09-24 10:45:37 -0700

    correction: now, according to assignment requirements, have first functional iteration

--- students/johnpharmd/lesson08/mailroom_redis/mailroom_rdb.py
+++ students/johnpharmd/lesson08/mailroom_redis/mailroom_rdb.py
@@ -3,10 +3,28 @@
 """
 
 import sys
+from random import choice as rc
 from login_database import login_redis_cloud
 
 r = login_redis_cloud()
 r.flushdb()
+
+
+def create_random_phone():
+    area = rc(['206-', '360-', '425-'])
+    prefix = rc(['234-', '321-', '456-', '987-'])
+    suffix = rc(['1212', '2789', '3767', '4123', '5555'])
+    return area + prefix + suffix
+
+
+def create_random_email():
+    donor_suffix = rc(['3000', '5000', '7000'])
+    domain = rc(['@gmail.com', '@hotmail.com', '@outlook.com'])
+    return donor_suffix + domain
+
+
+def choose_random_zip():
+    return rc(['98101', '98112', '98127', '98155'])
 
 
 def cache_data():
@@ -14,19 +32,40 @@ def cache_data():
         given redis's non-robust querying, donor data is hardcoded here
     """
     r.hmset('donor:Gates', {'title': 'Mr.', 'donations': 150000,
-                            'num_donations': 3})
+                            'num_donations': 3,
+                            'phone': create_random_phone(),
+                            'email': 'gates' + create_random_email(),
+                            'zip': choose_random_zip()})
     r.hmset('donor:Brin', {'title': 'Mr.', 'donations': 150000,
-                           'num_donations': 3})
+                           'num_donations': 3,
+                           'phone': create_random_phone(),
+                           'email': 'brin' + create_random_email(),
+                           'zip': choose_random_zip()})
     r.hmset('donor:Cerf', {'title': 'Mr.', 'donations': 50000,
-                           'num_donations': 2})
+                           'num_donations': 2,
+                           'phone': create_random_phone(),
+                           'email': 'cerf' + create_random_email(),
+                           'zip': choose_random_zip()})
     r.hmset('donor:Musk', {'title': 'Mr.', 'donations': 100000,
-                           'num_donations': 1})
+                           'num_donations': 1,
+                           'phone': create_random_phone(),
+                           'email': 'musk' + create_random_email(),
+                           'zip': choose_random_zip()})
     r.hmset('donor:Berners-Lee', {'title': 'Mr.', 'donations': 50000,
-                                  'num_donations': 2})
+                                  'num_donations': 2,
+                                  'phone': create_random_phone(),
+                                  'email': 'tbl' + create_random_email(),
+                                  'zip': choose_random_zip()})
     r.hmset('donor:Wojcicki', {'title': 'Ms.', 'donations': 125000,
-                               'num_donations': 1})
+                               'num_donations': 1,
+                               'phone': create_random_phone(),
+                               'email': 'awoj' + create_random_email(),
+                               'zip': choose_random_zip()})
     r.hmset('donor:Avey', {'title': 'Ms.', 'donations': 200000,
-                           'num_donations': 2})
+                           'num_donations': 2,
+                           'phone': create_random_phone(),
+                           'email': 'avey' + create_random_email(),
+                           'zip': choose_random_zip()})
 
 
 def get_one_donor_data():
@@ -39,7 +78,8 @@ def get_one_donor_data():
             break
         else:
             donor = 'donor:' + q_last_name
-            print(r.hmget(donor, 'title', 'donations', 'num_donations'))
+            print(r.hmget(donor, 'title', 'donations', 'num_donations',
+                          'phone', 'email', 'zip'))
             # print('title:', r.hget(donor, 'title'))
             # print('total donations:', r.hget(donor, 'donations'))
             # print('number of donations:', r.hget(donor, 'num_donations'))
