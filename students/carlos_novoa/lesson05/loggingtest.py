a91c8167c88210f353fb43260bd07eee3ca0826b
Carlos Novoa <novoac@uw.edu>
2018-09-27 19:16:57 -0700

    Lesson05 - Logging and Debugging

---
+++ students/carlos_novoa/lesson05/loggingtest.py
@@ -0,0 +1,8 @@
+# loggingtest.py
+import logging
+
+logging.critical("This is a critical error!")
+logging.error("I'm an error.")
+logging.warning("Hello! I'm a warning!")
+logging.info("This is some information.")
+logging.debug("Perhaps this information will help you find your problem?")
