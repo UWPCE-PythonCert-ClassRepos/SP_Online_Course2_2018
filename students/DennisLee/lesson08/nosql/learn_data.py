f3c8ff48c8176762ef5727e8eb0ff5daa0ea014e
Dennis Lee <dennis271lee@yahoo.com>
2018-10-08 00:07:51 -0700

    Updated the MongoDB part of the NOSQL assignment.
    Done blindly since the test script does not work right for MongoDB at the moment.

--- students/DennisLee/lesson08/nosql/learn_data.py
+++ students/DennisLee/lesson08/nosql/learn_data.py
@@ -10,40 +10,67 @@ def get_furniture_data():
 
     furniture_data = [
         {
-            'product': 'Red couch',
+            'product_type': 'Couch',
+            'color': 'Red',
             'description': 'Leather low back',
             'monthly_rental_cost': 12.99,
             'in_stock_quantity': 10
         },
         {
-            'product': 'Blue couch',
+            'product_type': 'Couch',
+            'color': 'Blue',
             'description': 'Cloth high back',
             'monthly_rental_cost': 9.99,
             'in_stock_quantity': 3
         },
         {
-            'product': 'Coffee table',
+            'product_type': 'Table',
+            'color': 'Coffee',
             'description': 'Plastic',
             'monthly_rental_cost': 2.50,
             'in_stock_quantity': 25
         },
         {
-            'product': 'Red couch',
+            'product_type': 'Couch',
+            'color': 'Red',
             'description': 'Leather high back',
             'monthly_rental_cost': 15.99,
             'in_stock_quantity': 17
         },
         {
-            'product': 'Blue recliner',
+            'product_type': 'Recliner',
+            'color': 'Blue',
             'description': 'Leather high back',
             'monthly_rental_cost': 19.99,
             'in_stock_quantity': 6
         },
         {
-            'product': 'Chair',
+            'product_type': 'Chair',
+            'color': 'White',
             'description': 'Plastic',
             'monthly_rental_cost': 1.00,
             'in_stock_quantity': 45
+        },
+        {
+            'product_type': 'Table',
+            'color': 'Yellow',
+            'description': 'Wood',
+            'monthly_rental_cost': 4.00,
+            'in_stock_quantity': 15
+        },
+        {
+            'product_type': 'Recliner',
+            'color': 'Red',
+            'description': 'Vinyl',
+            'monthly_rental_cost': 10.50,
+            'in_stock_quantity': 9
+        },
+        {
+            'product_type': 'Couch',
+            'color': 'Black',
+            'description': 'Marble',
+            'monthly_rental_cost': 6.00,
+            'in_stock_quantity': 30
         }
     ]
     return furniture_data
--- students/DennisLee/lesson08/nosql/mongodb_script.py
+++ students/DennisLee/lesson08/nosql/mongodb_script.py
@@ -1,5 +1,5 @@
 """
-    mongodb example
+mongodb example
 """
 
 import pprint
@@ -35,24 +35,44 @@ def run_example(furniture_items):
         print('Plastic products')
         pprint.pprint(results)
 
-        log.info('Step 5: Delete the blue couch (actually deletes all blue couches)')
-        furniture.remove({"product": {"$eq": "Blue couch"}})
+        log.info(
+            'Step 5: Delete the blue couch (actually deletes all blue couches)'
+        )
+        # furniture.remove({"product": {"$eq": "Blue couch"}})
+        furniture.delete_many({'product_type': 'Couch', 'color': 'Blue'})
 
         log.info('Step 6: Check it is deleted with a query and print')
-        query = {'product': 'Blue couch'}
+        # query = {'product': 'Blue couch'}
+        query = {'product_type': 'Couch', 'color': 'Blue'}
         results = furniture.find_one(query)
         print('The blue couch is deleted, print should show none:')
         pprint.pprint(results)
 
-        log.info(
-            'Step 7: Find multiple documents, iterate though the results and print')
+        log.info('Step 7: Find multiple documents, '
+                 'iterate though the results and print')
 
-        cursor = furniture.find({'monthly_rental_cost': {'$gte': 15.00}}).sort('monthly_rental_cost', 1)
+        cursor = furniture.find({'monthly_rental_cost': {'$gte': 15.00}}
+                               ).sort('monthly_rental_cost', 1)
         print('Results of search')
         log.info('Notice how we parse out the data from the document')
 
         for doc in cursor:
-            print(f"Cost: {doc['monthly_rental_cost']} product name: {doc['product']} Stock: {doc['in_stock_quantity']}")
+            print(f"Cost: {doc['monthly_rental_cost']}")
+            print(f"Product name: {doc['product_type']}")
+            print(f"Description: {doc['description']}")
+            print(f"Color: {doc['color']}")
+            print(f"Stock: {doc['in_stock_quantity']}")
 
-        log.info('Step 8: Delete the collection so we can start over')
+        log.info('Step 8: Find just the red-color products')
+        query = furniture.find({'color': {'$eq': 'Red'}})
+        print('Results of search should contain 2 couches and 1 recliner')
+        pprint.pprint(query)
+
+        log.info('Step 9: Find just the couches')
+        query = furniture.find({'product_type': {'$eq': 'Couch'}})
+        print('Results of search should contain 2 red couches, '
+              'a blue couch, and a black couch')
+        pprint.pprint(query)
+
+        log.info('Step 10: Delete the collection so we can start over')
         db.drop_collection('furniture')
