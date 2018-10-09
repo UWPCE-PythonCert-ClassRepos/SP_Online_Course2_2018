d997a82b058b3409f1d40eee0c14779479076d46
Dennis Lee <dennis271lee@yahoo.com>
2018-10-09 03:57:39 -0700

    Implemented neo4j extra functionality for NOSQL, though I still need to eliminate duplicates.

--- students/DennisLee/lesson08/nosql/neo4j_script.py
+++ students/DennisLee/lesson08/nosql/neo4j_script.py
@@ -25,13 +25,15 @@ def run_example():
 
         log.info('Adding a few Person nodes')
         log.info('The cyph language is analagous to sql for neo4j')
-        for first, last in [('Bob', 'Jones'),
-                            ('Nancy', 'Cooper'),
-                            ('Alice', 'Cooper'),
-                            ('Fred', 'Barnes'),
-                            ('Mary', 'Evans'),
-                            ('Marie', 'Curie'),
-                            ]:
+        people = [
+            ('Bob', 'Jones'),
+            ('Nancy', 'Cooper'),
+            ('Alice', 'Cooper'),
+            ('Fred', 'Barnes'),
+            ('Mary', 'Evans'),
+            ('Marie', 'Curie'),
+        ]
+        for first, last in people:
             cyph = "CREATE (n:Person {first_name:'%s', last_name: '%s'})" % (
                 first, last)
             session.run(cyph)
@@ -84,7 +86,7 @@ def run_example():
 
             session.run(cypher)
 
-        print("Step 6: Find all of Marie's friends?")
+        log.info("Step 6: Find all of Marie's friends?")
         cyph = """
           MATCH (marie {first_name:'Marie', last_name:'Curie'})
                 -[:FRIEND]->(friends)
@@ -95,3 +97,114 @@ def run_example():
         for rec in result:
             for friend in rec.values():
                 print(friend['first_name'], friend['last_name'])
+
+        log.info("Step 7: Add more people")
+        emcees = [
+                ('Bob', 'Barker'),
+                ('Tom', 'Kennedy'),
+                ('Regis', 'Philbin'),
+                ('Vicki', 'Lawrence')
+        ]
+        for first, last in emcees:
+            cyph = "CREATE (n:Person {first_name:'%s', last_name: '%s'})" % (
+                first, last)
+            session.run(cyph)
+        people.extend(emcees)
+
+        log.info("Step 8: Add colors")
+        color_list = ['Red', 'White', 'Blue', 'Green',
+                      'Purple', 'Orange', 'Yellow', 'Brown', 'Black']
+        for color in color_list:
+            cyph = "CREATE (c:Color {color: '%s'})" % color
+            session.run(cyph)
+
+        log.info("Step 9: Set color likes")
+        log.info("Set favorite color")
+        for first, last, fav_color in [
+                ('Bob', 'Jones', 'Brown'),
+                ('Nancy', 'Cooper', 'Orange'),
+                ('Alice', 'Cooper', 'Yellow'),
+                ('Fred', 'Barnes', 'Blue'),
+                ('Mary', 'Evans', 'Yellow'),
+                ('Marie', 'Curie', 'White'),
+                ('Bob', 'Barker', 'Purple'),
+                ('Tom', 'Kennedy', 'Brown'),
+                ('Regis', 'Philbin', 'Green'),
+                ('Vicki', 'Lawrence', 'Red')
+        ]:
+            cypher = """
+              MATCH (p:Person {first_name:'%s', last_name:'%s'})
+              CREATE (p)-[likes:LIKES]->(c:Color {color: '%s'})
+              RETURN p
+            """ % (first, last, fav_color)
+            session.run(cypher)
+
+        log.info("Set women to like blue")
+        for first, last in [
+                ('Nancy', 'Cooper'),
+                ('Alice', 'Cooper'),
+                ('Mary', 'Evans'),
+                ('Marie', 'Curie'),
+                ('Vicki', 'Lawrence')
+        ]:
+            cypher = """
+              MATCH (p:Person {first_name:'%s', last_name:'%s'})
+              CREATE (p)-[likes:LIKES]->(c:Color {color: 'Blue'})
+              RETURN p
+            """ % (first, last)
+            session.run(cypher)
+
+        log.info("Set men to like black")
+        for first, last in [
+                ('Bob', 'Jones'),
+                ('Fred', 'Barnes'),
+                ('Bob', 'Barker'),
+                ('Tom', 'Kennedy'),
+                ('Regis', 'Philbin')
+        ]:
+            cypher = """
+              MATCH (p:Person {first_name:'%s', last_name:'%s'})
+              CREATE (p)-[likes:LIKES]->(c:Color {color: 'Black'})
+              RETURN p
+            """ % (first, last)
+            session.run(cypher)
+
+        log.info("Set Bobs to like green")
+        cypher = """
+            MATCH (p:Person {first_name: 'Bob'})
+            CREATE (p)-[likes:LIKES]->(c:Color {color: 'Green'})
+            RETURN p
+        """
+        session.run(cypher)
+
+        log.info("Set Coopers to like purple")
+        cypher = """
+            MATCH (p:Person {last_name: 'Cooper'})
+            CREATE (p)-[likes:LIKES]->(Color {color: 'Purple'})
+            RETURN p
+        """
+        session.run(cypher)
+
+        log.info("Step 10: List all people who like a color")
+        for color in color_list:
+            cypher = (
+                "MATCH ((p)-[:LIKES]->(c:Color {color: '%s'})) "
+                "RETURN p" % color
+            )
+            result = session.run(cypher)
+            print(f"People who like {color}: ")
+            for person in result:
+                for val in person.values():
+                    print(f"\t{val['first_name']} {val['last_name']}")
+
+        log.info("Step 11: List each person's color likes")
+        for first, last in people:
+            cypher = (
+                "MATCH ((p {first_name:'%s', last_name:'%s'})-[:LIKES]->(c)) "
+                "RETURN c" % (first, last)
+            )
+            result = session.run(cypher)
+            print(f"{first} {last}'s favorite colors: ")
+            for color in result:
+                for val in color.values():
+                    print(f"\t{val['color']}")
