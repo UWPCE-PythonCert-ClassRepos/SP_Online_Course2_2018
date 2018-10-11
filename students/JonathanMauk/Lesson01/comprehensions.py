7b9f2fd714118c6c12b4df20607584168cf17a57
Jonathan Mauk <jorauk@uw.edu>
2018-10-11 14:30:19 -0700

    Started iterator assignment.

--- students/JonathanMauk/Lesson01/comprehensions.py
+++ students/JonathanMauk/Lesson01/comprehensions.py
@@ -5,5 +5,10 @@ music = pd.read_csv("featuresdf.csv")
 song_list = [x for x in zip(music.danceability, music.loudness, music.name, music.artists)
              if x[0] > 0.8 and x[1] < -5]
 
-for x in song_list:
-    print(x)
+quiet_and_danceable = sorted(song_list, reverse=True)
+
+print("Type five songs by danceability with loudness less than -5:\n")
+number = 1
+for x in quiet_and_danceable[:5]:
+    print(str(number) + ". " + x[2] + "\n   by " + x[3] + "\n   (Danceability: " + str(x[0]) + ")\n")
+    number = number + 1
