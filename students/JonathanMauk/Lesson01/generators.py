0dc5058538545feff400233e95d9a111b03b5b59
Jonathan Mauk <jorauk@uw.edu>
2018-10-11 15:51:06 -0700

    Wrote generators for Fibonacci sequence and prime numbers. Ran py.test with test file to ensure code works as intended.

--- students/JonathanMauk/Lesson01/generators.py
+++ students/JonathanMauk/Lesson01/generators.py
@@ -23,16 +23,23 @@ def doubler(a=1):
         a *= 2
 
 
-def fib():
+def fib(a=1, b=1):
     """
     The Fibonacci sequence as a generator:
         f(n) = f(n-1) + f(n-2)
         1, 1, 2, 3, 5, 8, 13, 21, 34…
     """
+    while True:
+        yield a
+        a, b = b, a + b
 
 
-def prime():
+def prime(a=2):
     """
     Generate the prime numbers (numbers only divisible by them self and 1):
         2, 3, 5, 7, 11, 13, 17, 19, 23…
     """
+    while True:
+        if not [b for b in range(2, a) if a % b == 0]:
+            yield a
+        a += 1
