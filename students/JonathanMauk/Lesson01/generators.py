794f5b7b8d4804a6cbce86f0cb127ad94247b9b1
Jonathan Mauk <jorauk@uw.edu>
2018-10-11 15:10:16 -0700

    Added descriptions of generators from assignment.

---
+++ students/JonathanMauk/Lesson01/generators.py
@@ -0,0 +1,38 @@
+def intsum():
+    """
+    Sum of the integers:
+        keep adding the next integer
+
+        0 + 1 + 2 + 3 + 4 + 5 + …
+
+        so the sequence is:
+
+        0, 1, 3, 6, 10, 15 …..
+    """
+
+def doubler():
+    """
+    Doubler:
+        Each value is double the previous value:
+
+        1, 2, 4, 8, 16, 32,
+    """
+
+def fib():
+    """
+    The Fibonacci sequence as a generator:
+
+        f(n) = f(n-1) + f(n-2)
+
+        1, 1, 2, 3, 5, 8, 13, 21, 34…
+    """
+
+def prime():
+    """
+    Generate the prime numbers (numbers only divisible by them self and 1):
+
+        2, 3, 5, 7, 11, 13, 17, 19, 23…
+    """
+
+
+
