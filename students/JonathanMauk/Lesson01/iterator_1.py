70c4e6d1054ef74c58eae3bd009e4dc1d362a4b6
Jonathan Mauk <jorauk@uw.edu>
2018-10-11 14:35:06 -0700

    Updated IterateMe_2 to behave more like range().

--- students/JonathanMauk/Lesson01/iterator_1.py
+++ students/JonathanMauk/Lesson01/iterator_1.py
@@ -33,15 +33,16 @@ class IterateMe_2:
     Extension of IterateMe_1 to make it more like range().
     """
 
-    def __init__(self, stop=5):
-        self.current = -1
+    def __init__(self, start, stop, step=1):
+        self.current = start - step
         self.stop = stop
+        self.step = step
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        self.current += 1
+        self.current += self.step
         if self.current < self.stop:
             return self.current
         else:
@@ -55,5 +56,5 @@ if __name__ == "__main__":
         print(i)
 
     print("Testing the enhanced iterator:")
-    for i in IterateMe_2():
+    for i in IterateMe_2(20, 60, 5):
         print(i)
