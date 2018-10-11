dc0151dbcf50a54f187a968fdbfc478f3a23a097
Jonathan Mauk <jorauk@uw.edu>
2018-10-11 14:59:49 -0700

    Final touches to comprehension assignment.

--- students/JonathanMauk/Lesson01/comprehensions.py
+++ students/JonathanMauk/Lesson01/comprehensions.py
@@ -12,3 +12,27 @@ number = 1
 for x in quiet_and_danceable[:5]:
     print(str(number) + ". " + x[2] + "\n   by " + x[3] + "\n   (Danceability: " + str(x[0]) + ")\n")
     number = number + 1
+
+'''
+Top five songs, per the above code:
+
+1. Bad and Boujee (feat. Lil Uzi Vert)
+   by Migos
+   (Danceability: 0.927)
+
+2. Fake Love
+   by Drake
+   (Danceability: 0.927)
+
+3. HUMBLE.
+   by Kendrick Lamar
+   (Danceability: 0.904)
+
+4. Bank Account
+   by 21 Savage
+   (Danceability: 0.884)
+
+5. You Don't Know Me - Radio Edit
+   by Jax Jones
+   (Danceability: 0.8759999999999999)
+'''
