4c59874b7476dd07f74bf71fba9d206092151a0c
Jonathan Mauk <jorauk@uw.edu>
2018-10-18 16:50:32 -0700

    Wrote basic closure to capture high energy songs.

--- students/JonathanMauk/Lesson02/closures.py
+++ students/JonathanMauk/Lesson02/closures.py
@@ -2,3 +2,11 @@ import pandas as pd
 
 
 music = pd.read_csv("featuresdf.csv")
+
+
+def high_energy_songs(energy=0.8):  # Default value is 0.8 but can be defined by user.
+    def find_songs(song_list):
+        return [song for song in zip(song_list.artists, song_list.name, song_list.energy) if song[2] > energy]
+    return find_songs
+
+
