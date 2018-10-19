21bcde2ea151fe8e90b40803f82d8c4f8677b2c7
Jonathan Mauk <jorauk@uw.edu>
2018-10-19 14:26:19 -0700

    Copied over tests from assignment. Also added a new medium_locke that is equal to the number of boats, to meet all cases. Also started adding skeleton of functions to the Locke class.

--- students/JonathanMauk/Lesson03/locke.py
+++ students/JonathanMauk/Lesson03/locke.py
@@ -2,3 +2,31 @@ class Locke:
 
     def __init__(self, capacity):
         self.capacity = capacity
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self):
+        return self
+
+    def move_boats_through(self):
+        return self
+
+
+if __name__ == "__main__":
+    small_locke = Locke(5)
+    medium_locke = Locke(8)
+    large_locke = Locke(10)
+    boats = 8
+
+    # Should raise an exception.
+    with small_locke as locke:
+        locke.move_boats_through(boats)
+
+    # Should run without any issues--this fills but does not exceed capacity.
+    with medium_locke as locke:
+        locke.move_boats_through(boats)
+
+    # Should run without any issues.
+    with large_locke as locke:
+        locke.move_boats_through(boats)
