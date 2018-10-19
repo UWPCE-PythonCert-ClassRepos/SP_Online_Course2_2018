6fb8e2e0f87a6182deb9e55d93073c617979dbe4
Jonathan Mauk <jorauk@uw.edu>
2018-10-19 14:41:10 -0700

    Wrote out function to detect number of boats and compare to locke capacity. In testing, ran into the "__exit__() takes 1 positional argument but 4 were given" error.

--- students/JonathanMauk/Lesson03/locke.py
+++ students/JonathanMauk/Lesson03/locke.py
@@ -4,16 +4,18 @@ class Locke:
         self.capacity = capacity
 
     def __enter__(self):
-        print("Stopping the pumps.\nOpening the doors.")
+        print("Stopping the pumps.\nOpening the doors.\nBoats entering locke.")
         return self
 
-    def move_boats_through(self):
-        return self
-
-    def __exit__(self):
+    def move_boats_through(self, boats_count):
+        if boats_count > self.capacity:
+            raise Exception("Too many boats. Locke has a capacity of {self.capacity}. Please try again.")
         print("Closing the doors.\nRestarting the pumps.")
         return self
 
+    def __exit__(self):
+        print("Stopping the pumps.\nOpening the doors.\nBoats exiting locke.")
+        return self
 
 
 if __name__ == "__main__":
@@ -23,8 +25,8 @@ if __name__ == "__main__":
     boats = 8
 
     # Should raise an exception.
-    with small_locke as locke:
-        locke.move_boats_through(boats)
+    # with small_locke as locke:
+    #     locke.move_boats_through(boats)
 
     # Should run without any issues--this fills but does not exceed capacity.
     with medium_locke as locke:
