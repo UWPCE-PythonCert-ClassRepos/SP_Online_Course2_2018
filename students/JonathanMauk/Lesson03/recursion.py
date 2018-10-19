ad08bfa77a8466d6e675427051bff22f9cdf69da
Jonathan Mauk <jorauk@uw.edu>
2018-10-19 15:07:51 -0700

    Added some basic error handling for when invalid values (floats or negatives) are entered. Also added a special if-statement to handle 0 and 1, since the factorial of both is 1.

--- students/JonathanMauk/Lesson03/recursion.py
+++ students/JonathanMauk/Lesson03/recursion.py
@@ -1,4 +1,9 @@
-def factorial(self):
+def factorial(n):
     """Returns the factorial of a given non-negative integer,
     the product of all positive integers less than or equal to that number."""
-    return self
+    if n != int:
+        print("Please enter an integer.")
+    elif n < 0:
+        print("Please enter a non-negative integer.")
+    elif n == 0 or n == 1:
+        return 1
