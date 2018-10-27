4257e6c05cf38de9aa4a878a444add1375cb6229
mmsunday1 <matt.m.sunday@gmail.com>
2018-10-27 14:32:57 -0700

    completed calculator testing exercise

---
+++ students/M_Sunday/lesson06/calculator/calculator/adder.py
@@ -0,0 +1,15 @@
+"""
+This module provides an Adder class
+"""
+
+
+class Adder:
+    """
+    This class allows for calculator addition
+    """
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """
+        This method calculates, and returns, the addition of the two operands
+        """
+        return operand_1 + operand_2
---
+++ students/M_Sunday/lesson06/calculator/calculator/subtracter.py
@@ -0,0 +1,16 @@
+"""
+This module provides a subtraction operator
+"""
+
+
+class Subtracter:
+    """
+    This class allows for calculator subtraction
+    """
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """
+        This method calculates, and returns, the subtraction of the two
+        operands
+        """
+        return operand_1 - operand_2
