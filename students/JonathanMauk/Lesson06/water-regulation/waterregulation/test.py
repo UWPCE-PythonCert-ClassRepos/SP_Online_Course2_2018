c6f4b8461738b3a09b60dc8989e3e4ebe8052668
Jonathan Mauk <jorauk@uw.edu>
2018-11-01 20:33:44 -0700

    Began implementing TODO for DeciderTests.

--- students/JonathanMauk/Lesson06/water-regulation/waterregulation/test.py
+++ students/JonathanMauk/Lesson06/water-regulation/waterregulation/test.py
@@ -14,26 +14,36 @@ from .decider import Decider
 
 class DeciderTests(unittest.TestCase):
     """
-    Unit tests for the Decider class
+    Unit tests for the Decider class.
     """
 
-    # TODO: write a test or tests for each of the behaviors defined for
-    #       Decider.decide
+    def test_decider(self):
+        """Method for DeciderTests. Should test every decider scenario."""
 
-    def test_dummy(self):
-        """
-        Just some example syntax that you might use
-        """
+        di = Decider(100, 0.05)
 
-        pump = Pump('127.0.0.1', 8000)
-        pump.set_state = MagicMock(return_value=True)
+        scenario1
+        scenario2
+        scenario3
+        scenario4
+        scenario5
+        scenario6
+        scenario7
 
-        self.fail("Remove this test.")
+    # def test_dummy(self):
+    #     """
+    #     Just some example syntax that you might use
+    #     """
+    #
+    #     pump = Pump('127.0.0.1', 8000)
+    #     pump.set_state = MagicMock(return_value=True)
+    #
+    #     self.fail("Remove this test.")
 
 
 class ControllerTests(unittest.TestCase):
     """
-    Unit tests for the Controller class
+    Unit tests for the Controller class.
     """
 
     # TODO: write a test or tests for each of the behaviors defined for
