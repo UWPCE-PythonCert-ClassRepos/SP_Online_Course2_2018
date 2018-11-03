0d6a3023de09f8d1d6590f18dbee3be3818c1342
Jonathan Mauk <jorauk@uw.edu>
2018-11-02 21:08:34 -0700

    Updated redis_script.py to include requested changes.

--- students/JonathanMauk/Lesson08/nosql/src/redis_script.py
+++ students/JonathanMauk/Lesson08/nosql/src/redis_script.py
@@ -9,7 +9,7 @@ import utilities
 
 def run_example():
     """
-        uses non-presistent Redis only (as a cache)
+        uses non-persistent Redis only (as a cache)
 
     """
 
@@ -19,40 +19,23 @@ def run_example():
         log.info('Step 1: connect to Redis')
         r = login_database.login_redis_cloud()
         log.info('Step 2: cache some data in Redis')
-        r.set('andy', 'andy@somewhere.com')
+        r.set('test', 'test@testing.net')
 
-        log.info('Step 2: now I can read it')
-        email = r.get('andy')
-        log.info('But I must know the key')
-        log.info(f'The results of r.get: {email}')
+        log.info('Step 2: adding some data to cache')
+        r.hmset('Keith', {'phone': '425-123-4567', 'zip code': '91234'})
+        r.hmset('Allison', {'phone': '206-987-6543', 'zip code': '94321'})
+        r.hmset('Jesse', {'phone': '360-123-4567', 'zip code': '90101'})
+        r.hmset('Lauren', {'phone': '425-606-6060', 'zip code': '96060'})
+        r.hmset('John', {'phone': '206-206-2062', 'zip code': '92062'})
+        r.hmset('Merlin', {'phone': '800-900-1000', 'zip code': '87654'})
 
-        log.info('Step 3: cache more data in Redis')
-        r.set('pam', 'pam@anywhere.com')
-        r.set('fred', 'fred@fearless.com')
+        log.info('Step 3: return Merlin\'s zip code.')
+        merlin_zip = r.hmget('Merlin', 'zip code')
+        log.info(f'Merlin\'s zip code: {merlin_zip}')
 
-        log.info('Step 4: delete from cache')
-        r.delete('andy')
-        log.info(f'r.delete means andy is now: {email}')
-
-        log.info(
-            'Step 6: Redis can maintain a unique ID or count very efficiently')
-        r.set('user_count', 21)
-        r.incr('user_count')
-        r.incr('user_count')
-        r.decr('user_count')
-        result = r.get('user_count')
-        log.info('I could use this to generate unique ids')
-        log.info(f'Redis says 21+1+1-1={result}')
-
-        log.info('Step 7: richer data for a SKU')
-        r.rpush('186675', 'chair')
-        r.rpush('186675', 'red')
-        r.rpush('186675', 'leather')
-        r.rpush('186675', '5.99')
-
-        log.info('Step 8: pull some data from the structure')
-        cover_type = r.lindex('186675', 2)
-        log.info(f'Type of cover = {cover_type}')
+        log.info('Step 4: return Merlin\'s phone number.')
+        merlin_phone = r.hmget('Merlin', 'phone number')
+        log.info(f'Merlin\'s phone number: {merlin_phone}')
 
     except Exception as e:
         print(f'Redis error: {e}')
