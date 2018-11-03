d497a3e458d1b85a9f153caa90158789ce689e5c
Jonathan Mauk <jorauk@uw.edu>
2018-11-02 20:49:26 -0700

    Added new type of furniture and separated out colors.

--- students/JonathanMauk/Lesson08/nosql/src/learn_data.py
+++ students/JonathanMauk/Lesson08/nosql/src/learn_data.py
@@ -10,40 +10,52 @@ def get_furniture_data():
 
     furniture_data = [
         {
-            'product': 'Red couch',
+            'product type': 'Couch',
+			'product color': 'Red',
             'description': 'Leather low back',
             'monthly_rental_cost': 12.99,
             'in_stock_quantity': 10
         },
         {
-            'product': 'Blue couch',
+            'product type': 'Couch',
+			'product color': 'Blue',
             'description': 'Cloth high back',
             'monthly_rental_cost': 9.99,
             'in_stock_quantity': 3
         },
         {
             'product': 'Coffee table',
+			'product color': 'White',
             'description': 'Plastic',
             'monthly_rental_cost': 2.50,
             'in_stock_quantity': 25
         },
         {
-            'product': 'Red couch',
+            'product type': 'Couch',
+			'product color': 'Red',
             'description': 'Leather high back',
             'monthly_rental_cost': 15.99,
             'in_stock_quantity': 17
         },
         {
-            'product': 'Blue recliner',
+            'product': 'Recliner',
+			'product color': 'Blue',
             'description': 'Leather high back',
             'monthly_rental_cost': 19.99,
             'in_stock_quantity': 6
         },
         {
             'product': 'Chair',
+			'product color': 'Black',
             'description': 'Plastic',
             'monthly_rental_cost': 1.00,
             'in_stock_quantity': 45
+        },
+            'product': 'Loveseat',
+			'product color': 'Brown',
+            'description': 'Leather high back',
+            'monthly_rental_cost': 17.99,
+            'in_stock_quantity': 25
         }
     ]
     return furniture_data
