da81708f580ca26eef10eef66ecf5ee7416d96c3
Jonathan Mauk <jorauk@uw.edu>
2018-11-02 19:48:58 -0700

    Imported recursive factorial code from Lesson 03.

--- students/JonathanMauk/Lesson10/generators_tests.py
+++ students/JonathanMauk/Lesson10/generators_tests.py
@@ -2,31 +2,6 @@ import math
 import time
 
 
-def intsum(a=0, b=0):
-    """
-    Sum of the integers:
-        keep adding the next integer
-        0 + 1 + 2 + 3 + 4 + 5 + …
-        so the sequence is:
-        0, 1, 3, 6, 10, 15 …..
-    """
-    while True:
-        b += a
-        yield b
-        a += 1
-
-
-def doubler(a=1):
-    """
-    Doubler:
-        Each value is double the previous value:
-        1, 2, 4, 8, 16, 32,
-    """
-    while True:
-        yield a
-        a *= 2
-
-
 def fib(a=1, b=1):
     """
     The Fibonacci sequence as a generator:
@@ -38,13 +13,19 @@ def fib(a=1, b=1):
         a, b = b, a + b
 
 
-def prime(a=2):
+def factorial(n):
     """
-    Generate the prime numbers (numbers only divisible by them self and 1):
-        2, 3, 5, 7, 11, 13, 17, 19, 23…
+    Returns the factorial of a given non-negative integer,
+    the product of all positive integers less than or equal to that number.
     """
-    while True:
-        if not [b for b in range(2, a) if a % b == 0]:
-            yield a
-        a += 1
+    if n == float:
+        print(f"Please enter an integer. You entered {n}.")
+        return
+    elif n < 0:
+        print(f"Please enter a non-negative integer. You entered {n}.")
+        return
+    elif n <= 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
 
