d4ec64eaceeb654054e4b44d1393637d55590270
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-11-05 14:24:07 -0800

    Fixed the capitol 'P' in my print() statement.

--- students/toddschultz/session02/closures.py
+++ students/toddschultz/session02/closures.py
@@ -1,16 +1,9 @@
 import pandas as pd
 music = pd.read_csv("featuresdf.csv")
 
-def artists_gen():
-    ''' Write a generator to find and print all of your favorite artist’s tracks. 
-    As it turns out The Weeknd has two tracks in this list: Starboy and I Feel It Comming. '''
-    index = 0
-    while index < len(music.artists):
-        if music.artists[index] == "The Weeknd":
-            yield (music.name[index])
-        index += 1
-
 def energy_closure():
+    ''' Using the same data set, write a closure to capture high energy tracks. We will define high energy tracks as anything over 0.8. 
+    Submit your code and the tracks it finds, artist name, track name and energy value.'''
     songs = [x for x in zip(music.energy, music.artists, music.name) if x[0] > 0.8]
     #sorted_songs = songs.sorted(reverse=True)
     def print_energy_tracks():
@@ -19,12 +12,29 @@ def energy_closure():
 
     print_energy_tracks()
 
-
-
-results = artists_gen()
-results_sorted = sorted(results, reverse=True)
-print("Tracks by The Weeknd are as follows: ")
-for i in results_sorted:
-    print(i)
-
+print("The following are HIGH ENERGY tracks!!!")
 energy_closure()
+
+
+'''
+HIGH ENERGY TRACKS
+0.81    Luis Fonsi          Despacito - Remix                  
+0.81    Post Malone         Congratulations                    
+0.82    Jason Derulo        Swalla (feat. Nicki Minaj & Ty Dolla $ign)
+0.83    Ed Sheeran          Castle on the Hill                 
+0.81    Imagine Dragons     Thunder                            
+ 0.8    Danny Ocean         Me Rehúso                          
+0.88    Ed Sheeran          Galway Girl                        
+0.81    The Weeknd          I Feel It Coming                   
+0.84    Starley             Call On Me - Ryan Riback Extended Remix
+0.84    Martin Jensen       Solo Dance                         
+0.82    Enrique Iglesias    SUBEME LA RADIO                    
+0.87    Maggie Lindemann    Pretty Girl - Cheat Codes X CADE Remix
+ 0.8    Bruno Mars          24K Magic                          
+ 0.8    Katy Perry          Chained To The Rhythm              
+0.86    Wisin               Escápate Conmigo                   
+0.93    Steve Aoki          Just Hold On                       
+0.84    CNCO                Reggaetón Lento (Bailemos)         
+0.81    The Vamps           All Night                          
+0.86    The Chainsmokers    Don't Let Me Down 
+'''
