0a0fecb84535b903e1b92cfaf6484966c881addb
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-11-05 12:22:53 -0800

    I was able to make my iterator work like range with a class, and keeping track of the current index and using StopIteration.

--- students/toddschultz/session01/iterator_2.py
+++ students/toddschultz/session01/iterator_2.py
@@ -1,8 +1,28 @@
-def iterator_2(start, stop, step=1):
-	while start < stop:
-		yield start
-		start = start+step
+class Iterator_2:
 
-qwerty = iterator_2(10, 30, 3)
-for i in qwerty:
-	print(i)
+    def __init__(self, start, stop, step=1):
+        self.index = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        return self
+    
+    def __next__(self):
+        self.index += self.step
+        if self.index < self.stop:
+            return self.index
+        else:
+            raise StopIteration
+
+if __name__ == "__main__":
+
+    print("Testing Iterator_2")
+    qwerty = Iterator_2(0,40,2)
+    for i in qwerty:
+        print(i)
+        if i == 16:
+            break
+    print("This is Iterator_2 after the break.")
+    for i in qwerty:
+        print(i)     
