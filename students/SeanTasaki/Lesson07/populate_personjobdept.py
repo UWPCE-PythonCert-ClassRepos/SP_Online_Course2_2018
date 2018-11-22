6ace39cfb10e610b54f51b19753278511edc6919
seaneightysix <sean.eightysix@GMAIL.com>
2018-11-21 18:05:54 -0600

    waterregulator script complete

--- students/SeanTasaki/Lesson07/populate_personjobdept.py
+++ students/SeanTasaki/Lesson07/populate_personjobdept.py
@@ -4,8 +4,95 @@ from personjobdept_model import *
 import logging
 
 
+def populate_db():
+    """
+    add person data to database
+    """
 
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
 
+    database = SqliteDatabase('personjob.db')
+
+    logger.info('Working with Person class')
+    logger.info('Note how I use constants and a list of tuples as a simple schema')
+    logger.info('Normally you probably will have prompted for this from a user')
+
+    PERSON_NAME = 0
+    LIVES_IN_TOWN = 1
+    NICKNAME = 2
+
+    people = [
+        ('Andrew', 'Sumner', 'Andy'),
+        ('Peter', 'Seattle', None),
+        ('Susan', 'Boston', 'Beannie'),
+        ('Pam', 'Coventry', 'PJ'),
+        ('Steven', 'Colchester', None),
+        ]
+
+    logger.info('Creating Person records: iterate through the list of tuples')
+    logger.info('Prepare to explain any errors with exceptions')
+    logger.info('and the transaction tells the database to fail on error')
+    '''
+    try:
+        database.connect()
+        database.execute_sql('PRAGMA foreign_keys = ON;')
+        for person in people:
+            with database.transaction():
+                new_person = Person.create(
+                        person_name = person[PERSON_NAME],
+                        lives_in_town = person[LIVES_IN_TOWN],
+                        nickname = person[NICKNAME])
+                new_person.save()
+                logger.info('Database add successful')
+
+        logger.info('Print the Person records we saved...')
+        for saved_person in Person:
+            logger.info(f'{saved_person.person_name} lives in {saved_person.lives_in_town} ' +\
+                f'and likes to be known as {saved_person.nickname}')
+
+    except Exception as e:
+        logger.info(f'Error creating = {person[PERSON_NAME]}')
+        logger.info(e)
+        logger.info('See how the database protects our data')
+
+    finally:
+        logger.info('database closes')
+        database.close()
+    '''
+    DEPT_NUM = 0
+    DEPT_NAME = 1
+    DEPT_MGR = 2
+
+    departments = [('Z100', 'Operations', 'BigBoss'),
+                   ('A100', 'Accounting', 'Allen'),
+                   ('M100', 'Marketing', 'Mindy'),
+                   ('H100', 'HR', 'Lucy')]
+
+    logger.info("Add departments to database")
+    try:
+        database.connect()
+        database.execute_sql('PRAGMA foreign_keys = ON;')
+        for dept in departments:
+            with database.transaction():               
+                cur_dept = Department.create(dept_num=dept[DEPT_NUM],
+                                            dept_name=dept[DEPT_NAME],
+                                            dept_manager=dept[DEPT_MGR])
+                cur_dept.save()
+                logger.info(f'Added {dept_name} successfully to Department table.')  
+    
+    except Exception as e:
+        logger.info(f'Error creating entry for {dept[DEPT_NAME]}')
+        logger.info(e)
+
+    finally:
+        logger.info('close database')
+        database.close() 
+
+if __name__ == '__main__':
+    populate_db()
+
+'''
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -124,4 +211,4 @@ if __name__ == '__main__':
     add_departments(departments)
     add_jobs(jobs)
     
-
+'''
