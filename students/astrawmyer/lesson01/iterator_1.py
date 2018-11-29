d53c811bfa599969c0fc965ca01fd3cffa4cd95f
Adam Strawmyer <adam@strawmyer.com>
2018-11-28 18:35:30 -0600

    L01 iterator work done

--- students/astrawmyer/lesson01/iterator_1.py
+++ students/astrawmyer/lesson01/iterator_1.py
@@ -27,9 +27,51 @@ class IterateMe_1:
         else:
             raise StopIteration
 
+class IterateMe_2:
+    """
+    About as simple an iterator as you can get:
+
+    returns the sequence of numbers from zero to 4
+    ( like range(4) )
+    """
+
+    def __init__(self, start, stop, step=1):
+        self.current = start - 1
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        self.current = self.start-1
+        return self
+
+    def __next__(self):
+        self.current += self.step
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
+
 
 if __name__ == "__main__":
 
     print("Testing the iterator")
     for i in IterateMe_1():
+        print(i)
+
+    it = IterateMe_2(2, 20, 1)
+    for i in it:
+        if i > 10:  break
+        print(i)
+
+    for i in it:
+        print(i)
+
+    print("Testing Range function")
+    r = range(2, 20, 1)
+    for i in r:
+        if i > 10:  break
+        print(i)
+
+    for i in r:
         print(i)
