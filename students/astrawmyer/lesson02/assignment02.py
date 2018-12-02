1730a6ef7e6e94972443fbda0c32f4e30df8aa70
Adam Strawmyer <adam@strawmyer.com>
2018-12-02 17:48:57 -0600

    L02. set up generator to return artist name only

--- students/astrawmyer/lesson02/assignment02.py
+++ students/astrawmyer/lesson02/assignment02.py
@@ -1,2 +1,11 @@
 import pandas as pd
 music = pd.read_csv("featuresdf.csv")
+
+def kendrick_generator():
+    for x in music.artists:
+        if x == 'Kendrick Lamar':
+            yield x
+            
+kendrick = kendrick_generator()
+for x in kendrick_generator():
+    print(x)
