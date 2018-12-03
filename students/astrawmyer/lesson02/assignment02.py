4d85bef28a26840918c2d47015ee7d52a1b1e89e
Adam Strawmyer <adam@strawmyer.com>
2018-12-02 18:08:22 -0600

    L02. generator complete. returns the name of the tracks for an artist.

--- students/astrawmyer/lesson02/assignment02.py
+++ students/astrawmyer/lesson02/assignment02.py
@@ -2,9 +2,9 @@ import pandas as pd
 music = pd.read_csv("featuresdf.csv")
 
 def favorite_generator(artist):
-    for x in music.artists:
-        if x == artist:
-            yield x
-            
+    for x in zip(music.artists,music.name):
+        if x[0] == artist:
+            yield x[1]
+
 for x in favorite_generator('Kendrick Lamar'):
     print(x)
