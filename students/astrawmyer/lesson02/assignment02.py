b5c94d5a80594cf250e2387f5a7a9adbd544ea20
Adam Strawmyer <adam@strawmyer.com>
2018-12-03 20:35:57 -0600

    L02 got closure to work by adjusting variable references. added display

--- students/astrawmyer/lesson02/assignment02.py
+++ students/astrawmyer/lesson02/assignment02.py
@@ -9,19 +9,18 @@ def favorite_generator(artist):
 for x in favorite_generator('Kendrick Lamar'):
     print(x)
 
-#use closure to capture high energy tracks (>0.8)
+# Use closure to capture high energy tracks (>0.8).
 
 song_list = zip(music.artists,music.name,music.energy)
 
 def high_energy(energy_level):
     def high(music):
-        res = [(artist, song, energy) for (artist, song, energy) in song_list if energy > energy_level]
-        print("k")
+        res = [(artist, song, energy) for (artist, song, energy) in music if energy > energy_level]
         return res
     return high
 
-""" result = [(artist, song, energy) for (artist, song, energy) in song_list if energy > 0.8]
-print(result) """
-
 result = high_energy(0.8)
-print(result)
+high_energy_list = result(song_list)
+
+for i in high_energy_list:
+    print(i[0] + " - " + i[1] + " - " + str(i[2]))
