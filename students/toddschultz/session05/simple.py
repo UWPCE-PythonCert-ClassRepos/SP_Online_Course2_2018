59ecbec12bb69c7294c660f809951997854ad242
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-12-04 12:13:46 -0800

    Done! For some reason I had to add "from logging import handlers" for SysLogHandler() to work. Simply putting in "import logging" did not work for that handler as it did for the other ones.

--- students/toddschultz/session05/simple.py
+++ students/toddschultz/session05/simple.py
@@ -1,5 +1,6 @@
 #simple.py
 import logging
+from logging import handlers
 from time import strftime
 
 format = "%(asctime)s %(filename)s:%(lineno)-3d %(levelname)s %(message)s"
@@ -17,7 +18,7 @@ console_handler = logging.StreamHandler()
 console_handler.setLevel(logging.DEBUG)          
 console_handler.setFormatter(formatter) 
 
-syslog_handler = logging.SysLogHandler()        
+syslog_handler = logging.handlers.SysLogHandler()        
 syslog_handler.setLevel(logging.ERROR)          
 syslog_handler.setFormatter(formatter_no_time)            
 
