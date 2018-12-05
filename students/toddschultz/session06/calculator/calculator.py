cd08de7544661761adbb3d50bea135a8f6d61093
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-12-05 14:43:43 -0800

    Final commit of all files.

--- students/toddschultz/session06/calculator/calculator.py
+++ students/toddschultz/session06/calculator/calculator.py
@@ -1,8 +1,12 @@
+''' My Digital Calculator '''
+
 from .exceptions import InsufficientOperands
 
-class Calculator(object):
+class Calculator():
+    ''' Base class '''
 
     def __init__(self, adder, subtracter, multiplier, divider):
+        ''' Initializer '''
         self.adder = adder
         self.subtracter = subtracter
         self.multiplier = multiplier
@@ -11,9 +15,11 @@ class Calculator(object):
         self.stack = []
 
     def enter_number(self, number):
+        ''' inserts a new number in the second position '''
         self.stack.insert(1, number)
 
     def _do_calc(self, operator):
+        ''' Execute operations with the proper operator and catches InsufficientOperands '''
         try:
             result = operator.calc(self.stack[0], self.stack[1])
         except IndexError:
@@ -23,13 +29,17 @@ class Calculator(object):
         return result
 
     def add(self):
+        ''' returns numbers added '''
         return self._do_calc(self.adder)
 
     def subtract(self):
+        ''' returns numbers subtracted '''
         return self._do_calc(self.subtracter)
 
     def multiply(self):
+        ''' returns numbers multiplied '''
         return self._do_calc(self.multiplier)
 
     def divide(self):
+        ''' returns numbers divided '''
         return self._do_calc(self.divider)
