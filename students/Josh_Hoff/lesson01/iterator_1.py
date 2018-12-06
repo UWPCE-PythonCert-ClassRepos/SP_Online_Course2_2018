0471cc476295e06b3c022f772ce797972b94006f
Josh Hoff <jhoff1985@hotmail.com>
2018-12-05 19:06:59 -0800

    Edited the iterator_1 program and gernerator program to better understand iterators, iterables, and generators.

--- students/Josh_Hoff/lesson01/iterator_1.py
+++ students/Josh_Hoff/lesson01/iterator_1.py
@@ -13,15 +13,32 @@ class IterateMe_1:
     ( like range(4) )
     """
 
-    def __init__(self, stop=5):
-        self.current = -1
+    def __init__(self, start=0, stop=5, step=1):
+        self.current = start - 1
         self.stop = stop
+        self.step = step
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        self.current += 1
+        self.current += self.step
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
+            
+class IterateMe_2:
+    def __init__(self, start=0, stop=5, step=1):
+        self.current = start - step
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        self.current += self.step
         if self.current < self.stop:
             return self.current
         else:
@@ -33,3 +50,15 @@ if __name__ == "__main__":
     print("Testing the iterator")
     for i in IterateMe_1():
         print(i)
+    print('Testing iterator_2')
+    it = IterateMe_2(2, 20, 2)
+    for i in it:
+        if i > 10:
+            break
+        print(i)
+    print('next')
+    #when you break from the loop and try to pick it up again, it starts where it left off
+    for i in it:
+        print(i)
+    #IterateMe_2 matches the function of range()
+    #range is an iterable, not an iterator
