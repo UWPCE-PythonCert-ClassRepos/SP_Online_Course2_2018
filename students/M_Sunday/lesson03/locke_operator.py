93e31ce1ae7c198748a2f09d3b954d2716ce9eaf
mmsunday1 <matt.m.sunday@gmail.com>
2018-12-09 16:32:19 -0800

    locke exercise complete

--- students/M_Sunday/lesson03/locke_operator.py
+++ students/M_Sunday/lesson03/locke_operator.py
@@ -1,14 +1,33 @@
+class TooManyBoats(Exception):
+    def __init__(self, max_boats):
+        self.max_boats = max_boats
+
 
 class Locke:
 
-    def __init__(self):
-        pass
+    def __init__(self, boat_limit):
+        self.boat_limit = boat_limit
 
     def __enter__(self):
-        pass
+        return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
-        pass
+        # returning None allows for a clean exit
+        return None
+
+    def move_boats_through(self, boat_count):
+        if boat_count > self.boat_limit:
+            raise TooManyBoats("There are too many boats in the Locke, "
+                               "the maximum number of boats allowed is {"
+                               "}".format(self.boat_limit))
+        else:
+            print("-----Moving {} boats through the Locke-----\n"
+                  "             Stopping the pumps.           \n"
+                  "             Opening the doors.            \n"
+                  "             Closing the doors.            \n"
+                  "             Restarting the pumps.         \n"
+                  "-----------Boat Transfer Complete---------\n".
+                  format(boat_count))
 
 
 def operator():
