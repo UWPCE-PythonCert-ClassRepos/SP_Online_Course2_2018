fb0f6687f1bbc1a1f2687e0634d8e7350fab2ad5
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-12-11 09:33:46 -0800

    Adding all the files for the Advanced Testing assignment.

---
+++ students/toddschultz/session06/calculator/subtracter.py
@@ -0,0 +1,8 @@
+'''Execute subtraction commands'''
+
+class Subtracter():
+    '''subtracts two opperands'''
+    
+    @staticmethod
+    def calc(operand_1, operand_2):
+        return operand_1 - operand_2
