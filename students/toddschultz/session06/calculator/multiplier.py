7d54c15da9cc230851ddf6f691ca8563c90f0a70
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-12-11 09:33:21 -0800

    Adding all the files for the Advanced Testing assignment.

---
+++ students/toddschultz/session06/calculator/multiplier.py
@@ -0,0 +1,7 @@
+'''Execute multiplication commands'''
+class Multiplier():
+    '''Execute multiplication commands'''
+    
+    @staticmethod
+    def calc(operand_1, operand_2):
+        return operand_1*operand_2
