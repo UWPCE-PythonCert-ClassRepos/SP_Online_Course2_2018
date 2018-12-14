f8e237b7e5528d8c19db789b9f6885f9cec7333f
Todd Schultz <toddschultz@Todds-MacBook-Pro.local>
2018-12-14 12:40:36 -0800

    Fixed some instructor feedback with regard to pylint and flake8.

--- students/toddschultz/session06/calculator/divider.py
+++ students/toddschultz/session06/calculator/divider.py
@@ -1,8 +1,10 @@
 '''Execute division commands'''
 
+
 class Divider():
     '''Divides two operands'''
-    
+
     @staticmethod
+    '''Static Method does the work'''
     def calc(operand_1, operand_2):
         return operand_1/operand_2
