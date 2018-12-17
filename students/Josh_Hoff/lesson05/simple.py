23a0e2aa25485fbc74b5f834a6dbea90f7413c62
Josh Hoff <jhoff1985@hotmail.com>
2018-12-16 17:26:55 -0800

    adding assignments from lesson 5. Finished the debugging assignment.

---
+++ students/Josh_Hoff/lesson05/simple.py
@@ -0,0 +1,37 @@
+#simple.py
+
+import logging
+
+log_format = "%(asctime)s %(filename)s:%(lineno)-4d %(levelname)s %(message)s"
+
+#BEGIN NEW STUFF
+formatter = logging.Formatter(log_format)
+
+file_handler = logging.FileHandler('mylog.log')
+file_handler.setLevel(logging.WARNING)
+file_handler.setFormatter(formatter)
+
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
+logger.addHandler(console_handler)
+#END NEW STUFF
+
+#logging.basicConfig(level=logging.WARNING, format=log_format, filename='mylog.log')
+
+def my_fun(n):
+    for i in range(0, n):
+        logging.debug(i)
+        if i == 50:
+            logging.warning("The value of i is 50.")
+        try:
+            100 / (50 - i)
+        except ZeroDivisionError:
+            logging.error(f'Tried to divide by zero. Variable i was {i}. Recovered gracefully.')
+        
+if __name__ == "__main__":
+    my_fun(100)
