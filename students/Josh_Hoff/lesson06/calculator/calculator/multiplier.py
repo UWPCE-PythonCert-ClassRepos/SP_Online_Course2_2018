9340db387d17dd72833288d984a2a51c265978f9
Josh Hoff <jhoff1985@hotmail.com>
2018-12-17 16:37:44 -0800

    Finished calculator assignment lesson 6

---
+++ students/Josh_Hoff/lesson06/calculator/calculator/multiplier.py
@@ -0,0 +1,16 @@
+"""
+This module provides a multiplication operator
+"""
+
+
+class Multiplier:
+    """
+    This class holds the multiplier method
+    """
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """
+        This method multiplies 2 numbers together
+        """
+        return operand_1 * operand_2
