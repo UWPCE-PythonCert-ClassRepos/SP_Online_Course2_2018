2eced7ea62b9c5fa6279c1e51be029cf18d65251
Adam Strawmyer <adam@strawmyer.com>
2018-12-17 20:02:43 -0600

    L03 Locke added working exceprion

--- students/astrawmyer/lesson03/locke.py
+++ students/astrawmyer/lesson03/locke.py
@@ -42,17 +42,29 @@ class Locke:
         return self
     
     def __exit__(self, exc_type, exc_val, exc_tb):
+        print(exc_val)
         print("Stopping the pumps")
         print("Opening the doors")
         print("Closing the doors")
         print("Restarting the pumps")
         return self
 
-    def move_boats_through(boats):
-        print("moving Boat")
-        return 8
+    def move_boats_through(self, boats):
+        if boats > self.capacity:
+            raise Exception("Too many Boats in the locke.")
+        else:
+            print("Moving {} Boats through".format(boats))
+        
     
 
 small_locke = Locke(5)
+large_locke = Locke(10)
+boats = 8
+
+# Too many boats through a small locke will raise an exception
 with small_locke as locke:
-    locke.move_boats_through(5)
+    locke.move_boats_through(boats)
+
+# A lock with sufficient capacity can move boats without incident.
+with large_locke as locke:
+    locke.move_boats_through(boats)
