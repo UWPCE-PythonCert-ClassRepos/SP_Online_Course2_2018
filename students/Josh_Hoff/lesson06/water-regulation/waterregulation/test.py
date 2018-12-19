eaa58b11d555a0ca4576c9b8f6d2ce15e7e9d0eb
Josh Hoff <jhoff1985@hotmail.com>
2018-12-18 21:45:36 -0800

    Finished water_regulation assignment, still need to assure flake8 and pylint have zero errors

--- students/Josh_Hoff/lesson06/water-regulation/waterregulation/test.py
+++ students/Josh_Hoff/lesson06/water-regulation/waterregulation/test.py
@@ -11,30 +11,33 @@ from sensor import Sensor
 from .controller import Controller
 from .decider import Decider
 
+actions = {'PUMP_IN': 1, 'PUMP_OFF': 0, 'PUMP_OUT': -1}
+
 
 class DeciderTests(unittest.TestCase):
     """
     Unit tests for the Decider class
     """
     def test_decider(self):
-    
-        actions = {'PUMP_IN': -1, 'PUMP_OUT': 0, 'PUMP_OFF': 1}
-        
-        decider = Decider(80, .05)
-        
-        decider.decide(70, 0, actions)
-    # TODO: write a test or tests for each of the behaviors defined for
-    #       Decider.decide
 
-#    def test_dummy(self):
-#        """
-#        Just some example syntax that you might use
-#        """
-#
-#        pump = Pump('127.0.0.1', 8000)
-#        pump.set_state = MagicMock(return_value=True)
-#
-#        self.fail("Remove this test.")
+        decider = Decider(80, .05)
+
+        for x in range(1, 76):
+            self.assertEqual(decider.decide(x, 0, actions), 1)
+        for x in range(76, 85):
+            self.assertEqual(decider.decide(x, 0, actions), 0)
+        for x in range(85, 120):
+            self.assertEqual(decider.decide(x, 0, actions), -1)
+
+        for x in range(81, 120):
+            self.assertEqual(decider.decide(x, 1, actions), 0)
+        for x in range(1, 76):
+            self.assertEqual(decider.decide(x, 1, actions), 1)
+
+        for x in range(1, 76):
+            self.assertEqual(decider.decide(x, -1, actions), 0)
+        for x in range(85, 120):
+            self.assertEqual(decider.decide(x, -1, actions), -1)
 
 
 class ControllerTests(unittest.TestCase):
@@ -42,9 +45,18 @@ class ControllerTests(unittest.TestCase):
     Unit tests for the Controller class
     """
     def test_controller(self):
-    
-        pump = Pump('0.0.0.1', 541)
-        # TODO: write a test or tests for each of the behaviors defined for
-        #       Controller.tick
 
-#        pass
+        pump = Pump('0.0.0.1', 541)
+        sensor = Sensor('0.0.0.1', 541)
+        decider = Decider(80, .05)
+        controller = Controller(sensor, pump, decider)
+
+        sensor.measure = MagicMock(return_value=60)
+        pump.get_state = MagicMock(return_value=1)
+        decider.decide = MagicMock(return_value=-1)
+        pump.set_state = MagicMock(return_value=True)
+
+        controller.tick()
+        sensor.measure.assert_called_with()
+        pump.get_state.assert_called_with()
+        decider.decide.assert_called_with(60, 1, actions)
