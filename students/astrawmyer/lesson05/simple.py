f2faec449a189f396fd185ecc81033ab99183434
Adam Strawmyer <adam@strawmyer.com>
2019-01-01 16:33:33 -0600

    L05 going throught example

--- students/astrawmyer/lesson05/simple.py
+++ students/astrawmyer/lesson05/simple.py
@@ -1,13 +1,18 @@
 # simple.py
 import logging
 
-logging.basicConfig(level=logging.WARNING)
+log_format = "%(asctime)s %(filename)s:%(lineno)-4d %(levelname)s %(message)s"  # Add/modify these
+logging.basicConfig(level=logging.WARNING, format=log_format)                   # two lines
+
 def my_fun(n):
     for i in range(0, n):
         logging.debug(i)
         if i == 50:                                   # Add this line
             logging.warning("The value of i is 50.")  # Add this line
-        100 / (50 - i)
+            try:
+                100 / (50 - i)
+            except ZeroDivisionError:
+                logging.error("Tried to divide by zero. Var i was {}. Recovered gracefully.".format(i))
 
 if __name__ == "__main__":
     my_fun(100)
