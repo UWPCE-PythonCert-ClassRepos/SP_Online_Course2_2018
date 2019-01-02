d4b657d1db4c823edb224387d727dd96cfb36fd0
Adam Strawmyer <adam@strawmyer.com>
2019-01-01 19:35:36 -0600

    Lo5 deleting extra files for assignment

--- students/astrawmyer/lesson05/loggingtest.py
+++
@@ -1,8 +0,0 @@
-# loggingtest.py
-import logging
-
-logging.critical("This is a critical error!")
-logging.error("I'm an error.")
-logging.warning("Hello! I'm a warning!")
-logging.info("This is some information.")
-logging.debug("Perhaps this information will help you find your problem?")
