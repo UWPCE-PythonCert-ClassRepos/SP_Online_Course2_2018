9be0cf870f2178c5613aa3fa3482776e4820ae72
Adam Strawmyer <adam@strawmyer.com>
2019-01-01 20:46:18 -0600

    L05 completed lesson

--- students/astrawmyer/lesson05/simple.py
+++ students/astrawmyer/lesson05/simple.py
@@ -1,28 +1,38 @@
 # simple.py
 import logging
+import datetime
+from logging.handlers import DatagramHandler
+from logging.handlers import SysLogHandler
 
-log_format = "%(asctime)s %(filename)s:%(lineno)-4d %(levelname)s %(message)s"  # Add/modify these
-# BEGIN NEW STUFF
+log_format = "%(asctime)s %(filename)s:%(lineno)-4d %(levelname)s %(message)s"
+log_format_serv = "%(filename)s:%(lineno)-4d %(levelname)s %(message)s"
+
 formatter = logging.Formatter(log_format)
+formatter_serv = logging.Formatter(log_format_serv)
 
-file_handler = logging.FileHandler('mylog.log')
+file_handler = logging.FileHandler('{}.log'.format(datetime.date.today()))
 file_handler.setLevel(logging.WARNING)
 file_handler.setFormatter(formatter)
 
-console_handler = logging.StreamHandler()        # Add this line
-console_handler.setLevel(logging.DEBUG)          # Add this line
-console_handler.setFormatter(formatter)          # Add this line
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.DEBUG)
+console_handler.setFormatter(formatter)
+
+syslog_handler = DatagramHandler('127.0.0.1', 514)
+syslog_handler.setLevel(logging.ERROR)
+syslog_handler.setFormatter(formatter_serv)
 
 logger = logging.getLogger()
-logger.setLevel(logging.DEBUG)                   # Add this line
+logger.setLevel(logging.DEBUG)
 logger.addHandler(file_handler)
-logger.addHandler(console_handler)               # Add this line
+logger.addHandler(console_handler)
+logger.addHandler(syslog_handler)
 
 def my_fun(n):
     for i in range(0, n):
         logging.debug(i)
-        if i == 50:                                   # Add this line
-            logging.warning("The value of i is 50.")  # Add this line
+        if i == 50:
+            logging.warning("The value of i is 50.")
             try:
                 100 / (50 - i)
             except ZeroDivisionError:
