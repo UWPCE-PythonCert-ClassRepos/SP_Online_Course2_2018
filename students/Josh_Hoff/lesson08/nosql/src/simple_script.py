e21da139fcf4f41a5fab7d9816604c1edbc4e8ce
Josh Hoff <jhoff1985@hotmail.com>
2019-01-05 17:12:07 -0800

    Finished Lesson 8 assignment #1

--- students/Josh_Hoff/lesson08/nosql/src/simple_script.py
+++ students/Josh_Hoff/lesson08/nosql/src/simple_script.py
@@ -12,11 +12,92 @@ import utilities
 
 log = utilities.configure_logger('default', '../logs/mongodb_script.log')
 
-
+def assignment():
+    """
+    Simple persistance and serialization
+    """
+    wine_data = [
+        {'label_name': 'Sutter Home',
+            'grape': 'Cabernet Sauvignon',
+            'region': 'California',
+            'retail_price': 7.99,
+            'alcohol_percentage': 13.5
+        },
+        {'label_name': 'Boomtown',
+            'grape': 'Syrah',
+            'region': 'Washington',
+            'retail_price': 15.99,
+            'alcohol_percentage': 14.7
+        },
+        {'label_name': 'Pundit',
+            'grape': 'Syrah',
+            'region': 'Washington',
+            'retail_price': 19.99,
+            'alcohol_percentage': 14.5
+        },
+        {'label_name': 'Red Schooner',
+            'grape': 'Malbec',
+            'region': 'California',
+            'retail_price': 49.99,
+            'alcohol_percentage': 15.2
+        },
+        {'label_name': 'Intrinsic',
+            'grape': 'Cabernet Sauvignon',
+            'region': 'Washington',
+            'retail_price': 24.99,
+            'alcohol_percentage': 14.0
+        },
+        {'label_name': 'Red Diamond',
+            'grape': 'Red Blend',
+            'region': 'California',
+            'retail_price': 9.99,
+            'alcohol_percentage': 13.5
+        },
+        {'label_name': 'Kim Crawford',
+            'grape': 'Sauvignon Blanc',
+            'region': 'New Zealand',
+            'retail_price': 17.99,
+            'alcohol_percentage': 13.2
+        },
+        {'label_name': 'Erath',
+            'grape': 'Pinot Noir',
+            'region': 'Oregon',
+            'retail_price': 15.99,
+            'alcohol_percentage': 14.0
+        },
+        {'label_name': 'Mer Soleil',
+            'grape': 'Chardonnay',
+            'region': 'California',
+            'retail_price': 29.99,
+            'alcohol_percentage': 13.9
+        },
+        {'label_name': 'Meiomi',
+            'grape': 'Chardonnay',
+            'region': 'California',
+            'retail_price': 27.99,
+            'alcohol_percentage': 14.1
+        }]
+        
+    pickle.dump(wine_data, open('../data/data.pkl', 'wb'))
+    
+    read_data = pickle.load(open('../data/data.pkl', 'rb'))
+    
+    assert read_data == wine_data
+    print('PRINTING PICKLE DATA')
+    pprint.pprint(read_data)
+    
+    shelf_file = shelve.open('../data/shelve.dat')
+    shelf_file['key'] = wine_data
+    read_items = shelf_file['key']
+    assert read_items == wine_data
+    print('PRINTING SHELF DATA')
+    pprint.pprint(read_items)
+    del shelf_file['key']
+    shelf_file.close()
+    return
 def run_example(furniture_items):
     """
     various persistence and serialization scenarios
-
     """
 
     def run_pickle():
@@ -111,3 +192,5 @@ def run_example(furniture_items):
     run_json()
 
     return
+if __name__ == '__main__':
+    assignment()
