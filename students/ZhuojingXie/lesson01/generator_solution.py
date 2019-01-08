5d94dfa9d7a48a33c6175755a29f4de8efa7c18d
ZhuojingXie <llxllxzj@gmail.com>
2019-01-07 22:40:34 -0800

    lesson 01 HW

---
+++ students/ZhuojingXie/lesson01/generator_solution.py
@@ -0,0 +1,26 @@
+def intsum(i=0):
+    sum = 0
+    while True:
+        yield sum
+        sum = sum + i
+        i = i + 1
+
+
+def doubler(i=1):
+    while True:
+        yield i
+        i *= 2
+
+
+def fib():
+    i, j = 0, 1
+    while True:
+        i, j = j, i + j
+        yield i
+
+
+def prime(i=2):
+    while True:
+        if i < 2 or not [x for x in range(2, i) if i % x == 0]:
+            yield i
+        i += 1
