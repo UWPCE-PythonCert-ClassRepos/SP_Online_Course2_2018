1cd50cf341ccef21c5855350d6e67d77f215b59e
Victor Medina <vmedina1014@gmail.com>
2019-01-08 18:46:38 -0800

    added grid printer part 1

---
+++ students/VictorM/Assignment_2/Grid_Printer/Grid_Printer.py
@@ -0,0 +1,18 @@
+plus='+'
+minus=' - '
+wall='|'
+row='+ - - - - + - - - - +\n'
+non_row='|         |         |\n'
+print(row)
+print(non_row)
+print(non_row)
+print(non_row)
+print(non_row)
+print(row)
+print(non_row)
+print(non_row)
+print(non_row)
+print(non_row)
+print(row)
+	 
+
