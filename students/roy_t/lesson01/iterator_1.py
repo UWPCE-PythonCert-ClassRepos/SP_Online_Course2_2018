d53786aba4b47c7c67f68bcfa22dab53b34283fd
tater <roytate@hotmail.com>
2019-01-10 19:57:34 -0800

    started class

--- students/roy_t/lesson01/iterator_1.py
+++ students/roy_t/lesson01/iterator_1.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python3
 
+class IterateMe_2:
+
+    def __init__(self, start, stop, step=1):
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def interator_2(self, start, stop, step=1):
 
 
+def main():
+    it = IterateMe_2(2, 20, 2)
+    for i in it:
+        if i > 10:  break
+        print(i)
+    for i in it:
+        print(i)
 
-it = IterateMe_2(2, 20, 2)
-for i in it:
-    if i > 10:  break
-    print(i)
-for i in it:
-    print(i)
+if __name__ == "__main__":
+    main()
