ac81a63f7aa812af6eab54ba38a5326ea03b6311
Toni Lugosan <tlugosan@hotmail.com>
2019-01-15 20:42:39 -0800

    correcting the answer from a list to a generator

--- students/tlugosan/Lesson02/generators2.py
+++ students/tlugosan/Lesson02/generators2.py
@@ -5,8 +5,16 @@ import pandas as pd
 music = pd.read_csv("featuresdf.csv")
 
 
-list3 = [x for x in zip(music.artists, music.name) if x[0] == "Ed Sheeran"]
+list3 = (x for x in zip(music.artists, music.name) if x[0] == "Ed Sheeran")
 
-print("List of all the songs by a favorite artist:")
+'''print("List of all the songs by a favorite artist:")
 for x in list3:
-    print ('{:30} {:50}'.format(x[0], x[1]))
+    print ('{:30} {:50}'.format(x[0], x[1]))'''
+
+
+if __name__ == "__main__":
+   print(next(list3))
+   print(next(list3))
+   print(next(list3))
+   print(next(list3))
+   '''print(next(list3)) - throwing exception'''
