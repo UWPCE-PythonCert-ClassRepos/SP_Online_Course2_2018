971b764226425b4ee7b04496616ef8d108706912
Paul Jurek <pjurek3@gmail.com>
2019-01-23 20:19:06 -0800

    completed iterator assignment. worked out resetting state on new call with __iter__

--- students/paulJurek/lesson01/iterator_1.py
+++ students/paulJurek/lesson01/iterator_1.py
@@ -27,9 +27,41 @@ class IterateMe_1:
         else:
             raise StopIteration
 
+class IterateMe_2:
+    """
+    About as simple an iterator as you can get:
+
+    returns the sequence of numbers from zero to 4
+    ( like range(4) )
+    """
+
+    def __init__(self, start: int=-1, stop: int=5, step: int=1):
+        self.current = start - step
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __iter__(self):
+        self.current = self.start - self.step
+        return self
+
+    def __next__(self):
+        self.current += self.step
+        if self.current < self.stop:
+            return self.current
+        else:
+            raise StopIteration
+
+
+
+
 
 if __name__ == "__main__":
 
     print("Testing the iterator")
     for i in IterateMe_1():
+        print(i)
+
+    print('range like IterateMe')
+    for i in IterateMe_2(2,20,2):
         print(i)
