aa06ac37884c483be1ebb49c7e8941d5774b281b
Paul Jurek <pjurek3@gmail.com>
2019-01-27 14:17:06 -0800

    completed high energy closure exercise

---
+++ students/paulJurek/lesson02/high_energy_tracks.py
@@ -0,0 +1,26 @@
+import csv
+
+def get_high_energy_tracks(energy_level_limit):
+
+    
+    def get_tracks_by_energy_level(filename: str=r'lesson02\top-tracks-of-2017.csv'):
+        """general function which searches and returns specific energy level records"""
+        artist_name = 2
+        track_name = 1
+        energy_level = 4
+        track_list = []
+        with open(filename, "r") as f:
+            reader = csv.reader(f)
+            for row in reader:
+                try:
+                    if float(row[energy_level]) >= energy_level_limit:
+                        track_list.append((row[track_name], row[artist_name], row[energy_level]))
+                except ValueError:
+                    # catches first line an skips
+                    continue
+        return track_list
+    return get_tracks_by_energy_level
+
+if __name__ == '__main__':
+    t = get_high_energy_tracks(energy_level_limit=0.8)
+    print(set(t()))
