2a8a88b24d85b411e60a2ed6b9a00b3af69baaf1
Paul Jurek <pjurek3@gmail.com>
2019-01-31 05:35:05 -0800

    finishing lesson 2

--- students/paulJurek/lesson02/favorite_artist_generator.py
+++ students/paulJurek/lesson02/favorite_artist_generator.py
@@ -23,6 +23,26 @@ def get_artist_tracks(artist: str, filename: str) -> str:
                 print(row[track_name])
                 yield row[track_name]
 
+def get_high_energy_tracks(artist: str, filename: str) -> str:
+    """generator which iterates through the specified file and 
+    returns tracks for artist.  Using a generator to iteratre through file 
+    to enable very large file handling
+    args:
+        artist: indifies the artist we are searching for
+        filename: filename to search in
+    returns:
+        returns tracks for artist in file"""
+    artist_name = 2
+    track_name = 1
+    print(filename)
+    with open(filename, "r") as f:
+        reader = csv.reader(f)
+        for row in reader:
+            if row[artist_name] == artist:
+                print(row[track_name])
+                yield row[track_name]
+
+
 if __name__ == '__main__':
     print(set(get_artist_tracks(artist='Ed Sheeran', 
                           filename=r'top-tracks-of-2017.csv')))
