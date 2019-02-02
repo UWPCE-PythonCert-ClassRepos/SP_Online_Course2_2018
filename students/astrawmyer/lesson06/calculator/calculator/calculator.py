fcfec05e7705bf458d1b23724b60d3e1e2247782
Adam Strawmyer <adam@strawmyer.com>
2019-02-01 20:34:50 -0600

    L06 calculator module passes pylint

--- students/astrawmyer/lesson06/calculator/calculator/calculator.py
+++ students/astrawmyer/lesson06/calculator/calculator/calculator.py
@@ -1,6 +1,11 @@
+"""
+Module for the calculator. This module calls the other modules for calculations.
+"""
+
 from .exceptions import InsufficientOperands
 
 class Calculator(object):
+    """Class Calculator."""
 
     def __init__(self, adder, subtracter, multiplier, divider):
         self.adder = adder
@@ -11,6 +16,7 @@ class Calculator(object):
         self.stack = []
 
     def enter_number(self, number):
+        """Function for entering new numbers to the calculator from user."""
         self.stack.insert(0, number)
 
     def _do_calc(self, operator):
@@ -23,13 +29,17 @@ class Calculator(object):
         return result
 
     def add(self):
+        """Returns the adder module to do addition."""
         return self._do_calc(self.adder)
 
     def subtract(self):
+        """Returns the subtractor module to do addition."""
         return self._do_calc(self.subtracter)
 
     def multiply(self):
+        """Returns the multiplier module to do addition."""
         return self._do_calc(self.multiplier)
 
     def divide(self):
+        """Returns the divider module to do addition."""
         return self._do_calc(self.divider)
