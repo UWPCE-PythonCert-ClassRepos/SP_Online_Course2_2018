27d6196bf11c623dc2e4f64237c776633dfbdbf0
Adam Strawmyer <adam@strawmyer.com>
2019-02-01 20:26:52 -0600

    L06 make divide and multiply pass pylint

--- students/astrawmyer/lesson06/calculator/calculator/divider.py
+++ students/astrawmyer/lesson06/calculator/calculator/divider.py
@@ -1,5 +1,9 @@
+"""Module for dividing two numbers"""
+
 class Divider(object):
+    """Class for division of 2 numbers."""
 
     @staticmethod
     def calc(operand_1, operand_2):
+        """ Perforn the division step."""
         return operand_1/operand_2
