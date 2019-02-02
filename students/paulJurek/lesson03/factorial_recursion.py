70acb68358918b041692e81373379c10e3420333
Paul Jurek <pjurek3@gmail.com>
2019-02-01 21:29:40 -0800

    created factoial module and seperate test suite.  I prefer seperate test suits to help run tests

---
+++ students/paulJurek/lesson03/factorial_recursion.py
@@ -0,0 +1,12 @@
+"""implemenation of factorial with recursion"""
+
+def factorial(n: int) -> int:
+    """recursive implementation of factorial
+    args:
+        n: positive integer for factoial input
+    returns:
+        integer of product of factorial"""
+    pass
+
+if __name__=="__main__":
+    pass
---
+++ students/paulJurek/lesson03/test_factorial.py
@@ -0,0 +1,2 @@
+"""test suite for recursive factoial"""
+
