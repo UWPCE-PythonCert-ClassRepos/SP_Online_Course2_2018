f7ee763863e359425793b7f8072c11b48c9b8d58
Roy Tate <roytate@hotmail.com>
2019-02-05 22:35:52 -0800

    initial commit lesson06

---
+++ students/roy_t/lesson06/unittest_calculator.py
@@ -0,0 +1,75 @@
+"""
+Collection of unit-tests for verifying and validating the following files:
+calculator/*
+"""
+
+from unittest import TestCase
+from unittest.mock import MagicMock
+
+from calculator.adder import Adder
+from calculator.subtracter import Subtracter
+from calculator.multiplier import Multiplier
+from calculator.divider import Divider
+from calculator.calculator import Calculator
+from calculator.exceptions import InsufficientOperands
+
+
+class AdderTests(TestCase):
+    """Tests for Adder"""
+
+    def test_adding(self):
+        """Create an Adder, verify the result"""
+        adder = Adder()
+
+        for i in range(-10, 10):
+            for j in range(-10, 10):
+                self.assertEqual(i + j, adder.calc(i, j))
+
+
+class SubtracterTests(TestCase):
+    """Class for testing Subtractor"""
+
+    def test_subtracting(self):
+        """Create a Subtractor, verify the result"""
+        subtracter = Subtracter()
+
+        for i in range(-10, 10):
+            for j in range(-10, 10):
+                self.assertEqual(i - j, subtracter.calc(i, j))
+
+
+class CalculatorTests(TestCase):
+    """Class for testing the Calculator"""
+
+    def setUp(self):
+        """
+        Configure a new Adder, Subtractor, Multiplier, and Divider to instantiate a calculator
+        """
+        self.adder = Adder()
+        self.subtracter = Subtracter()
+        self.multiplier = Multiplier()
+        self.divider = Divider()
+        self.calculator = Calculator(self.adder, self.subtracter, self.multiplier, self.divider)
+
+    def test_insufficient_operands(self):
+        """At least two operands are needed."""
+        self.calculator.enter_number(0)
+
+        with self.assertRaises(InsufficientOperands):
+            self.calculator.add()
+
+    def test_adder_call(self):
+        """Test numbers can be added."""
+        self.adder.calc = MagicMock(return_value=0)
+        self.calculator.enter_number(1)
+        self.calculator.enter_number(2)
+        self.calculator.add()
+        self.adder.calc.assert_called_with(1, 2)
+
+    def test_subtracter_call(self):
+        """Test numbers can be substracted"""
+        self.subtracter.calc = MagicMock(return_value=0)
+        self.calculator.enter_number(1)
+        self.calculator.enter_number(2)
+        self.calculator.subtract()
+        self.subtracter.calc.assert_called_with(1, 2)
