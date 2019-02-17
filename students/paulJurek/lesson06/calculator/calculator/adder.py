8c4ddec34d30262771da231d384dffd7afe222a6
Paul Jurek <pjurek3@gmail.com>
2019-02-16 22:41:18 -0800

    quick fix to convert tabs to spaces

--- students/paulJurek/lesson06/calculator/calculator/adder.py
+++ students/paulJurek/lesson06/calculator/calculator/adder.py
@@ -1,8 +1,8 @@
 """defines adder object"""
 
 class Adder:
-	"""adder object to be used in calculator"""
-
+    """adder object to be used in calculator"""
     @staticmethod
     def calc(operand_1, operand_2):
+        """runs addtion for calc call"""
         return operand_1 + operand_2
