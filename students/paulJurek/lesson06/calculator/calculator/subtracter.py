251f2fa59892cc65d3159724165271c349b0c99b
Paul Jurek <pjurek3@gmail.com>
2019-02-16 23:08:22 -0800

    added doc strings to subtraction * removed object reference

--- students/paulJurek/lesson06/calculator/calculator/subtracter.py
+++ students/paulJurek/lesson06/calculator/calculator/subtracter.py
@@ -3,8 +3,10 @@ This module provides a subtraction operator
 """
 
 
-class Subtracter(object):
+class Subtracter:
+    """basic subrtaction object"""
 
     @staticmethod
     def calc(operand_1, operand_2):
+        """subtraction method"""
         return operand_1 - operand_2
