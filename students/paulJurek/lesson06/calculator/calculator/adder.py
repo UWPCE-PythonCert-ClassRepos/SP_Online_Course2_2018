097fb79bee23e4952a2a4873cc0ebf00dbf74212
Paul Jurek <pjurek3@gmail.com>
2019-02-16 22:33:36 -0800

    setup calculator in lesson06 sub directory * made initial changes to adder for docstrings to fix pylint"

---
+++ students/paulJurek/lesson06/calculator/calculator/adder.py
@@ -0,0 +1,8 @@
+"""defines adder object"""
+
+class Adder:
+	"""adder object to be used in calculator"""
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        return operand_1 + operand_2
