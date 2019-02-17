a2b4b5ce049273e1e7fc3fd381d7509f296986d5
Paul Jurek <pjurek3@gmail.com>
2019-02-17 13:55:21 -0800

    tdd: created failing unit test for decider lint: cleaned up pylint errors

--- students/paulJurek/lesson06/water-regulation/waterregulation/test.py
+++ students/paulJurek/lesson06/water-regulation/waterregulation/test.py
@@ -30,6 +30,50 @@ class DeciderTests(unittest.TestCase):
 
         self.fail("Remove this test.")
 
+    def test_pump_in_for_off_pump_low_height(self):
+        """1. If the pump is off and the height is below the
+        margin region, then the pump should be turned to PUMP_IN."""
+        self.fail("implement this test")
+
+    def test_pump_out_for_pump_off_height_above(self):
+        """2. If the pump is off and the height is above the margin region,
+        then the pump should be turned to PUMP_OUT."""
+        self.fail("implement this test")
+
+    def test_pump_off_for_pump_off_height_good(self):
+        """3. If the pump is off and the height is within the margin
+        region or on the exact boundary of the margin region, then
+        the pump shall remain at PUMP_OFF."""
+        self.fail("implement this test")
+
+    def test_pump_off_for_height_above_pump_in(self):
+        """4. If the pump is performing PUMP_IN and the height is above
+             the target height, then the pump shall be turned to PUMP_OFF,
+             otherwise the pump shall remain at PUMP_IN.
+        """
+        self.fail("implement this test")
+
+    def test_pump_in_for_height_low_pump_in(self):
+        """4. If the pump is performing PUMP_IN and the height is above
+             the target height, then the pump shall be turned to PUMP_OFF,
+             otherwise the pump shall remain at PUMP_IN.
+        """
+        self.fail("implement this test")
+
+    def test_pump_off_for_pump_out_height_ok(self):
+        """5. If the pump is performing PUMP_OUT and the height is below
+             the target height, then the pump shall be turned to PUMP_OFF,
+             otherwise, the pump shall remain at PUMP_OUT.
+        """
+        self.fail("implement this test")
+
+    def test_pump_out_for_pump_out_height_hight(self):
+        """5. If the pump is performing PUMP_OUT and the height is below
+             the target height, then the pump shall be turned to PUMP_OFF,
+             otherwise, the pump shall remain at PUMP_OUT.
+        """
+        self.fail("implement this test")
+
 
 class ControllerTests(unittest.TestCase):
     """
@@ -95,21 +139,21 @@ class ControllerTests(unittest.TestCase):
             given the above
           4. set the pump to that new state
         """
-        CURRENT_HEIGHT = 5
-        CURRENT_PUMP_STATE = 0
-        NEXT_PUMP_STATE = 1
+        current_height = 5
+        current_pump_state = 0
+        next_pump_state = 1
 
-        self.sensor.measure = MagicMock(return_value=CURRENT_HEIGHT)
-        self.pump.get_state = MagicMock(return_value=CURRENT_PUMP_STATE)
-        self.decider.decide = MagicMock(return_value=NEXT_PUMP_STATE)
+        self.sensor.measure = MagicMock(return_value=current_height)
+        self.pump.get_state = MagicMock(return_value=current_pump_state)
+        self.decider.decide = MagicMock(return_value=next_pump_state)
         self.pump.set_state = MagicMock(return_value=True)
 
         self.controller.tick()
 
         self.sensor.measure.assert_called_with()
         self.pump.get_state.assert_called_with()
-        self.decider.decide.assert_called_with(current_height=CURRENT_HEIGHT,
-                                               pump_state=CURRENT_PUMP_STATE,
+        self.decider.decide.assert_called_with(current_height=current_height,
+                                               pump_state=current_pump_state,
                                                actions=self.controller.actions)
 
-        self.pump.set_state.assert_called_with(state=NEXT_PUMP_STATE)
+        self.pump.set_state.assert_called_with(state=next_pump_state)
