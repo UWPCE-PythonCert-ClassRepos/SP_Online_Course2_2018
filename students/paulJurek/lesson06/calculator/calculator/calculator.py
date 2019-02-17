f61841215120a9d9bbbc45827f6974817beab039
Paul Jurek <pjurek3@gmail.com>
2019-02-16 22:41:44 -0800

    fixed pylint issues * added doc strings to module * added doc string to class * added doc strings to methods

--- students/paulJurek/lesson06/calculator/calculator/calculator.py
+++ students/paulJurek/lesson06/calculator/calculator/calculator.py
@@ -1,6 +1,9 @@
+"""defines calculator object"""
+
 from .exceptions import InsufficientOperands
 
-class Calculator(object):
+class Calculator:
+    """defines calculator object"""
 
     def __init__(self, adder, subtracter, multiplier, divider):
         self.adder = adder
@@ -10,7 +13,13 @@ class Calculator(object):
 
         self.stack = []
 
-    def enter_number(self, number):
+    def enter_number(self, number: float):
+        """adds number to stack
+        args:
+            number: number input
+        return:
+            None
+        """
         self.stack.insert(0, number)
 
     def _do_calc(self, operator):
@@ -23,13 +32,17 @@ class Calculator(object):
         return result
 
     def add(self):
+        """sets add method with adder"""
         return self._do_calc(self.adder)
 
     def subtract(self):
+        """sets subtract method with subracter"""
         return self._do_calc(self.subtracter)
 
     def multiply(self):
+        """sets method with multiplier"""
         return self._do_calc(self.multiplier)
 
     def divide(self):
+        """sets divide method call with divider"""
         return self._do_calc(self.divider)
