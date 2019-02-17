3d182cfa9899c11ced7d645ecc9ef905ee9ceb63
Paul Jurek <pjurek3@gmail.com>
2019-02-16 22:44:51 -0800

    pylinted divider * module docstring added * class docstring added * method docstring added

--- students/paulJurek/lesson06/calculator/calculator/divider.py
+++ students/paulJurek/lesson06/calculator/calculator/divider.py
@@ -1,5 +1,16 @@
-class Divider(object):
+"""defines divider objects"""
+
+class Divider:
+    """defines basic divider object"""
 
     @staticmethod
-    def calc(operand_1, operand_2):
+    def calc(operand_1: float, operand_2: float):
+        """performs division of two numerators with first input being numerator and
+        second input denomenator
+        args:
+            operand_1: numerator float input
+            operand_2: denomenator float input
+        returns:
+            float from division of operand_1/operand_2
+        """
         return operand_1/operand_2
