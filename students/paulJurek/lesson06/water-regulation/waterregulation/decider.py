58b719bd38e840c503e1a46c9344a83c6d7fb5f9
Paul Jurek <pjurek3@gmail.com>
2019-02-17 15:01:47 -0800

    tdd: made item 1 test pass

--- students/paulJurek/lesson06/water-regulation/waterregulation/decider.py
+++ students/paulJurek/lesson06/water-regulation/waterregulation/decider.py
@@ -7,19 +7,26 @@ class Decider(object):
     """
     Encapsulates decision making in the water-regulation module
     """
-
     def __init__(self, target_height, margin):
         """
         Create a new decider instance for this tank.
 
         :param target_height: the target height for liquid in this tank
         :param margin: the margin of liquid above and below the target height
-                       for which the pump should not turn on. Ex: .05
-                       represents a 5% margin above and
-                       below the target_height.
+                    for which the pump should not turn on. Ex: .05
+                    represents a 5% margin above and
+                    below the target_height.
         """
         self.target_height = target_height
         self.margin = margin
+
+    def get_target_low(self):
+        """returns value of target minus margin"""
+        return self.target_height * (1-self.margin)
+
+    def get_target_high(self):
+        """returns value of target plus margin"""
+        return self.target_height * (1+self.margin)
 
     def decide(self, current_height, current_action, actions):
         """
@@ -31,29 +38,35 @@ class Decider(object):
 
         The *decide* method shall obey the following behaviors:
 
-          1. If the pump is off and the height is below the
-             margin region, then the pump should be turned to PUMP_IN.
-          2. If the pump is off and the height is above the margin region,
-             then the pump should be turned to PUMP_OUT.
-          3. If the pump is off and the height is within the margin
-             region or on the exact boundary of the margin region, then
-             the pump shall remain at PUMP_OFF.
-          4. If the pump is performing PUMP_IN and the height is above
-             the target height, then the pump shall be turned to PUMP_OFF,
-             otherwise the pump shall remain at PUMP_IN.
-          5. If the pump is performing PUMP_OUT and the height is below
-             the target height, then the pump shall be turned to PUMP_OFF,
-             otherwise, the pump shall remain at PUMP_OUT.
+            1. If the pump is off and the height is below the
+                margin region, then the pump should be turned to PUMP_IN.
+            2. If the pump is off and the height is above the margin region,
+                then the pump should be turned to PUMP_OUT.
+            3. If the pump is off and the height is within the margin
+                region or on the exact boundary of the margin region, then
+                the pump shall remain at PUMP_OFF.
+            4. If the pump is performing PUMP_IN and the height is above
+                the target height, then the pump shall be turned to PUMP_OFF,
+                otherwise the pump shall remain at PUMP_IN.
+            5. If the pump is performing PUMP_OUT and the height is below
+                the target height, then the pump shall be turned to PUMP_OFF,
+                otherwise, the pump shall remain at PUMP_OUT.
 
         :param current_height: the current height of liquid in the tank
         :param current_action: the current action of the pump
         :param actions: a dictionary containing the keys 'PUMP_IN', 'PUMP_OFF',
                         and 'PUMP_OUT'
         :return: The new action for the pump: one of actions['PUMP_IN'],
-                                                     actions['PUMP_OUT'],
-                                                     actions['PUMP_OFF']
+                                                        actions['PUMP_OUT'],
+                                                        actions['PUMP_OFF']
         """
-
+        if ((current_action == 'PUMP_OFF') & 
+           (current_height < self.get_target_low())):
+           output = 'PUMP_IN'
         # TODO: Implement the properties of this method described above.
 
-        return actions['PUMP_IN']
+        else:
+            output = current_action
+        
+        return actions[output]
+
