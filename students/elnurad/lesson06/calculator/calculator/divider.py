fcb153c3167eac6a61a1985fbccf475e2fc5adae
Elnura Donabedian <elnurab@gmail.com>
2019-02-19 11:03:56 +0100

    adding calculator assignment

---
+++ students/elnurad/lesson06/calculator/calculator/divider.py
@@ -0,0 +1,14 @@
+"""
+This module provides a divider operator
+"""
+
+# pylint: disable = too-few-public-methods
+
+
+class Divider():
+    """This class provides a divider operator"""
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """Divide operand_1 by operand_2 and return the result"""
+        return operand_1/operand_2
