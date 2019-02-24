71d4c3fab8b4c91f4d7d931daaa9c44a024ed874
Paul Jurek <pjurek3@gmail.com>
2019-02-23 21:52:01 -0800

    cleaned up with flake8 * did not address import * issues as this seems to make code more readable

--- students/paulJurek/lesson07/RDBMS-example/main.py
+++ students/paulJurek/lesson07/RDBMS-example/main.py
@@ -4,7 +4,10 @@
         (but running this program does not require it)
 """
 import logging
+import datetime
+
 from personjob_model import *
+
 
 def populate_people():
     """
@@ -17,8 +20,6 @@ def populate_people():
     database = SqliteDatabase('personjob.db')
 
     logger.info('Working with Person class')
-    logger.info('Note how I use constants and a list of tuples as a simple schema')
-    logger.info('Normally you probably will have prompted for this from a user')
 
     PERSON_NAME = 0
     LIVES_IN_TOWN = 1
@@ -41,16 +42,17 @@ def populate_people():
             logger.info(f'building {person}')
             with database.transaction():
                 new_person = Person.create(
-                        person_name = person[PERSON_NAME],
-                        lives_in_town = person[LIVES_IN_TOWN],
-                        nickname = person[NICKNAME])
+                        person_name=person[PERSON_NAME],
+                        lives_in_town=person[LIVES_IN_TOWN],
+                        nickname=person[NICKNAME])
                 new_person.save()
                 logger.info('Database add successful')
 
         logger.info('Print the Person records we saved...')
         for saved_person in Person:
-            logger.info(f'{saved_person.person_name} lives in {saved_person.lives_in_town} ' +\
-                f'and likes to be known as {saved_person.nickname}')
+            logger.info(f'{saved_person.person_name} lives in '
+                        f'{saved_person.lives_in_town} and likes to be known '
+                        f'as {saved_person.nickname}')
 
     except Exception as e:
         logger.info(f'Error creating = {person[PERSON_NAME]}')
@@ -61,9 +63,10 @@ def populate_people():
         logger.info('database closes')
         database.close()
 
+
 def populate_departments():
     """fills in the departments for database"""
-    
+
     logging.basicConfig(level=logging.INFO)
     logger = logging.getLogger(__name__)
 
@@ -93,13 +96,14 @@ def populate_departments():
                 new_department = Department.create(
                         department_number=department[NUMBER],
                         department_name=department[NAME],
-                        department_manager = department[MANAGER])
+                        department_manager=department[MANAGER])
                 new_department.save()
                 logger.info('Database add successful')
 
         logger.info('Print the Department records we saved...')
         for saved_department in Department:
-            logger.info(f'{saved_department.department_name} has manager {saved_department.department_manager}')
+            logger.info(f'{saved_department.department_name} has manager '
+                        f'{saved_department.department_manager}')
 
     except Exception as e:
         logger.info(f'Error creating = {department[NAME]}')
@@ -109,6 +113,7 @@ def populate_departments():
     finally:
         logger.info('database closes')
         database.close()
+
 
 def populate_jobs():
     """
@@ -121,7 +126,6 @@ def populate_jobs():
     database = SqliteDatabase('personjob.db')
 
     logger.info('Working with Job class')
-    logger.info('Creating Job records: just like Person. We use the foreign key')
 
     JOB_NAME = 0
     START_DATE = 1
@@ -131,11 +135,16 @@ def populate_jobs():
     DEPARTMENT = 5
 
     jobs = [
-        ('Analyst', datetime.datetime(2001,9,22), datetime.datetime(2003, 1,30),65500, 'Andrew', 'D001'),
-        ('Senior analyst', datetime.datetime(2001,9,22), datetime.datetime(2003, 1,30), 70000, 'Andrew', 'D001'),
-        ('Senior business analyst', datetime.datetime(2001,9,22), datetime.datetime(2003, 1,30), 80000, 'Andrew', 'D002'),
-        ('Admin supervisor', datetime.datetime(2001,9,22), datetime.datetime(2003, 1,30), 45900, 'Peter', 'D002'),
-        ('Admin manager', datetime.datetime(2001,9,22), datetime.datetime(2003, 1,30), 45900, 'Peter', 'D002')
+        ('Analyst', datetime.datetime(2001, 9, 22),
+         datetime.datetime(2003, 1, 30), 65500, 'Andrew', 'D001'),
+        ('Senior analyst', datetime.datetime(2001, 9, 22),
+         datetime.datetime(2003, 1, 30), 70000, 'Andrew', 'D001'),
+        ('Senior business analyst', datetime.datetime(2001, 9, 22),
+         datetime.datetime(2003, 1, 30), 80000, 'Andrew', 'D002'),
+        ('Admin supervisor', datetime.datetime(2001, 9, 22),
+         datetime.datetime(2003, 1, 30), 45900, 'Peter', 'D002'),
+        ('Admin manager', datetime.datetime(2001, 9, 22),
+         datetime.datetime(2003, 1, 30), 45900, 'Peter', 'D002')
         ]
 
     try:
@@ -144,17 +153,18 @@ def populate_jobs():
         for job in jobs:
             with database.transaction():
                 new_job = Job.create(
-                    job_name = job[JOB_NAME],
-                    start_date = job[START_DATE],
-                    end_date = job[END_DATE],
-                    salary = job[SALARY],
-                    person_employed = job[PERSON_EMPLOYED],
-                    department = job[DEPARTMENT],)
+                    job_name=job[JOB_NAME],
+                    start_date=job[START_DATE],
+                    end_date=job[END_DATE],
+                    salary=job[SALARY],
+                    person_employed=job[PERSON_EMPLOYED],
+                    department=job[DEPARTMENT],)
                 new_job.save()
 
-        logger.info('Reading and print all Job rows (note the value of person)...')
+        logger.info('Reading and print all Job rows')
         for job in Job:
-            logger.info(f'{job.job_name} : {job.start_date} to {job.end_date} for {job.person_employed}')
+            logger.info(f'{job.job_name} : {job.start_date} to {job.end_date} '
+                        f'for {job.person_employed}')
             logger.info(f'days on job: {job.days_in_job}')
 
     except Exception as e:
@@ -165,12 +175,17 @@ def populate_jobs():
         logger.info('database closes')
         database.close()
 
+
 def report_persons_departments():
     """creates report of all departments person worked"""
     query = (Person
-             .select(Person.person_name, Department.department_name, Job.job_name)
-             .join(Job, join_type=JOIN.LEFT_OUTER,  on=(Person.person_name == Job.person_employed))
-             .join(Department, join_type=JOIN.LEFT_OUTER, on=(Job.department == Department.department_number))
+             .select(Person.person_name,
+                     Department.department_name,
+                     Job.job_name)
+             .join(Job, join_type=JOIN.LEFT_OUTER,
+                   on=(Person.person_name == Job.person_employed))
+             .join(Department, join_type=JOIN.LEFT_OUTER,
+                   on=(Job.department == Department.department_number))
              .dicts()
              )
 
--- students/paulJurek/lesson07/RDBMS-example/personjob_model.py
+++ students/paulJurek/lesson07/RDBMS-example/personjob_model.py
@@ -5,9 +5,10 @@
 """
 
 from peewee import *
-import datetime
+
 
 database = SqliteDatabase('personjob.db')
+
 
 class BaseModel(Model):
     class Meta:
@@ -19,9 +20,9 @@ class Person(BaseModel):
         This class defines Person, which maintains details of someone
         for whom we want to research career to date.
     """
-    person_name = CharField(primary_key = True, max_length = 30)
-    lives_in_town = CharField(max_length = 40)
-    nickname = CharField(max_length = 20, null = True)
+    person_name = CharField(primary_key=True, max_length=30)
+    lives_in_town = CharField(max_length=40)
+    nickname = CharField(max_length=20, null=True)
 
 
 class Department(BaseModel):
@@ -29,9 +30,9 @@ class Department(BaseModel):
         This class defines Depatment, which maintains department information
         related to the job
     """
-    department_number = CharField(primary_key = True, max_length = 4)
-    department_name = CharField(max_length = 30)
-    department_manager = CharField(max_length = 30, null=True)
+    department_number = CharField(primary_key=True, max_length=4)
+    department_name = CharField(max_length=30)
+    department_manager = CharField(max_length=30, null=True)
 
 
 class Job(BaseModel):
@@ -39,16 +40,19 @@ class Job(BaseModel):
         This class defines Job, which maintains details of past Jobs
         held by a Person.
     """
-    job_name = CharField(primary_key = True, max_length = 30)
+    job_name = CharField(primary_key=True, max_length=30)
     start_date = DateField(null=False)
     end_date = DateField(null=False)
-    salary = DecimalField(max_digits = 7, decimal_places = 2)
-    person_employed = ForeignKeyField(Person, related_name='was_filled_by', null=False)
-    department = ForeignKeyField(Department, related_name='was_filled_by', null=False)
+    salary = DecimalField(max_digits=7, decimal_places=2)
+    person_employed = ForeignKeyField(Person, related_name='was_filled_by',
+                                      null=False)
+    department = ForeignKeyField(Department, related_name='was_filled_by',
+                                 null=False)
 
     @property
     def days_in_job(self):
         """returns the time in the job"""
         return (self.end_date - self.start_date).days
+
 
 database.create_tables([Person, Department, Job])
