a52f2cac91030c4668d52aeb27428aa56d018f34
jayjones <jayjonesseattle@gmail.com>
2019-03-02 07:14:09 -0800

    updating recursion

--- students/TerranceJ/lesson3/recursion.py
+++ students/TerranceJ/lesson3/recursion.py
@@ -7,11 +7,18 @@ March 2, 2019
 def factorial(n):
     if n == 1:
         return 1
-    return n * factorial(n-1)
+
+    elif n < 1:
+        raise ValueError("Number must be greater than 1")
+    
+    else:
+        return n * factorial(n-1)
 
 
 if __name__ == '__main__':  
     print(factorial(5))    
     print(factorial(10))
+    
+
     
 
