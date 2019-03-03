7d39aceea5df564157145db239c42b97d37ba31f
Roy Crowley <roybillups@comcast.net>
2019-03-02 20:00:41 -0800

    Lesson 6 Advanced Testing

---
+++ students/RoyC/lesson06/calculator/calculator/adder.py
@@ -0,0 +1,16 @@
+"""
+This module provides a addition operator
+"""
+
+
+class Adder():
+    """
+    Addition operator for calculator
+    """
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """
+        Perform addition
+        """
+        return operand_1 + operand_2
---
+++ students/RoyC/lesson06/calculator/calculator/subtracter.py
@@ -0,0 +1,16 @@
+"""
+This module provides a subtraction operator
+"""
+
+
+class Subtracter():
+    """
+    Subtraction operator for calculator
+    """
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """
+        Perform subtraction
+        """
+        return operand_1 - operand_2
