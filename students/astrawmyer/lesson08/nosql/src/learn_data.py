f49b754626ebe1cb11a9ca5951ef0bb6d1b8562f
Adam Strawmyer <adam@strawmyer.com>
2019-03-03 19:43:39 -0600

    L08 separated product into type and color

--- students/astrawmyer/lesson08/nosql/src/learn_data.py
+++ students/astrawmyer/lesson08/nosql/src/learn_data.py
@@ -10,37 +10,43 @@ def get_furniture_data():
 
     furniture_data = [
         {
-            'product': 'Red couch',
+            'product_type': 'Couch',
+            'color': 'red',
             'description': 'Leather low back',
             'monthly_rental_cost': 12.99,
             'in_stock_quantity': 10
         },
         {
-            'product': 'Blue couch',
+            'product_type': 'Couch',
+            'color': 'blue',
             'description': 'Cloth high back',
             'monthly_rental_cost': 9.99,
             'in_stock_quantity': 3
         },
         {
-            'product': 'Coffee table',
+            'product_type': 'Coffee table',
+            'color': 'black',
             'description': 'Plastic',
             'monthly_rental_cost': 2.50,
             'in_stock_quantity': 25
         },
         {
-            'product': 'Red couch',
+            'product_type': 'Couch',
+            'color': 'red',
             'description': 'Leather high back',
             'monthly_rental_cost': 15.99,
             'in_stock_quantity': 17
         },
         {
-            'product': 'Blue recliner',
+            'product_type': 'Recliner',
+            'color': 'blue',
             'description': 'Leather high back',
             'monthly_rental_cost': 19.99,
             'in_stock_quantity': 6
         },
         {
-            'product': 'Chair',
+            'product_type': 'Chair',
+            'color': 'white'
             'description': 'Plastic',
             'monthly_rental_cost': 1.00,
             'in_stock_quantity': 45
