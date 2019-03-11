05de7c38bd72c5bc9c9bf60f280b41982aea9887
Adam Strawmyer <adam@strawmyer.com>
2019-03-10 19:47:08 -0500

    L09 fixed baseurl

--- students/astrawmyer/lesson09/multi_news.py
+++ students/astrawmyer/lesson09/multi_news.py
@@ -7,15 +7,14 @@ WORD = "boeing"
 
 NEWS_API_KEY = "0c90527956054643acefdedb6587d07f"
 
-base_url = 'https://newsapi.org/v1'
+base_url = 'https://newsapi.org/v2/'
 
 
 def get_sources():
     url = base_url + "sources"
-    params = {"language": "en"}
-    resp = requests.get(url, params=params)
-    data = resp.json()
-    sources = [src['id'].strip() for src in data['sources']]
+    params = {"language": "en", "apiKey": NEWS_API_KEY}
+    response = requests.get(url, params=params).json()
+    sources = [src['id'].strip() for src in response['sources']]
     print("all the sources")
     print(sources)
     return sources
@@ -45,3 +44,16 @@ def count_word(word, titles):
         if word in titles:
             count += 1
     return count
+
+start = time.time()
+sources = get_sources()
+
+""" art_count = 0
+word_count = 0
+for source in sources:
+    titles = get_articles(source)
+    art_count += len(titles)
+    word_count += count_word(WORD, titles)
+
+print(WORD, 'found {} times in {} articles'.format(word_count, art_count))
+print('Process took {:.0f} seconds'.format(time.time()-start)) """
