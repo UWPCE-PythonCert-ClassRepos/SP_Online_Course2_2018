6abe88bdfe8075894fd2fb3b1a649d9b3ec4ea72
Adam Strawmyer <adam@strawmyer.com>
2019-03-15 23:12:52 -0500

    L10 deleted commented out code

--- students/astrawmyer/lesson10/great_circle.py
+++ students/astrawmyer/lesson10/great_circle.py
@@ -27,59 +27,6 @@ def great_circle_raw(lon1, lat1, lon2, lat2, sample_list):
     print(sample_list)
     return radius * c + e
 
-
-# Funciton with acos factored out to a function
-
-""" def calculate_acos(a, b, theta):
-    return math.acos((math.cos(a) * math.cos(b)) + (math.sin(a) * math.sin(b) * math.cos(theta)))
-
-
-def great_circle_acos(lon1, lat1, lon2, lat2):
-    radius = 3956 # miles
-    x = math.pi / 180.0
-    a = (90.0 - lat1) * (x)
-    b = (90.0 - lat2) * (x)
-    theta = (lon2 - lon1) * (x)
-    # c = math.acos((math.cos(a) * math.cos(b)) + (math.sin(a) * math.sin(b) * math.cos(theta)))
-    c = calculate_acos(a, b, theta)
-    return radius * c
-
-
-# Great Circle algorithm with most steps factored out as a function
-
-def calculate_x():
-    return math.pi / 180.0
-
-
-def calculate_coordinate(lat, x):
-    return (90.0 - lat) * x
-
-
-def calculate_theta(lon2, lon1, x):
-    return (lon2 - lon1) * x
-
-
-def great_circle_factored(lon1, lat1, lon2, lat2):
-    radius = 3956 # miles
-    x = calculate_x()
-    a = calculate_coordinate(lat1, x)
-    b = calculate_coordinate(lat2, x)
-    theta = calculate_theta(lon2, lon1, x)
-    c = calculate_acos(a, b, theta)
-    return radius * c """
-
-
 if __name__ == "__main__":
 
     great_circle_raw(lon1, lat1, lon2, lat2, sample_list)
-    # great_circle_acos(lon1, lat1, lon2, lat2)
-    # great_circle_factored(lon1, lat1, lon2, lat2)
-
-    # for use as follows:
-    #   $ /usr/bin/time -p python great_circle.py
-    #   $ /usr/bin/time -p pypy great_circle.py
-
-    #for i in range(100):
-    #    great_circle_raw(lon1, lat1, lon2, lat2)
-        # great_circle_acos(lon1, lat1, lon2, lat2)
-        # great_circle_factored(lon1, lat1, lon2, lat2)
