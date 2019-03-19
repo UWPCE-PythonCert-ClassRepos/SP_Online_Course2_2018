ea7e77d42361f5201f2da9b9e35758f61d814532
Chris Kenyon <cbkenyon89@gmail.com>
2019-03-18 22:11:00 -0700

    Lesson 6: Calculator

---
+++ students/Chris_Kenyon/Lesson_6/calculator/calculator/adder.py
@@ -0,0 +1,12 @@
+"""
+Module to create an a add operator for calculator
+"""
+
+
+class Adder(object):
+    """class to implement addition operation"""
+
+    @staticmethod
+    def calc(operand_1, operand_2):
+        """Method to return the result of an addition."""
+        return operand_1 + operand_2
