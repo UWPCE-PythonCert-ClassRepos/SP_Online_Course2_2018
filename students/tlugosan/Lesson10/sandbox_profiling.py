9ca6672253ec877ce2bfacc711c11be8a7f780be
Toni Lugosan <tlugosan@hotmail.com>
2019-03-24 23:55:34 -0700

    memoization memory profiling and python vs pypy3 performance comparison

---
+++ students/tlugosan/Lesson10/sandbox_profiling.py
@@ -0,0 +1,19 @@
+import time
+from memory_profiler import profile
+
+fp = open('memory_profiler.log','w+')
+
+@profile(stream=fp)
+def fib_profiling(n):
+    if n <= 1:
+        return n
+    else:
+        return fib_profiling(n-1) + fib_profiling(n-2)
+
+
+if __name__ == '__main__':
+
+    fib_profiling(10)
+
+
+
