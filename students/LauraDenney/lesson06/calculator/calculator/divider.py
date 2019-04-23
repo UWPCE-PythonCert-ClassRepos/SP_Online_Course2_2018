5db049a7e52832d3fb94290296b53bbbdeba7331
Laura Denney <Laura.A.Denney@gmail.com>
2019-04-22 22:23:20 -0700

    finished working on calculator files

---
+++ students/LauraDenney/lesson06/calculator/calculator/divider.py
@@ -0,0 +1,11 @@
+'''
+This module provides a divider operator
+'''
+
+
+class Divider():
+    '''Creates object for dividing 2 numbers'''
+    @staticmethod
+    def calc(operand_1, operand_2):
+        '''Returns division of operands'''
+        return operand_1 / operand_2
---
+++ students/LauraDenney/lesson06/calculator/calculator/exceptions.py
@@ -0,0 +1,7 @@
+'''
+This module provides an InsufficientOperands Exception type
+'''
+
+
+class InsufficientOperands(Exception):
+    '''Creates unique Exception for insufficient operands'''
