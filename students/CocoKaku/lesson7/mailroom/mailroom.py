adc018f849735c892772bede4d3a6bfa9fd124b7
Colleen Kaku <colleenkaku@gmail.com>
2019-04-22 18:58:48 -0700

    add files for mailroom assignment for lesson 7

---
+++ students/CocoKaku/lesson7/mailroom/mailroom.py
@@ -0,0 +1,103 @@
+#!/usr/bin/python3
+"""
+updated mailroom program for Python 220 Lesson 7 assignment (relational databases)
+(1) changed data model from JSON to peewee/sqlite database
+(2) added logging
+"""
+from donors_model import *
+
+def send_thank_you():
+    """Add a donor/donation and print out a thank you letter"""
+    # loop for user input: donor name, or list, or quit
+    while True:
+        name = input("\nDonor Full Name (type 'list' for donor list or 'q' to quit): ")
+        if name in ('q', 'quit'):
+            return
+        if name == 'list':
+            print(Donation.list_donors())
+            continue
+        while True:
+            amount = input("Donation amount (type 'q' to quit): ")
+            if amount in ('q', 'quit'):
+                return
+            try:
+                Donation.add_donation(name, amount)
+            except ValueError:
+                print('Invalid input, try again')
+            else:
+                break
+        print('\n' + Donation.thank_you_letter(name))
+
+
+def create_a_report():
+    """Print a summary of donors and amounts donated to screen"""
+    print("\n"+Donation.summary_report())
+
+
+def send_all_letters():
+    """Write thank you letters to all donors to text files, filename = <donor_name>.txt"""
+    dir_name = input("Output directory ('.' for current dir): ")
+    Donation.send_all_letters(dir_name)
+
+
+def run_projection():
+    """Run projection showing total contribution of challenge scenario"""
+    while True:
+        factor = input("\nChallenge factor ('q' to quit): ")
+        if factor in ('q', 'quit'):
+            return
+        min_filter = input("Minimum donation to challenge (<return> for none, 'q' to quit): ")
+        if min_filter in ('q', 'quit'):
+            return
+        max_filter = input("Maximum donation to challenge (<return> for none, 'q' to quit): ")
+        if max_filter in ('q', 'quit'):
+            return
+        try:
+            scenario = Donation.challenge(float(factor),
+                                          float(min_filter) if min_filter else None,
+                                          float(max_filter) if max_filter else None)
+            print("\n"+scenario)
+        except ValueError:
+            print('Invalid inputs, try again')
+        else:
+            break
+
+
+def main_menu_error():
+    """print error message if invalid menu item is entered"""
+    print("Invalid choice, try again")
+
+
+def quit_program():
+    """close database, then exit program"""
+    database.close()
+    exit()
+
+
+def main():
+    """Main menu for mailroom program"""
+    database.init('donations.db')
+    database.connect()
+    database.execute_sql('PRAGMA foreign_keys = ON;')
+
+    switch_menu_dict = {
+        "1": send_thank_you,
+        "2": create_a_report,
+        "3": send_all_letters,
+        "4": run_projection,
+        "q": quit_program,
+        "quit": quit_program
+        }
+    while True:
+        print("\nMAIN MENU")
+        print("   1 = Send a Thank You")
+        print("   2 = Create a Report")
+        print("   3 = Send Letters to Everyone")
+        print("   4 = Run A Projection")
+        print("   q = Quit")
+        choice = input("   ? ")
+        switch_menu_dict.get(choice, main_menu_error)()
+
+
+if __name__ == '__main__':
+    main()
---
+++ students/CocoKaku/lesson7/mailroom/test_donors.py
@@ -0,0 +1,147 @@
+"""
+updated Donors class for Python 220 Lesson 4 assignment (metaprogramming)
+(1) ensuring that tests still run with updated Donors class
+"""
+from donors_model import *
+import os
+
+
+def setup_function():
+    if os.path.isfile('test.db'): os.remove('test.db')
+    database.init('test.db')
+    database.connect()
+    database.execute_sql('PRAGMA foreign_keys = ON;')
+    database.create_tables([Donor, Donation])
+    logging.basicConfig(level=logging.ERROR)
+
+
+def teardown_function():
+    database.close()
+    os.remove('test.db')
+
+
+def test_donation_list_donors_empty_db():
+    expected = ""
+    assert Donation.list_donors() == expected
+
+
+def test_donation_summary_report_empty_db():
+    expected = "DONOR NAME             TOTAL DONATED   NUM DONATIONS   AVG DONATION\n"
+    assert Donation.summary_report() == expected
+
+
+def test_donation_add_donation():
+    Donation.add_donation("Ann", 1)
+    assert Donor.get(name="Ann").name == "Ann"
+    assert Donation.get(name="Ann").name.name == "Ann"
+    assert Donation.get(name="Ann").amount == 1
+
+
+def test_donation_list_donors_one_donor_one_donation():
+    Donation.add_donation("Ann", 1)
+    assert Donation.list_donors() == "   Ann"
+
+
+def test_donors_summary_report_one_name_one_value():
+    Donation.add_donation("Ann", 1)
+    expected = "DONOR NAME             TOTAL DONATED   NUM DONATIONS   AVG DONATION\n" \
+               "Ann                    $        1.00   1               $       1.00\n"
+    assert Donation.summary_report() == expected
+
+
+def test_donation_list_donors_one_donor_plusone_donation():
+    Donation.add_donation("Ann", 1)
+    Donation.add_donation("Ann", 1.1)
+    assert Donation.list_donors() == "   Ann"
+
+
+def test_donation_summary_report_one_name_multiple_values():
+    Donation.add_donation("Ann", 1)
+    Donation.add_donation("Ann", 1.1)
+    expected = "DONOR NAME             TOTAL DONATED   NUM DONATIONS   AVG DONATION\n" \
+               "Ann                    $        2.10   2               $       1.05\n"
+    assert Donation.summary_report() == expected
+
+
+def test_donation_thank_you_letter():
+    Donation.add_donation("Bill", 300)
+    expected = f"Dear Bill,\n" \
+               f"Thank you very much for your generous donation of $300.00.\n" \
+               f"Sincerely,\n" \
+               f"PYTHON220 Class of 2019"
+    assert Donation.thank_you_letter("Bill") == expected
+
+
+def test_donation_thank_you_letter_multiple_donations():
+    Donation.add_donation("Joe", 100)
+    Donation.add_donation("Joe", 200)
+    expected = f"Dear Joe,\n" \
+               f"Thank you very much for your generous donation of $200.00.\n" \
+               f"Sincerely,\n" \
+               f"PYTHON220 Class of 2019"
+    assert Donation.thank_you_letter("Joe") == expected
+
+
+
+def test_donation_summary_report_multiple_names_multiple_values():
+    Donation.add_donation("Ann", 1)
+    Donation.add_donation("Ann", 1.1)
+    Donation.add_donation("Bill", 300)
+    Donation.add_donation("Joe", 100)
+    Donation.add_donation("Joe", 200)
+    expected = "DONOR NAME             TOTAL DONATED   NUM DONATIONS   AVG DONATION\n" \
+               "Bill                   $      300.00   1               $     300.00\n" \
+               "Joe                    $      300.00   2               $     150.00\n" \
+               "Ann                    $        2.10   2               $       1.05\n"
+    assert Donation.summary_report() == expected
+
+
+# tests for donors challenge
+def test_donation_challenge():
+    Donation.add_donation("Ann", 1)
+    Donation.add_donation("Ann", 2)
+    Donation.add_donation("Ann", 3)
+    Donation.add_donation("Barry", 4)
+    Donation.add_donation("Barry", 5)
+    Donation.add_donation("Barry", 6)
+    expected = "   Ann: $12.00 = 2 * $6.00\n" \
+               "   Barry: $30.00 = 2 * $15.00\n" \
+               "\n   Total contribution required: $42.00\n"
+    assert Donation.challenge(2) == expected
+
+def test_donors_challenge_min_donation():
+    Donation.add_donation("Ann", 1)
+    Donation.add_donation("Ann", 2)
+    Donation.add_donation("Ann", 3)
+    Donation.add_donation("Barry", 4)
+    Donation.add_donation("Barry", 5)
+    Donation.add_donation("Barry", 6)
+    expected = "   Barry: $30.00 = 2 * $15.00\n" \
+               "\n   Total contribution required: $30.00\n"
+    assert Donation.challenge(2, min_donation=4) == expected
+
+
+def test_donors_challenge_max_donation():
+    Donation.add_donation("Ann", 1)
+    Donation.add_donation("Ann", 2)
+    Donation.add_donation("Ann", 3)
+    Donation.add_donation("Barry", 4)
+    Donation.add_donation("Barry", 5)
+    Donation.add_donation("Barry", 6)
+    expected = "   Ann: $12.00 = 2 * $6.00\n" \
+               "   Barry: $8.00 = 2 * $4.00\n" \
+               "\n   Total contribution required: $20.00\n"
+    assert Donation.challenge(2, max_donation=4) == expected
+
+
+def test_donors_challenge_min_and_max_donation():
+    Donation.add_donation("Ann", 1)
+    Donation.add_donation("Ann", 2)
+    Donation.add_donation("Ann", 3)
+    Donation.add_donation("Barry", 4)
+    Donation.add_donation("Barry", 5)
+    Donation.add_donation("Barry", 6)
+    expected = "   Ann: $10.00 = 2 * $5.00\n" \
+               "   Barry: $18.00 = 2 * $9.00\n" \
+               "\n   Total contribution required: $28.00\n"
+    assert Donation.challenge(2, min_donation=2, max_donation=5) == expected
