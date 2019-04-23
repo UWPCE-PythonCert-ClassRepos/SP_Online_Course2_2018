baa02be2ef31b1ffe675ed03c96425dac0041d33
Laura Denney <Laura.A.Denney@gmail.com>
2019-04-22 22:23:53 -0700

    commiting files from squarer testing

---
+++ students/LauraDenney/lesson06/squarer.py
@@ -0,0 +1,11 @@
+# squarer.py
+class Squarer(object):
+
+    @staticmethod
+    def calc(operand):
+        #return operand*operand     # OLD
+        # return operand ** operand # WRONG
+        # return operand*operand    # OLD
+        # return operand**2         # OLD
+        # return operand**operand   #BAD
+        return operand*operand #This should work
