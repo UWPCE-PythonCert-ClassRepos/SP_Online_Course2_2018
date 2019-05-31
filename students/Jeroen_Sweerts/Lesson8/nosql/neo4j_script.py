235611e5303b831674cdfbcffb918095362d6272
JeroenSweerts <jeroensweerts@hotmail.com>
2019-05-31 23:00:25 +0200

    Solution for Neo4J

--- students/Jeroen_Sweerts/Lesson8/nosql/neo4j_script.py
+++ students/Jeroen_Sweerts/Lesson8/nosql/neo4j_script.py
@@ -95,3 +95,124 @@ def run_example():
         for rec in result:
             for friend in rec.values():
                 print(friend['first_name'], friend['last_name'])
+
+                
+def neo4j_assignment():
+    log.info('Step 1: First, clear the entire database, so we can start over')
+    log.info("Running clear_all")
+
+    driver = login_database.login_neo4j_cloud()
+    with driver.session() as session:
+        session.run("MATCH (n) DETACH DELETE n")
+
+    log.info("Step 2: Add a few people")
+
+    with driver.session() as session:
+
+        log.info('Adding a few Person nodes')
+        log.info('The cyph language is analagous to sql for neo4j')
+        for first, last in [('Bob', 'Jones'),
+                            ('Nancy', 'Cooper'),
+                            ('Alice', 'Cooper'),
+                            ('Fred', 'Barnes'),
+                            ('Mary', 'Evans'),
+                            ('Marie', 'Curie'),
+                            ('Pal', 'Jas'),
+                            ('Mad','Onna'),
+                            ('Hillarious','Clinton')
+                            ]:
+            cyph = "CREATE (n:Person {first_name:'%s', last_name: '%s'})" % (
+                first, last)
+            session.run(cyph)    
+            
+        log.info("Step 3: Get all of people in the DB:")
+        cyph = """MATCH (p:Person)
+                  RETURN p.first_name as first_name, p.last_name as last_name
+                """
+        result = session.run(cyph)
+        result_copy = []  # creating a copy to use later on
+        print("People in database:")
+        for record in result:
+            print(record['first_name'], record['last_name'])
+            result_copy.append(record)            
+#############################################################################################################
+        for color in ['Pink',
+                      'AppleBlueSeaGreen',
+                      'Pistache',
+                      'Kakki',
+                      'Black',
+                      'White',
+                      'Rainbow']:
+            color_cyph = "CREATE (c:Color {color:'%s'})" % (color)
+            session.run(color_cyph)
+        log.info("Assign favorite colors to people.")
+        for first, last in [('Bob', 'Jones'),
+                            ('Nancy', 'Cooper'),
+                            ('Alice', 'Cooper')]:
+            for color in ['Pink',
+                      'AppleBlueSeaGreen',
+                      'Pistache']:
+                fav_color_cyph_1 = """MATCH (p1:Person), (c1:Color)
+                                      WHERE p1.first_name='%s' AND p1.last_name='%s' AND c1.color="%s"
+                                      create (p1)-[f:FAVORITE_COLOR]->(c1)
+                                      return p1, f, c1
+                """ % (first, last, color)
+                session.run(fav_color_cyph_1)
+
+        for first, last in [('Fred', 'Barnes'),
+                            ('Mary', 'Evans'),
+                            ('Marie', 'Curie')]:
+            for color in ['Kakki',
+                      'Black',
+                      'White']:
+                fav_color_cyph_2 = """MATCH (p1:Person), (c1:Color)
+                                      WHERE p1.first_name='%s' AND p1.last_name='%s' AND c1.color="%s"
+                                      create (p1)-[f:FAVORITE_COLOR]->(c1)
+                                      return p1, f, c1
+                """ % (first, last, color)
+                session.run(fav_color_cyph_2)
+
+        for first, last in [('Pal', 'Jas'),
+                            ('Mad','Onna'),
+                            ('Hillarious','Clinton')]:
+            for color in ['AppleBlueSeaGreen',
+                          'Rainbow'
+                         ]:
+                fav_color_cyph_3 = """MATCH (p1:Person), (c1:Color)
+                                      WHERE p1.first_name='%s' AND p1.last_name='%s' AND c1.color="%s"
+                                      create (p1)-[f:FAVORITE_COLOR]->(c1)
+                                      return p1, f, c1
+                """ % (first, last, color)
+                session.run(fav_color_cyph_3)
+
+        all_color_cyph = """MATCH (c1:Color)
+                            RETURN c1.color
+        """
+        all_colors = session.run(all_color_cyph)
+        log.info("Show people's favorite colors")
+        for fav_color in all_colors:
+            who_likes_cyph = """MATCH (color_likers)-[f:FAVORITE_COLOR]->(c1 {color:'%s'})
+                                RETURN color_likers
+            """ % (fav_color)
+            output = session.run(who_likes_cyph)
+            favorite = fav_color.values()[0]
+            print("The people who like {} are:".format(favorite))
+            for row in output:
+                for liker in row.values():
+                    print(liker['first_name'], liker['last_name'])
+
+        # recalling the result cypher from earlier that pulled all people
+
+        for record in result_copy:
+            first = record['first_name']
+            last = record['last_name']
+            likes_what_cyph = """MATCH (p1:Person)-[f:FAVORITE_COLOR]->(likes_what_colors)
+                                 WHERE p1.first_name='%s' AND p1.last_name='%s'
+                                 RETURN likes_what_colors
+            """ % (first, last)
+            print('{} {}\'s favorite colors are:'.format(first, last))
+            color_output = session.run(likes_what_cyph)
+            for row in color_output:
+                print(row.values()[0]['color'])
+
+    
