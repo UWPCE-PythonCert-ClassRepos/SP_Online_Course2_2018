23c78a7c7635a0f6c9debf3ba454e01731cfe286
jayjones <jayjonesseattle@gmail.com>
2019-06-05 11:11:43 -0700

    adding rdbs

---
+++ students/TerranceJ/lesson7/RDBS/Jay_prettyprint.py
@@ -0,0 +1,33 @@
+"""
+    This program prints the person, job, and department tables
+"""
+
+from pprint import pprint as pp
+from Jay_personjobdept_model import *
+import logging
+
+
+def printdb():
+
+    logging.basicConfig(level=logging.INFO)
+    logger = logging.getLogger(__name__)
+
+    database = SqliteDatabase('personjobdept.db')
+
+    try:
+        database.connect()
+        database.execute_sql('PRAGMA foreign_keys = ON;')
+        query = (Job.select())
+        pp('Job Name                  | Employee        | Department     ')
+        pp('-------------------------------------------------------------')
+        for job in query:
+            pp('{:25} | {:15} | {:15}'.format(
+                job.job_name, str(job.person_employed), job.department_name))
+    except Exception as e:
+        logger.info(e)
+    finally:
+        logger.info('database closes')
+        database.close()
+
+if __name__ == '__main__':
+    printdb()
