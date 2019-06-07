05cebbba6e09974f8ccc5c3dfc4bb8a88d21ee52
srepking <shanerepking@gmail.com>
2019-06-06 23:09:36 -0700

    6/6/2019

--- students/srepking/lesson10/pypy_vs_python.py
+++ students/srepking/lesson10/pypy_vs_python.py
@@ -1,10 +1,18 @@
+import time
 
+my_list = list(range(100))
+times = 10000
 
 def intsum():
-    startval = 0
+    def intsum_init():
+        return 0
+    startval = intsum_init()
     while True:
-        yield startval
-        startval += 1
+        val = (yield startval)
+        if val == 'restart':
+            startval = intsum_init()
+        else:
+            startval += 1
 
 
 def intsum2():
@@ -23,6 +31,7 @@ def doubler():
         start_val1 = start_val1 * 2
 
 
+
 def fib():
     start_val1 = 1
     start_val2 = 1
@@ -36,6 +45,8 @@ def fib():
 
 
 def prime():
+    def prime_init():
+        num_gen.send('restart')
     num_gen = intsum()
     next_num = next(num_gen)
     while next_num < 2:
@@ -48,11 +59,54 @@ def prime():
                 next_num = next(num_gen)
                 break
         else:
-            yield next_num
-            next_num = next(num_gen)
+            val = (yield next_num)
+            if val == 'restart':
+                prime_init()
+                num_gen = intsum()
+                next_num = next(num_gen)
+            else:
+             next_num = next(num_gen)
+
 
 def factorial(num):
-    if num == 1:
-        return 1
-    else:
-        return num*factorial(num-1)
+    fact = 1
+    for i in range(1, num+1):
+        fact = fact * i
+    return fact
+
+
+
+
+
+init = time.perf_counter()
+doublerdo = doubler() #initiate the generator
+for t in range(times):
+    doubler_seq = [next(doublerdo) for x in my_list]
+    doublerdo = doubler()
+    #print(doubler_seq)
+print("Doubler took: %s" % (time.perf_counter() - init))
+
+## Beginning of Fibonacci Code
+fibber = fib()
+init = time.perf_counter()
+for t in range(times):
+    fibber_list = [next(fibber) for x in my_list]
+    fibber = fib()
+    #print(fibber_list)
+print("Fibonacci took: %s" % (time.perf_counter() - init))
+
+
+primer = prime()
+init = time.perf_counter()
+for t in range(times):
+    prime_list = [next(primer) for x in my_list]
+    #prime.send('restart')
+    primer = prime()
+    #print(prime_list)
+print("Prime took: %s" % (time.perf_counter() - init))
+
+init = time.perf_counter()
+for t in range(times):
+    factorial_list = [factorial(x) for x in my_list]
+    #print(factorial_list)
+print("Factorial took: %s" % (time.perf_counter() - init))
