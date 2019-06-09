ce77da90d861ae90a43cca1e19a98eab63bf1d53
srepking <shanerepking@gmail.com>
2019-06-08 22:32:14 -0700

    Ready for review 6/8

--- students/srepking/lesson10/pypy_no_lists.py
+++ students/srepking/lesson10/pypy_no_lists.py
@@ -1,9 +1,18 @@
+""""Instead of using comprehensions and range to call the generators and
+create a list, we will use a while loop until a certain number size is reached.
+We will use the largest number in the lists that pypy_vs_python.py created, and
+then compare the speed from this file to the pypy_vs_python.py file."""
+
 import time
 import cProfile
 import pstats
 
-my_list = list(range(1000))
+#my_list = list(range(1000))
 times = 1000
+double_limit = 5357543035931336604742125245300009052807024058527668037218751941851755255624680612465991894078479290637973364587765734125935726428461570217992288787349287401967283887412115492710537302531185570938977091076523237491790970633699383779582771973038531457285598238843271083830214915826312193418602834034688
+fib_limit = 43466557686937456435688527675040625802564660517371780402481729089536555417949051890403879840079255169295922593080322634775209689623239873322471161642996440906533187938298969649928516003704476137795166849228875
+prime_limit = 7919
+
 
 def intsum():
     startval = 0
@@ -28,7 +37,6 @@ def doubler():
         start_val1 = start_val1 * 2
 
 
-
 def fib():
     start_val1 = 1
     start_val2 = 1
@@ -42,10 +50,7 @@ def fib():
 
 
 def prime():
-    """This was my first attempt at calculating Prime. The methods were
-    slow, and the profiler wasn't identifying anything that was obviously
-    causing the slowdown. All of the time was taken in the 'Prime()' function.
-    """
+
     num_gen = intsum()
     next_num = next(num_gen)
     while next_num < 2:
@@ -61,18 +66,8 @@ def prime():
             yield next_num
             next_num = next(num_gen)
 
+
 def prime_new():
-    """ Every composite number (number that is not prime) has a factor
-    less than or equal to its square root. Therefore, we can search only,
-    up to this square root, and if a factor is not found, the number is
-    prime. What is interesting to note looking at the profiler between
-    the prime_new function and the prime() function is that the builtins.next
-    were called the same amount. This means that the number generator
-    was called the exact same amount of time for both of these functions.
-    All of the time savings is presumably due to the factor that prime_new()
-    is creating a much smaller range of numbers because it is only searching up
-    to the square root of the number being evaluated for being prime.
-    Interesting and unexpected to me!"""
 
     num_gen = intsum()
     next_num = next(num_gen)
@@ -90,15 +85,17 @@ def prime_new():
             next_num = next(num_gen)
 
 
-
 pr_double = cProfile.Profile()  # create profiler instance
 pr_double.enable()  # start profiler
 init = time.perf_counter()
-doublerdo = doubler() #initiate the generator
 for t in range(times):
-    doubler_seq = [next(doublerdo) for x in my_list]
     doublerdo = doubler()
-    #print(doubler_seq)
+    doubler_seq = []
+    next_num = next(doublerdo)
+    while next_num <= double_limit:
+        doubler_seq.append(next_num)
+        next_num = next(doublerdo)
+#print(doubler_seq)
 print('Last Double # is: ', doubler_seq[-1])
 print("Doubler took: %s" % (time.perf_counter() - init))
 pr_double.disable()  # stop the profiler
@@ -108,12 +105,15 @@ stats_double.print_stats()  # print the statistics from profiler
 ## Beginning of Fibonacci Code
 pr_fib = cProfile.Profile()
 pr_fib.enable()
-fibber = fib()
 init = time.perf_counter()
 for t in range(times):
-    fibber_list = [next(fibber) for x in my_list]
     fibber = fib()
-    #print(fibber_list)
+    fibber_list = []
+    next_num = next(fibber)
+    while next_num <= fib_limit:
+        fibber_list.append(next_num)
+        next_num = next(fibber)
+#print(fibber_list)
 print('Last Fibonacci # is: ', fibber_list[-1])
 print("Fibonacci took: %s" % (time.perf_counter() - init))
 pr_fib.disable()
@@ -127,9 +127,12 @@ pr_prime.enable()  # start profiler
 primer = prime()
 init = time.perf_counter()
 for t in range(times):
-    prime_list = [next(primer) for x in my_list]
     primer = prime()
-    #print(prime_list)
+    prime_list = []
+    next_num = next(primer)
+    while next_num <= prime_limit:
+        prime_list.append(next_num)
+        next_num = next(primer)
 print('Last prime # is: ', prime_list[-1])
 print("Prime took: %s" % (time.perf_counter() - init))
 pr_prime.disable()  # stop profiler
@@ -138,20 +141,20 @@ stats_prime.print_stats()  # print the stats from profiler
 
 
 ## Beginning of Prime_New Code with square root limiting range
-pr_prime_new = cProfile.Profile() # create profiler instance
+pr_prime_new = cProfile.Profile()  # create profiler instance
 pr_prime_new.enable()  # start profiler
 primer_n = prime_new()
 init = time.perf_counter()
 for t in range(times):
-    prime_n_list = [next(primer_n) for x in my_list]
     primer_n = prime_new()
-    #print(prime_n_list)
-#print('Last prime_new # is: ', prime_n_list[-1])
+    prime_n_list = []
+    next_num = next(primer_n)
+    while next_num <= prime_limit:
+        prime_n_list.append(next_num)
+        next_num = next(primer_n)
+print('Last prime_new # is: ', prime_n_list[-1])
 print("Prime Search limited by Square Root took: %s"
       % (time.perf_counter() - init))
 pr_prime_new.disable()  # stop profiler
 stats_prime_new = pstats.Stats(pr_prime_new)  # create stats instance
 stats_prime_new.print_stats()  # print the stats from profiler
-
-
-
