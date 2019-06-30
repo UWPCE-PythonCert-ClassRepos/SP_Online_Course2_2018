fb1e6bf0d95feb22210e54d3024e20d02026c23d
jaredmulholland <jaredmulholland26@gmail.com>
2019-06-30 15:57:05 -0700

    made unit test executable from outside file

--- students/jaredmulholland/lesson_6/calculator/test_calculator.py
+++ students/jaredmulholland/lesson_6/calculator/test_calculator.py
@@ -1,12 +1,12 @@
 from unittest import TestCase
 from unittest.mock import MagicMock
 
-from adder import Adder
-from subtracter import Subtracter
-from divider import Divider
-from multiplier import Multiplier
-from calculator import Calculator
-from exceptions import InsufficientOperands
+from calculator.adder import Adder
+from calculator.subtracter import Subtracter
+from calculator.divider import Divider
+from calculator.multiplier import Multiplier
+from calculator.calculator import Calculator
+from calculator.exceptions import InsufficientOperands
 
 class AdderTests(TestCase):
 
@@ -98,27 +98,3 @@ class CalculatorTests(TestCase):
 
         self.multiplier.calc.assert_called_with(1,2)
 
-class ModuleTest(TestCase):
-
-    def test_module(self):
-
-        calculator = Calculator(Adder(), Subtracter(), Multiplier(), Divider())
-
-        calculator.enter_number(5)
-        calculator.enter_number(2)
-
-        calculator.multiply()
-
-        calculator.enter_number(46)
-
-        calculator.add()
-
-        calculator.enter_number(8)
-
-        calculator.divide()
-
-        calculator.enter_number(1)
-
-        result = calculator.subtract()
-
-        self.assertEqual(6, result)
